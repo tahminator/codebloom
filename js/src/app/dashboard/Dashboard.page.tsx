08f6c7c5be45eddcd6c4a9979c5a42fb1fa116f9
Alisha Zaman <az2924@nyu.edu>
2025-01-08 21:28:54 -0500

    Used @ for imports

--- js/src/app/dashboard/Dashboard.page.tsx
+++ js/src/app/dashboard/Dashboard.page.tsx
@@ -1,8 +1,8 @@
 import Toast from "@/components/ui/toast/Toast";
 import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
 import { Loader } from "@mantine/core";
-import LogoutButton from "../../components/ui/LogoutButton";
-import { useAuthQuery } from "../login/hooks";
+import LogoutButton from "@/components/ui/LogoutButton";
+import { useAuthQuery } from "@/app/login/hooks";
 
 export default function DashboardPage() {
   const { data, status } = useAuthQuery();
@@ -18,6 +18,7 @@ export default function DashboardPage() {
   if (status === "error") {
     return <Toast message="Sorry, something went wrong." />;
   }
+
   const authenticated = !!data.user && !!data.session;
 
   if (!authenticated) {
@@ -25,6 +26,7 @@ export default function DashboardPage() {
       <ToastWithRedirect to="/login" message="You are not authenticated!" />
     );
   }
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <LogoutButton />
