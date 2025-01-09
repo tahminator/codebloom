8457f5212ff193b171a0fd0cbf5a6195eecf7fbc
Alisha Zaman <az2924@nyu.edu>
2025-01-09 02:31:17 -0500

    Login Page with Button to log in and go back

--- js/src/components/ui/auth/LoginButton.tsx
+++ js/src/components/ui/auth/LoginButton.tsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function LoginButton({ className }: { className?: string }) {
   return (
     <Link to="/api/auth/flow/discord" reloadDocument>
-      <Button className={className}>Login to Discord</Button>
+      <Button fullWidth radius="md" className={className}>
+        Login to Discord
+      </Button>
     </Link>
   );
 }
