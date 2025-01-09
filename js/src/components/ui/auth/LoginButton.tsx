c4fa5068472735fe94434842d8a46fcaa3dd928a
Alisha Zaman <az2924@nyu.edu>
2025-01-09 18:10:07 -0500

    Added Discord logo to login button

--- js/src/components/ui/auth/LoginButton.tsx
+++ js/src/components/ui/auth/LoginButton.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@mantine/core";
 import { Link } from "react-router-dom";
+import { FaDiscord } from "react-icons/fa";
 
 export default function LoginButton({ className }: { className?: string }) {
   return (
     <Link to="/api/auth/flow/discord" reloadDocument>
-      <Button fullWidth radius="md" className={className}>
+      <Button
+        color="#5865F2"
+        fullWidth
+        leftSection={<FaDiscord size={14} />}
+        radius="md"
+        className={className}
+      >
         Login to Discord
       </Button>
     </Link>
