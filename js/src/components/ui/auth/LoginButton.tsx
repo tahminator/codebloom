ae0d2f068cd98446484649a8a024f0823635d1aa
Alisha Zaman <az2924@nyu.edu>
2025-01-08 22:56:16 -0500

    Changed function name from LogoutButton to LoginButton

--- js/src/components/ui/auth/LoginButton.tsx
+++ js/src/components/ui/auth/LoginButton.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@mantine/core";
 import { Link } from "react-router-dom";
 
-export default function LogoutButton({ className }: { className?: string }) {
+export default function LoginButton({ className }: { className?: string }) {
   return (
     <Link to="/api/auth/flow/discord" reloadDocument>
       <Button className={className}>Login to Discord</Button>
