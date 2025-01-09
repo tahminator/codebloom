a33aba983a7e5d8daf0155fd30fdf71968d6a463
Alisha Zaman <az2924@nyu.edu>
2025-01-08 20:50:44 -0500

    Set up the logout button with authentification

---
+++ js/src/app/dashboard/Dashboard.page.tsx
@@ -0,0 +1,33 @@
+import Toast from "@/components/ui/toast/Toast";
+import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
+import { Loader } from "@mantine/core";
+import LogoutButton from "../../components/ui/LogoutButton";
+import { useAuthQuery } from "../login/hooks";
+
+export default function DashboardPage() {
+  const { data, status } = useAuthQuery();
+
+  if (status === "pending") {
+    return (
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return <Toast message="Sorry, something went wrong." />;
+  }
+  const authenticated = !!data.user && !!data.session;
+
+  if (!authenticated) {
+    return (
+      <ToastWithRedirect to="/login" message="You are not authenticated!" />
+    );
+  }
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <LogoutButton />
+    </div>
+  );
+}
