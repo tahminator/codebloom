7c53888244cfcde43046d35a9ca2f0e15b868928
Alisha Zaman <az2924@nyu.edu>
2025-01-10 03:56:55 -0500

    Created navbar for dashboard with icon, dashbord button, home button, and logout button, and menu view for mobile

--- js/src/app/dashboard/Dashboard.page.tsx
+++ js/src/app/dashboard/Dashboard.page.tsx
@@ -1,12 +1,28 @@
+import { useAuthQuery } from "@/app/login/hooks";
+import LogoutButton from "@/components/ui/auth/LogoutButton";
 import Toast from "@/components/ui/toast/Toast";
 import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
-import { Loader } from "@mantine/core";
-import LogoutButton from "@/components/ui/auth/LogoutButton";
-import { useAuthQuery } from "@/app/login/hooks";
+import {
+  Box,
+  Burger,
+  Button,
+  Group,
+  Loader,
+  Title,
+  Text,
+  Divider,
+  Drawer,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Link } from "react-router-dom";
+import classes from "./Dashboard.page.module.css";
+import Logo from "@/logo.png";
 
 export default function DashboardPage() {
   const { data, status } = useAuthQuery();
 
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
   if (status === "pending") {
     return (
       <div className="flex flex-col items-center justify-center w-screen h-screen">
@@ -28,8 +44,50 @@ export default function DashboardPage() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <LogoutButton />
-    </div>
+    <Box
+      style={{
+        background: "#303030",
+      }}
+    >
+      <header className={classes.header}>
+        <Group>
+          <img src={Logo} width={45} alt="Logo" />
+          <Title>
+            <Text>CodeBloom</Text>
+          </Title>
+        </Group>
+        <Group>
+          <Link to="/" reloadDocument>
+            <Button>Home</Button>
+          </Link>
+          <Link to="/dashboard" reloadDocument>
+            <Button>Dashboard</Button>
+          </Link>
+        </Group>
+        <Group>
+          <LogoutButton />
+        </Group>
+        <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />{" "}
+      </header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="CodeBloom"
+      >
+        <Divider my="sm" />
+        <Link to="/" reloadDocument>
+          <Button>Home</Button>
+        </Link>
+        <Divider my="sm" />
+        <Link to="/dashboard" reloadDocument>
+          <Button>Dashboard</Button>
+        </Link>
+        <Divider my="sm" />
+        <LogoutButton />
+      </Drawer>
+    </Box>
   );
 }
