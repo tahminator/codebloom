2023c857dfb943e73bba43f0b170611e7356e1e5
Tahmid Ahmed <tahmid@tahmid.io>
2025-01-13 17:35:03 -0500

    Dynamically import swagger.css to avoid CSS leaks.

--- js/src/lib/router.tsx
+++ js/src/lib/router.tsx
@@ -2,6 +2,7 @@ import DashboardPage from "@/app/dashboard/Dashboard.page";
 import LeaderboardPage from "@/app/leaderboard/Leaderboard.page";
 import LoginPage from "@/app/login/Login.page";
 import RootPage from "@/app/Root.page";
+import SwaggerUIParent from "@/lib/swagger/swagger-ui";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -20,5 +21,13 @@ export const router = createBrowserRouter([
   {
     path: "/leaderboard",
     element: <LeaderboardPage />,
+  },
+  {
+    path: "/swagger",
+    element: import.meta.env.DEV ? (
+      <SwaggerUIParent />
+    ) : (
+      <div>This endpoint is disabled in production.</div>
+    ),
   },
 ]);
