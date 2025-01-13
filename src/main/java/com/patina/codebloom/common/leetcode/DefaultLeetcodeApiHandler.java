3103f097b6f170ea69dd4241e8ad6ca872903ead
Tahmid Ahmed <tahmid@tahmid.io>
2025-01-13 00:43:24 -0500

    Fill in the LeetcodeHandler implementer class

--- src/main/java/com/patina/codebloom/common/leetcode/DefaultLeetcodeApiHandler.java
+++ src/main/java/com/patina/codebloom/common/leetcode/DefaultLeetcodeApiHandler.java
@@ -1,5 +1,8 @@
 package com.patina.codebloom.common.leetcode;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DefaultLeetcodeApiHandler implements LeetcodeApiHandler {
 
 }
