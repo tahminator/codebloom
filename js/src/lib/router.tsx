4ad9cfb1b4b53b8970787eded9edc838a2d86d3a
Tahmid Ahmed <tahmid@tahmid.io>
2025-01-13 20:06:09 -0500

    Removed client side Swagger and added a clever redirect to handle /swagger on the frontend to serve the proper files from the backend.

--- js/src/lib/router.tsx
+++ js/src/lib/router.tsx
@@ -2,7 +2,6 @@ import DashboardPage from "@/app/dashboard/Dashboard.page";
 import LeaderboardPage from "@/app/leaderboard/Leaderboard.page";
 import LoginPage from "@/app/login/Login.page";
 import RootPage from "@/app/Root.page";
-import SwaggerUIParent from "@/lib/swagger/swagger-ui-parent";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -21,13 +20,5 @@ export const router = createBrowserRouter([
   {
     path: "/leaderboard",
     element: <LeaderboardPage />,
-  },
-  {
-    path: "/swagger",
-    element: import.meta.env.DEV ? (
-      <SwaggerUIParent />
-    ) : (
-      <div>This endpoint is disabled in production.</div>
-    ),
   },
 ]);
