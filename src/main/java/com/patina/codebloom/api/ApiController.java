8ab5266b57cf043d5e50c1486d7081eeef9ac929
Alfardil Alam <fardilfayed@gmail.com>
2025-01-13 19:24:21 -0500

    editting leetcoe handler

--- src/main/java/com/patina/codebloom/api/ApiController.java
+++ src/main/java/com/patina/codebloom/api/ApiController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patina.codebloom.common.dto.ApiResponder;
+import com.patina.codebloom.common.leetcode.LeetcodeApiHandler;
 import com.patina.codebloom.common.metadata.ServerMetadataObject;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,9 +18,16 @@ import jakarta.servlet.http.HttpServletRequest;
 @Tag(name = "Basic server metadata")
 public class ApiController {
 
+    private final LeetcodeApiHandler leetcodeApiHandler;
+
+    public ApiController(LeetcodeApiHandler leetcodeApiHandler) {
+        this.leetcodeApiHandler = leetcodeApiHandler;
+    }
+
     @Operation(summary = "Basic metadata about the server")
     @GetMapping()
     public ResponseEntity<ApiResponder<ServerMetadataObject>> apiIndex(HttpServletRequest request) {
+        leetcodeApiHandler.findQuestionBySlug("two-sum");
         return ResponseEntity.ok().body(ApiResponder.success("Hello from Codebloom!", new ServerMetadataObject()));
     }
 
