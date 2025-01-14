1dc02d8a0f567883e1ac001314c55efab2d15e76
Alfardil Alam <fardilfayed@gmail.com>
2025-01-13 20:01:50 -0500

    finished the leetcode handler methods and cleaned up the codebase

--- src/main/java/com/patina/codebloom/api/ApiController.java
+++ src/main/java/com/patina/codebloom/api/ApiController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patina.codebloom.common.dto.ApiResponder;
-import com.patina.codebloom.common.leetcode.LeetcodeApiHandler;
 import com.patina.codebloom.common.metadata.ServerMetadataObject;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,16 +17,10 @@ import jakarta.servlet.http.HttpServletRequest;
 @Tag(name = "Basic server metadata")
 public class ApiController {
 
-    private final LeetcodeApiHandler leetcodeApiHandler;
-
-    public ApiController(LeetcodeApiHandler leetcodeApiHandler) {
-        this.leetcodeApiHandler = leetcodeApiHandler;
-    }
-
     @Operation(summary = "Basic metadata about the server")
     @GetMapping()
     public ResponseEntity<ApiResponder<ServerMetadataObject>> apiIndex(HttpServletRequest request) {
-        leetcodeApiHandler.findQuestionBySlug("trapping-rain-water");
         return ResponseEntity.ok().body(ApiResponder.success("Hello from Codebloom!", new ServerMetadataObject()));
     }
+
 }
