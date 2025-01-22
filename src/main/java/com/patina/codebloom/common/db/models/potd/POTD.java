03cf641d3d099ed601bde2f6fca84abf619c9119
Alfardil Alam <fardilfayed@gmail.com>
2025-01-22 14:20:28 -0500

    created POTD table + sql migration

---
+++ src/main/java/com/patina/codebloom/common/db/models/potd/POTD.java
@@ -0,0 +1,60 @@
+package com.patina.codebloom.common.db.models.potd;
+
+import java.time.LocalDateTime;
+
+public class POTD {
+    private String id;
+    private String title;
+    private String slug;
+    private int multiplier;
+    private LocalDateTime createdAt;
+
+    public POTD(String id, String title, String slug, int multiplier, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.slug = slug;
+        this.multiplier = multiplier;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
---
+++ src/main/java/com/patina/codebloom/common/db/repos/potd/POTDRepository.java
@@ -0,0 +1,17 @@
+package com.patina.codebloom.common.db.repos.potd;
+
+import java.util.ArrayList;
+
+import com.patina.codebloom.common.db.models.potd.POTD;
+
+public interface POTDRepository {
+    POTD createPOTD(POTD potd);
+
+    POTD getPOTDById(String id);
+
+    ArrayList<POTD> getAllPOTDS();
+
+    void updatePOTD(POTD potd);
+
+    void deletePOTD(String id);
+}
