6b9bf1c077ad29f45aee98ba3ab65869c6c282bf
Tahmid Ahmed <tahmid@tahmid.io>
2025-02-06 04:07:14 -0500

    Cleaning up the useURLState hook.

--- js/src/lib/custom-hooks/use-url-state.ts
+++ js/src/lib/custom-hooks/use-url-state.ts
@@ -2,22 +2,32 @@ import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 /**
- * This function allows us to coerce the URL param value string to the type of our default value.
+ * This function allows us to coerce the URL param value string to the type of our default value. If coercion fails, a Symbol is returned so that the error case can be handled.
  */
 function coerce<T>(param: string, defaultValue: T): T | symbol {
-  const paramType = typeof param;
-  const defaultValueType = typeof defaultValue;
+  try {
+    const paramType = typeof param;
+    const defaultValueType = typeof defaultValue;
 
-  if (defaultValueType === "string") {
-    return paramType as T;
-  }
+    // The default param value is always string, so no type coercion is needed.
+    if (defaultValueType === "string") {
+      return paramType as T;
+    }
 
-  if (paramType != defaultValueType) {
-    if (defaultValueType === "number") return Number(param) as T;
+    if (defaultValueType === "number") {
+      const possibleNumber = Number(param);
+      if (Number.isNaN(possibleNumber)) {
+        return Symbol();
+      }
+      return possibleNumber as T;
+    }
+
     if (defaultValueType === "boolean") return (param === "true") as T;
-  }
 
-  return Symbol();
+    return Symbol();
+  } catch {
+    return Symbol();
+  }
 }
 
 /**
@@ -25,59 +35,68 @@ function coerce<T>(param: string, defaultValue: T): T | symbol {
  *
  * @param name The name of the key in the URL
  * @param defaultValue The default value of the param if none is present in the URL. The type of this variable will also be used to determine if the string value from the URL should be coerced into something else.
- * @param tieToUrl If you would like, you may disable tieing the values of this to a hook. This defaults to true.
+ * @param enabled If you would like, you may disable tieing the values of this to a hook. This defaults to true.
+ * @param resetOnDefault If the value equals to the default value (either on mount or if it returns back to the default value), clear the URL param. This defaults to true.
  *
  * Returns a stateful value and a function to update it.
  */
 export function useURLState<T>(
   name: string,
   defaultValue: T,
-  tieToUrl = true,
+  enabled = true,
   resetOnDefault = true
 ) {
   const [initial, setInitial] = useState(false);
   const [value, setValue] = useState<T>(defaultValue);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  // On initial mount, update the state with the URL params.
+  // On initial mount, update the state with the URL params. This hook will not run if the hook is not enabled or the initial value has already been provided.
   useEffect(() => {
-    if (tieToUrl) {
-      const param = searchParams.get(name);
+    if (!enabled || initial) {
+      return;
+    }
 
-      if (param == null) {
-        setValue(defaultValue);
-      } else {
-        const val = coerce(param, defaultValue);
-        // If coercion of type fails, it will return Symbol.
-        setValue(typeof val === "symbol" ? defaultValue : val);
-      }
+    const param = searchParams.get(name);
 
+    // No value found in the URL.
+    if (param == null) {
+      setValue(defaultValue);
       setInitial(true);
+      return;
     }
-  }, [defaultValue, name, searchParams, tieToUrl]);
 
-  // Update the URL with the new state, only if the initial value hasn't been set already.
+    const val = coerce(param, defaultValue);
+    // If coercion of type fails, it will return Symbol.
+    setValue(typeof val === "symbol" ? defaultValue : val);
+    setInitial(true);
+  }, [defaultValue, name, searchParams, enabled, initial]);
+
+  // Update the URL with the new state, only if the initial value hasn't been set already and if the hook is enabled.
   useEffect(() => {
-    if (tieToUrl && initial) {
-      if (value === defaultValue && resetOnDefault) {
-        setSearchParams((prev) => {
-          prev.delete(name);
-          return prev;
-        });
-      } else {
-        setSearchParams((prev) => {
-          prev.set(name, String(value));
-          return prev;
-        });
-      }
+    if (!enabled || !initial) {
+      return;
     }
+
+    // If resetOnDefault is enabled, clear the key.
+    if (value === defaultValue && resetOnDefault) {
+      setSearchParams((prev) => {
+        prev.delete(name);
+        return prev;
+      });
+      return;
+    }
+
+    setSearchParams((prev) => {
+      prev.set(name, String(value));
+      return prev;
+    });
   }, [
     defaultValue,
+    enabled,
     initial,
     name,
     resetOnDefault,
     setSearchParams,
-    tieToUrl,
     value,
   ]);
 
