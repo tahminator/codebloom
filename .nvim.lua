d3a4f7d1d5a801bb2182206226d77c5303d1b799
Tahmid Ahmed <tahmid@tahmid.io>
2025-02-25 01:01:26 -0500

    Fixed syntax issues on java formatter + added nvim config to support vscode defaults

---
+++ .nvim.lua
@@ -0,0 +1,26 @@
+-- Set tab size to 4 spaces locally
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Ensure jdtls uses 4 spaces for indentation
+local config = {
+	settings = {
+		java = {
+			format = {
+				settings = {
+					url = "./java-formatter.xml",
+					tabSize = 4,
+					indentationSize = 4,
+					insertSpaces = true, -- Ensures spaces instead of tabs
+				},
+			},
+		},
+	},
+}
+
+-- Apply settings only if jdtls is running
+if vim.lsp.get_active_clients({ name = "jdtls" }) then
+	require("lspconfig").jdtls.setup(config)
+end
