31b1cc1698cce627ba91b7fa36b1952e0c8d9eb8
Tahmid Ahmed <tahmid@tahmid.io>
2025-02-26 19:19:51 -0500

    Added JSDoc to JDA event listener

--- src/main/java/com/patina/codebloom/jda/JDAEventListener.java
+++ src/main/java/com/patina/codebloom/jda/JDAEventListener.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+/**
+ * Do not remove this. JDA requires at least one listener in order to function.
+ */
 @Component
 public class JDAEventListener extends ListenerAdapter {
     public void say(final SlashCommandInteractionEvent event, final String content) {
