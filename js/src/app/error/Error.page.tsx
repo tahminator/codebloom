cb5100c8434033852c4fd443d9131fc5bbb542b6
Alisha Zaman <az2924@nyu.edu>
2025-03-09 18:45:18 -0400

    Error Page

---
+++ js/src/app/error/Error.page.tsx
@@ -0,0 +1,35 @@
+import { Button, Container, Flex, Group, Text, Title } from "@mantine/core";
+import { Illustration } from "./Illustration";
+import classes from "./ErrorPage.module.css";
+import { Link } from "react-router-dom";
+
+export default function ErrorPage() {
+  return (
+    <Container className={classes.root}>
+      <div className={classes.inner}>
+        <Illustration className={classes.image} />
+        <div className={classes.content}>
+          <Flex gap={"md"} direction={"column"}>
+            <Title className={classes.title}>Womp Womp.</Title>
+            <div className="flex flex-col flex-wrap items-center">
+              <Text
+                c="dimmed"
+                size="lg"
+                ta="center"
+                className={classes.description}
+              >
+                Unfortunately, this is only a 404 page. You may have mistyped
+                the address, or the page has been moved to another URL.
+              </Text>
+            </div>
+            <Group justify="center">
+              <Button size="md" component={Link} to={"/"}>
+                Go back to the home page
+              </Button>
+            </Group>
+          </Flex>
+        </div>
+      </div>
+    </Container>
+  );
+}
