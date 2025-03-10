951ea0da62e2ca4e727d7221ede239a522fcfdb9
Alisha Zaman <az2924@nyu.edu>
2025-03-09 21:28:27 -0400

    Fixed error page background

--- js/src/app/error/Error.page.tsx
+++ js/src/app/error/Error.page.tsx
@@ -1,36 +1,38 @@
-import { Button, Container, Flex, Group, Text, Title } from "@mantine/core";
+import { Illustration } from "@/app/error/IIllustration";
+import { Box, Button, Flex, Group, Text, Title } from "@mantine/core";
 import { Link } from "react-router-dom";
-
-import classes from "./ErrorPage.module.css";
-import { Illustration } from "./Illustration";
 
 export default function ErrorPage() {
   return (
-    <Container className={classes.root}>
-      <div className={classes.inner}>
-        <Illustration className={classes.image} />
-        <div className={classes.content}>
-          <Flex gap={"md"} direction={"column"}>
-            <Title className={classes.title}>Womp Womp.</Title>
-            <div className="flex flex-col flex-wrap items-center">
-              <Text
-                c="dimmed"
-                size="lg"
-                ta="center"
-                className={classes.description}
-              >
-                Unfortunately, this is only a 404 page. You may have mistyped
-                the address, or the page has been moved to another URL.
-              </Text>
-            </div>
-            <Group justify="center">
-              <Button size="md" component={Link} to={"/"}>
-                Go back to the home page
-              </Button>
-            </Group>
+    <Flex p={"160px"} justify={"center"} h={"100vh"}>
+      <Flex pos={"relative"} align={"center"}>
+        <Box
+          pos={"absolute"}
+          inset={0}
+          opacity={0.75}
+          c={"dark"}
+          component={Illustration}
+        />
+        <Flex
+          style={{ textAlign: "center" }}
+          pos={"relative"}
+          gap={"md"}
+          direction={"column"}
+        >
+          <Title ta={"center"}>Womp Womp.</Title>
+          <Flex direction={"column"} wrap={"wrap"} align={"center"}>
+            <Text c="dimmed" size="lg" ta="center">
+              Unfortunately, this is only a 404 page. You may have mistyped the
+              address, or the page has been moved to another URL.
+            </Text>
           </Flex>
-        </div>
-      </div>
-    </Container>
+          <Group justify="center">
+            <Button size="md" component={Link} to={"/"}>
+              Go back to the home page
+            </Button>
+          </Group>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
