eb191fedb3dad4f9544bab970cf8f80293b988a4
Tahmid Ahmed <tahmid@tahmid.io>
2025-03-16 02:40:22 -0400

    Abstract submissions per user into separate routes (DESC)
    Querying submissions and attaching an user object from within that SQL query has been very brittle and led to very weird SQL queries (especially when trying to implement filters on the question). As a result, this is now separated into two endpoints, fetching a user profile, and fetching user submissions. These queries are also moved out of the SubmissionController as they tie more to finding data on any user, compared to the actual heavier logic (that is more tied to Leetcode.com) in SubmissionController.

---
+++ src/main/java/com/patina/codebloom/api/user/UserController.java
@@ -0,0 +1,90 @@
+package com.patina.codebloom.api.user;
+
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.patina.codebloom.common.db.models.question.Question;
+import com.patina.codebloom.common.db.models.user.User;
+import com.patina.codebloom.common.db.repos.question.QuestionRepository;
+import com.patina.codebloom.common.db.repos.user.UserRepository;
+import com.patina.codebloom.common.dto.ApiResponder;
+import com.patina.codebloom.common.dto.autogen.UnsafeGenericFailureResponse;
+import com.patina.codebloom.common.lag.FakeLag;
+import com.patina.codebloom.common.page.Page;
+import com.patina.codebloom.common.security.Protector;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@Tag(name = "General user routes", description = "This controller is responsible for handling general user data, such as user profile, user submissions, and more.")
+@RequestMapping("/api/user")
+public class UserController {
+    /* Page size for submissions */
+    private static final int SUBMISSIONS_PAGE_SIZE = 20;
+
+    private final Protector protector;
+    private final QuestionRepository questionRepository;
+    private final UserRepository userRepository;
+
+    public UserController(final Protector protector, final QuestionRepository questionRepository, final UserRepository userRepository) {
+        this.protector = protector;
+        this.questionRepository = questionRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Operation(summary = "Public route that returns the given user's profile", description = """
+                        Unprotected endpoint that returns the user profile of the user ID that is passed to the endpoint's path.
+                    """, responses = {
+            @ApiResponse(responseCode = "404", description = "User profile has not been found", content = @Content(schema = @Schema(implementation = UnsafeGenericFailureResponse.class))),
+            @ApiResponse(responseCode = "200", description = "User profile has been found")
+    })
+    @GetMapping("{userId}/profile")
+    public ResponseEntity<ApiResponder<User>> getUserProfileByUserId(final HttpServletRequest request, @PathVariable final String userId) {
+        User user = userRepository.getUserById(userId);
+
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Failed to find user profile.");
+        }
+
+        return ResponseEntity.ok().body(ApiResponder.success("User profile found!", user));
+    }
+
+    @Operation(summary = "Returns a list of the questions successfully submitted by the user.", description = """
+                    Protected endpoint that returns the list of questions completed by the user.
+                    These questions are guaranteed to be completed by the user.
+                    """, responses = { @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "401", description = "Not authenticated", content = @Content(schema = @Schema(implementation = UnsafeGenericFailureResponse.class))) })
+    @GetMapping("{userId}/submissions")
+    public ResponseEntity<ApiResponder<Page<ArrayList<Question>>>> getAllQuestionsForUser(final HttpServletRequest request,
+                    @Parameter(description = "Page index", example = "1") @RequestParam(required = false, defaultValue = "1") final int page,
+                    @Parameter(description = "Question Title", example = "Two") @RequestParam(required = false, defaultValue = "") final String query,
+                    @PathVariable final String userId) {
+        FakeLag.sleep(250);
+
+        protector.validateSession(request);
+
+        ArrayList<Question> questions = questionRepository.getQuestionsByUserId(userId, page, SUBMISSIONS_PAGE_SIZE, query);
+
+        int totalQuestions = questionRepository.getQuestionCountByUserId(userId, query);
+        int totalPages = (int) Math.ceil((double) totalQuestions / SUBMISSIONS_PAGE_SIZE);
+        boolean hasNextPage = page < totalPages;
+
+        Page<ArrayList<Question>> createdPage = new Page<>(hasNextPage, questions, totalPages, SUBMISSIONS_PAGE_SIZE);
+
+        return ResponseEntity.ok().body(ApiResponder.success("All questions have been fetched!", createdPage));
+    }
+}
