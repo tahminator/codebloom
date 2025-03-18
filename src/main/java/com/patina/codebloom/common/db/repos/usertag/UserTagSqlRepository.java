d55f76fc0e5aff6cab6ce05e5336ccd2a7683a64
Tahmid Ahmed <tahmid@tahmid.io>
2025-03-18 15:53:27 -0400

    Implement user tags (DESC)
    Implemented User Tags which is a table that is derived from the User table. All User Tags are automatically jointed to the User models from the SQL repositories, but in order to update User Tags, you have to use the UserTagRepository. These tags will allow us to mark/filter for Patina members, but we can also trivially add support for universities with college emails. This hasn't actually been implemented into the core logic yet, just the implementation.

---
+++ src/main/java/com/patina/codebloom/common/db/repos/usertag/UserTagSqlRepository.java
@@ -0,0 +1,194 @@
+package com.patina.codebloom.common.db.repos.usertag;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import com.patina.codebloom.common.db.DbConnection;
+import com.patina.codebloom.common.db.models.usertag.Tag;
+import com.patina.codebloom.common.db.models.usertag.UserTag;
+
+@Component
+public class UserTagSqlRepository implements UserTagRepository {
+    private Connection conn;
+
+    public UserTagSqlRepository(final DbConnection dbConnection) {
+        this.conn = dbConnection.getConn();
+    }
+
+    private UserTag parseResultSetToTag(final ResultSet rs) throws SQLException {
+        var id = rs.getString("id");
+        var createdAt = rs.getTimestamp("createdAt").toLocalDateTime();
+        var userId = rs.getString("userId");
+        var tag = Tag.valueOf(rs.getString("tag"));
+        return new UserTag(id, createdAt, userId, tag);
+    }
+
+    @Override
+    public UserTag findTagByTagId(final String tagId) {
+        String sql = """
+                        SELECT
+                            id,
+                            createdAt,
+                            "userId",
+                            tag
+                        FROM
+                            "UserTag"
+                        WHERE
+                            id = ?
+                                """;
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, UUID.fromString(tagId));
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return parseResultSetToTag(rs);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to fetch user tag by tag ID", e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public UserTag findTagByUserIdAndTag(final String userId, final Tag tag) {
+        String sql = """
+                        SELECT
+                            id,
+                            "createdAt",
+                            userId,
+                            tag
+                        FROM
+                            "UserTag"
+                        WHERE
+                            tag = ?
+                            AND
+                            "userId" = ?
+                                """;
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, tag.name(), java.sql.Types.OTHER);
+            stmt.setObject(2, UUID.fromString(userId));
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return parseResultSetToTag(rs);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to fetch user tag by user ID and tag", e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public ArrayList<UserTag> findTagsByUserId(final String userId) {
+        ArrayList<UserTag> tags = new ArrayList<>();
+        String sql = """
+                        SELECT
+                            id,
+                            "createdAt",
+                            "userId",
+                            tag
+                        FROM
+                            "UserTag"
+                        WHERE
+                            "userId" = ?
+                                """;
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, UUID.fromString(userId));
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    var tag = parseResultSetToTag(rs);
+                    if (tag != null) {
+                        tags.add(tag);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to fetch user tag by user ID and tag", e);
+        }
+
+        return tags;
+    }
+
+    @Override
+    public String createTagByUserId(final String userId, final Tag tag) {
+        UUID userTagId = UUID.randomUUID();
+        String sql = """
+                            INSERT INTO "UserTag"
+                                (id, "userId", tag)
+                            VALUES
+                                (?, ?, ?)
+                        """;
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, userTagId);
+            stmt.setObject(2, UUID.fromString(userId));
+            stmt.setObject(3, tag.name(), java.sql.Types.OTHER);
+
+            int rowsAffected = stmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                return userTagId.toString();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to create user tag by user ID", e);
+        }
+
+        return null;
+
+    }
+
+    @Override
+    public boolean deleteTagByTagId(final String tagId) {
+        String sql = """
+                        DELETE FROM
+                            "UserTag"
+                        WHERE
+                            id = ?
+                        """;
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, UUID.fromString(tagId));
+            int rowsAffected = stmt.executeUpdate();
+
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to delete tag by tag ID", e);
+        }
+
+    }
+
+    @Override
+    public boolean deleteTagByUserIdAndTag(final String userId, final Tag tag) {
+        String sql = """
+                            DELETE FROM
+                                "UserTag"
+                            WHERE
+                                "userId" = ?
+                                AND
+                                tag = ?
+                        """;
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, UUID.fromString(userId));
+            stmt.setObject(2, tag.name(), java.sql.Types.OTHER);
+
+            int rowsAffected = stmt.executeUpdate();
+
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to delete tag by user ID and tag", e);
+        }
+    }
+
+}
