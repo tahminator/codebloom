fe93eb34037bcda2d8fd599f59f946c24faa9cee
Tahmid Ahmed <tahmid@tahmid.io>
2025-03-19 05:51:25 -0400

    Implemented new leaderboard logic/route to frontend (DESC)
    Implemented the slightly different leaderboard routes, as well as remove authentication for all leaderboard and submission routes on the backend and frontend. Some types on the frontend are not updated to better match the DTO. TODO: Abstract some of these hooks and delete some of the hooks that are not used.

--- js/src/app/leaderboard/Leaderboard.page.tsx
+++ js/src/app/leaderboard/Leaderboard.page.tsx
@@ -1,34 +1,8 @@
 import Leaderboard from "@/app/leaderboard/_components/Leaderboard";
-import { useAuthQuery } from "@/app/login/hooks";
 import { Footer } from "@/components/ui/footer/Footer";
 import Header from "@/components/ui/header/Header";
-import Toast from "@/components/ui/toast/Toast";
-import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
-import { Loader } from "@mantine/core";
 
 export default function LeaderboardPage() {
-  const { data, status } = useAuthQuery();
-
-  if (status === "pending") {
-    return (
-      <div className="flex flex-col items-center justify-center w-screen h-screen">
-        <Loader />
-      </div>
-    );
-  }
-
-  if (status === "error") {
-    return <Toast message="Sorry, something went wrong." />;
-  }
-
-  const authenticated = !!data.user && !!data.session;
-
-  if (!authenticated) {
-    return (
-      <ToastWithRedirect to="/login" message="You are not authenticated!" />
-    );
-  }
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
--- js/src/app/submission/[submissionId]/SubmissionDetails.page.tsx
+++ js/src/app/submission/[submissionId]/SubmissionDetails.page.tsx
@@ -1,39 +1,8 @@
-import { useAuthQuery } from "@/app/login/hooks";
 import SubmissionDetailsContent from "@/app/submission/[submissionId]/_components/SubmissionDetailsContent";
-import Toast from "@/components/ui/toast/Toast";
-import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
-import { Flex, Loader } from "@mantine/core";
 import { useParams } from "react-router";
 
 export default function SubmissionDetailsPage() {
   const { submissionId } = useParams();
-  const { data, status } = useAuthQuery();
-
-  if (status === "pending") {
-    return (
-      <Flex
-        direction={"column"}
-        align={"center"}
-        justify={"center"}
-        w={"100vw"}
-        h={"100vh"}
-      >
-        <Loader />
-      </Flex>
-    );
-  }
-
-  if (status === "error") {
-    return <Toast message="Sorry, something went wrong." />;
-  }
-
-  const authenticated = !!data.user && !!data.session;
-
-  if (!authenticated) {
-    return (
-      <ToastWithRedirect to="/login" message="You are not authenticated!" />
-    );
-  }
 
   return <SubmissionDetailsContent submissionId={submissionId} />;
 }
