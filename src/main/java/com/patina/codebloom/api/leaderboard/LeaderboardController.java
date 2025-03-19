e9efb1c3b322e0f83a02690a1a558c93c0f73975
Tahmid Ahmed <tahmid@tahmid.io>
2025-03-19 18:26:17 -0400

    Add patina filter to leaderboard users route (DESC)
    Added the ability to filter based off Patina users on the fetch users from leaderboard route

--- src/main/java/com/patina/codebloom/api/leaderboard/LeaderboardController.java
+++ src/main/java/com/patina/codebloom/api/leaderboard/LeaderboardController.java
@@ -59,14 +59,15 @@ public class LeaderboardController {
             @ApiResponse(responseCode = "401", description = "Not authenticated", content = @Content(schema = @Schema(implementation = UnsafeGenericFailureResponse.class))) })
     public ResponseEntity<ApiResponder<Page<ArrayList<UserWithScore>>>> getCurrentLeaderboardUsers(final HttpServletRequest request,
                     @Parameter(description = "Page index", example = "1") @RequestParam(required = false, defaultValue = "1") final int page,
-                    @Parameter(description = "Page size (maximum of " + LEADERBOARD_PAGE_SIZE) @RequestParam(required = false, defaultValue = "" + LEADERBOARD_PAGE_SIZE) final int pageSize) {
+                    @Parameter(description = "Page size (maximum of " + LEADERBOARD_PAGE_SIZE) @RequestParam(required = false, defaultValue = "" + LEADERBOARD_PAGE_SIZE) final int pageSize,
+                    @Parameter(description = "Filter for Patina users") @RequestParam(required = false, defaultValue = "false") final boolean patina) {
         FakeLag.sleep(800);
 
         final int parsedPageSize = Math.min(pageSize, LEADERBOARD_PAGE_SIZE);
 
-        ArrayList<UserWithScore> leaderboardData = leaderboardRepository.getRecentLeaderboardUsers(page, parsedPageSize);
+        ArrayList<UserWithScore> leaderboardData = leaderboardRepository.getRecentLeaderboardUsers(page, parsedPageSize, patina);
 
-        int totalUsers = leaderboardRepository.getRecentLeaderboardUserCount();
+        int totalUsers = leaderboardRepository.getRecentLeaderboardUserCount(patina);
         int totalPages = (int) Math.ceil((double) totalUsers / parsedPageSize);
         boolean hasNextPage = page < totalPages;
 
