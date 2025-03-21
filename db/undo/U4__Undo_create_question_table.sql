c5facc4f6f3f966a063234c1af9f03e4bd9bad00
Tahmid Ahmed <tahmid@tahmid.io>
2025-03-21 05:45:34 -0400

    Add mock data + fix bug (DESC)
    Created a repeatable Flyway script to generate mock data. Deleted undo folder as Flyway kept complaining and it isn't used anyways due to Flyway CE. Fixed a bug with the submission handler having the wrong order of values causing null pointer exceptions.

--- db/undo/U4__Undo_create_question_table.sql
+++
@@ -1,8 +0,0 @@
-DO $$ BEGIN
-    DROP TYPE question_difficulty;
-EXCEPTION 
-    -- Don't know how to handle the specific exception, but this is good enough.
-    WHEN others THEN null;
-END $$;
-
-DROP TABLE IF EXISTS "Question";
