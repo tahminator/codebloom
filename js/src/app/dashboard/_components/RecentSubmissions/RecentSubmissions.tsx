85dff2b903c5dd086f08948aa3b8f1cce67fb216
Alfardil Alam <fardilfayed@gmail.com>
2025-03-28 16:24:23 -0400

    changed logic to hard fetch 20 questions, but we only use the first 5 for the dasboard page

--- js/src/app/dashboard/_components/RecentSubmissions/RecentSubmissions.tsx
+++ js/src/app/dashboard/_components/RecentSubmissions/RecentSubmissions.tsx
@@ -4,7 +4,7 @@ import { Badge, Box, Button, Card, Flex, Text, Title } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 export default function RecentSubmissions({ userId }: { userId: string }) {
-  const { data, status } = useUserSubmissionsQuery({ userId, pageSize: 5 });
+  const { data, status } = useUserSubmissionsQuery({ userId });
 
   if (status === "pending") {
     return <RecentSubmissionsSkeleton />;
@@ -47,6 +47,7 @@ export default function RecentSubmissions({ userId }: { userId: string }) {
   }
 
   const questions = data.data.data;
+  const dashboardQuestions = questions.slice(0, 5);
 
   if (!questions.length) {
     return (
@@ -76,7 +77,7 @@ export default function RecentSubmissions({ userId }: { userId: string }) {
         </Button>
       </Flex>
 
-      {questions.map((q, idx) => {
+      {dashboardQuestions.map((q, idx) => {
         const badgeColor = (() => {
           if (q.questionDifficulty === "Easy") {
             return undefined;
