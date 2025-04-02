47cb4fdebd62aa0dd2840f731ca5922261e02b8f
Tahmid Ahmed <tahmid@tahmid.io>
2025-04-02 19:23:59 -0400

    Custom hook useCountdown (DESC)
    Refactored the countdown logic for the submission button to be a custom hook called useCountdown, which helps us remove unnecessary useEffects inside of components. Furthermore, we will re-use this hook for other things that may need countdown, such as starting a new leaderboard

---
+++ js/src/lib/hooks/useCountdown.ts
@@ -0,0 +1,32 @@
+import { useCallback, useEffect, useState } from "react";
+
+/**
+ * A custom React hook that counts a number down to 0.
+ *
+ * @param start - The initial starting value (in seconds). The default is 60 seconds.
+ * @param interval - How frequently to count down (in milliseconds). The default value is 1000, so it counts down every second.
+ *
+ * Returns a stateful value and a function to reset the countdown to a new value.
+ */
+export default function useCountdown(start = 60, interval = 1000) {
+  const [countdown, setCountdown] = useState(start);
+
+  useEffect(() => {
+    let timeoutId: number;
+    if (countdown > 0) {
+      timeoutId = setTimeout(() => {
+        setCountdown((prev) => prev - interval / 1000);
+      }, interval);
+    }
+    return () => clearTimeout(timeoutId);
+  }, [countdown, interval]);
+
+  const reset = useCallback(
+    (newValue: number) => {
+      setCountdown(newValue);
+    },
+    [setCountdown],
+  );
+
+  return [countdown, reset] as [number, (newValue: number) => void];
+}
