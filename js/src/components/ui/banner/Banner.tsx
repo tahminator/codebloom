b1d401103f4fb708c4e8953d0ae0033357edbf97
Tahmid Ahmed <tahmid@tahmid.io>
2025-04-03 15:52:46 -0400

    Add a banner to countdown new leaderboard

---
+++ js/src/components/ui/banner/Banner.tsx
@@ -0,0 +1,41 @@
+import PrettyCounter from "@/components/ui/pretty-counter/PrettyCounter";
+import useCountdown from "@/lib/hooks/useCountdown";
+import { Flex, Text, Transition } from "@mantine/core";
+import { useMounted } from "@mantine/hooks";
+
+export default function Banner({
+  message,
+  counter,
+}: {
+  message: string;
+  counter?: Date;
+}) {
+  const mounted = useMounted();
+
+  const time = (() => {
+    if (!counter) {
+      return 0;
+    }
+
+    return Math.floor((counter.getTime() - Date.now()) / 1000);
+  })();
+  const [countdown] = useCountdown(time);
+
+  return (
+    <Transition keepMounted mounted={mounted} transition={"scale-y"}>
+      {(style) => (
+        <Flex
+          bg={"green"}
+          ta={"center"}
+          direction={"row"}
+          style={style}
+          justify={"center"}
+          gap={"sm"}
+        >
+          <Text size={"lg"}>{message}</Text>
+          {counter && <PrettyCounter size={"lg"} time={countdown} />}
+        </Flex>
+      )}
+    </Transition>
+  );
+}
