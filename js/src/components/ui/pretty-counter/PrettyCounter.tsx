6f8916a75e93011c213b9b24b6829792fe39be7d
Tahmid Ahmed <tahmid@tahmid.io>
2025-04-03 15:52:46 -0400

    Add a banner to countdown new leaderboard

---
+++ js/src/components/ui/pretty-counter/PrettyCounter.tsx
@@ -0,0 +1,16 @@
+import { Text, TextProps } from "@mantine/core";
+
+const formatCountdown = (seconds: number): string => {
+  const days = Math.floor(seconds / (24 * 60 * 60));
+  const hours = Math.floor((seconds % (24 * 60 * 60)) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+
+  return `${days}d ${hours}h ${minutes}m ${secs}s`;
+};
+
+type CounterProps = TextProps & { time: number };
+
+export default function PrettyCounter({ time, ...props }: CounterProps) {
+  return <Text {...props}>{formatCountdown(time)}</Text>;
+}
