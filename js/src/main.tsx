6bdc6d744b3aba5f996048872588f7413e1ff4cc
Tahmid Ahmed <tahmid@tahmid.io>
2025-04-03 15:52:46 -0400

    Add a banner to countdown new leaderboard

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -1,22 +1,27 @@
+import Banner from "@/components/ui/banner/Banner";
 import ReactQueryProvider from "@/lib/queryProvider";
 import { router } from "@/lib/router";
+import "@mantine/core/styles.css";
 import { themeOverride } from "@/lib/theme";
 import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
-import { Notifications } from "@mantine/notifications";
 import "@mantine/notifications/styles.css";
+import { Notifications } from "@mantine/notifications";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 
 import "./index.css";
 
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReactQueryProvider>
       <MantineProvider theme={themeOverride} forceColorScheme={"dark"}>
+        <Banner
+          message="The FIRST official leaderboard for the month of April will be starting very soon!"
+          counter={new Date("2025-04-09T18:00:00-04:00")}
+        />
         <RouterProvider router={router} />
         <Notifications />
       </MantineProvider>
