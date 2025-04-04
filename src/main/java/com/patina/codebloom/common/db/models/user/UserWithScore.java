4f6e1774436301a1c67e2f850ce7f7eb2df858ad
Alisha Zaman <az2924@nyu.edu>
2025-04-03 23:43:58 -0400

    Add an admin boolean to the user table

--- src/main/java/com/patina/codebloom/common/db/models/user/UserWithScore.java
+++ src/main/java/com/patina/codebloom/common/db/models/user/UserWithScore.java
@@ -7,8 +7,9 @@ import com.patina.codebloom.common.db.models.usertag.UserTag;
 public class UserWithScore extends User {
     private int totalScore;
 
-    public UserWithScore(final String id, final String discordId, final String discordName, final String leetcodeUsername, final String nickname, final int totalScore, final ArrayList<UserTag> tags) {
-        super(id, discordId, discordName, leetcodeUsername, nickname, tags);
+    public UserWithScore(final String id, final String discordId, final String discordName, final String leetcodeUsername, final String nickname, final Boolean admin, final int totalScore,
+                    final ArrayList<UserTag> tags) {
+        super(id, discordId, discordName, leetcodeUsername, nickname, admin, tags);
         this.totalScore = totalScore;
     }
 
