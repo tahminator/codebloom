6455dad72661228a5a5eb264bc321e1bd16f4891
Angela Yu <angelayu0530@gmail.com>
2025-04-04 14:21:36 -0400

    white space formatting fixes

--- src/main/java/com/patina/codebloom/common/security/Protector.java
+++ src/main/java/com/patina/codebloom/common/security/Protector.java
@@ -55,9 +55,10 @@ public class Protector {
 
         throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
     }
-    public AuthenticationObject validateAdminSession(final HttpServletRequest request){
+
+    public AuthenticationObject validateAdminSession(final HttpServletRequest request) {
         AuthenticationObject admin = validateSession(request);
-        if(!admin.getUser().isAdmin()){
+        if (!admin.getUser().isAdmin()) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
         }
         return admin;
