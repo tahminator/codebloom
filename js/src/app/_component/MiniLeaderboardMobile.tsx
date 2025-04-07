deca4024dd8f586ee84dba9fb81fdeb1a6ef1581
Alfardil Alam <fardilfayed@gmail.com>
2025-04-07 02:07:44 -0400

    added blur view to mobile version

--- js/src/app/_component/MiniLeaderboardMobile.tsx
+++ js/src/app/_component/MiniLeaderboardMobile.tsx
@@ -3,13 +3,13 @@ import LeaderboardMetadata from "@/app/leaderboard/_components/LeaderboardMetada
 import LeaderboardCard from "@/components/ui/LeaderboardCard";
 import Toast from "@/components/ui/toast/Toast";
 import { useCurrentLeaderboardUsersQuery } from "@/lib/api/queries/leaderboard";
-import { Button, SegmentedControl, Table } from "@mantine/core";
+import { Button, Overlay, SegmentedControl, Table } from "@mantine/core";
 import { FaDiscord } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 import { Link } from "react-router-dom";
 
 export default function MiniLeaderboardMobile() {
-  const { data, status, patina, togglePatina } =
+  const { data, status, patina, togglePatina, isPlaceholderData } =
     useCurrentLeaderboardUsersQuery({ pageSize: 5 });
 
   if (status === "pending") {
@@ -45,42 +45,86 @@ export default function MiniLeaderboardMobile() {
         ]}
         onChange={togglePatina}
       />
-      <div
-        className="flex flex-col sm:flex-row items-center sm:items-end justify-center gap-4"
-        style={{ marginBottom: "2rem", marginTop: "1rem" }}
-      >
-        {first && (
-          <LeaderboardCard
-            placeString={"First"}
-            discordName={first.discordName}
-            leetcodeUsername={first.leetcodeUsername}
-            nickname={first.nickname}
-            totalScore={first.totalScore}
-            width={"300px"}
-            userId={first.id}
+      <div style={{ position: "relative" }}>
+        {isPlaceholderData && (
+          <Overlay
+            zIndex={1000}
+            backgroundOpacity={0.55}
+            blur={10}
+            radius={"md"}
           />
         )}
-        {second && (
-          <LeaderboardCard
-            placeString={"Second"}
-            discordName={second.discordName}
-            leetcodeUsername={second.leetcodeUsername}
-            nickname={second.nickname}
-            totalScore={second.totalScore}
-            width={"300px"}
-            userId={second.id}
-          />
-        )}
-        {third && (
-          <LeaderboardCard
-            placeString={"Third"}
-            discordName={third.discordName}
-            leetcodeUsername={third.leetcodeUsername}
-            nickname={third.nickname}
-            totalScore={third.totalScore}
-            width={"300px"}
-            userId={third.id}
-          />
+        <div
+          className="flex flex-col sm:flex-row items-center sm:items-end justify-center gap-4"
+          style={{ marginBottom: "2rem", marginTop: "1rem" }}
+        >
+          {first && (
+            <LeaderboardCard
+              placeString={"First"}
+              discordName={first.discordName}
+              leetcodeUsername={first.leetcodeUsername}
+              nickname={first.nickname}
+              totalScore={first.totalScore}
+              width={"300px"}
+              userId={first.id}
+            />
+          )}
+          {second && (
+            <LeaderboardCard
+              placeString={"Second"}
+              discordName={second.discordName}
+              leetcodeUsername={second.leetcodeUsername}
+              nickname={second.nickname}
+              totalScore={second.totalScore}
+              width={"300px"}
+              userId={second.id}
+            />
+          )}
+          {third && (
+            <LeaderboardCard
+              placeString={"Third"}
+              discordName={third.discordName}
+              leetcodeUsername={third.leetcodeUsername}
+              nickname={third.nickname}
+              totalScore={third.totalScore}
+              width={"300px"}
+              userId={third.id}
+            />
+          )}
+        </div>
+        {leaderboardData.data.length > 3 && (
+          <Table>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th></Table.Th>
+                <Table.Th>Name</Table.Th>
+                <Table.Th>Total Points</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+              {leaderboardData.data.map((entry, index) => {
+                if ([0, 1, 2].includes(index)) return null;
+                return (
+                  <Table.Tr key={entry.discordName}>
+                    <Table.Td>{index + 1}</Table.Td>
+                    <Table.Td>
+                      <div style={{ display: "flex", flexDirection: "column" }}>
+                        <span style={{ fontSize: "18px", lineHeight: "28px" }}>
+                          <FaDiscord style={{ display: "inline" }} />{" "}
+                          {entry.discordName}
+                        </span>
+                        <span>
+                          <SiLeetcode style={{ display: "inline" }} />{" "}
+                          {entry.leetcodeUsername}
+                        </span>
+                      </div>
+                    </Table.Td>
+                    <Table.Td>{entry.totalScore}</Table.Td>
+                  </Table.Tr>
+                );
+              })}
+            </Table.Tbody>
+          </Table>
         )}
       </div>
       {leaderboardData.data.length > 3 && (
