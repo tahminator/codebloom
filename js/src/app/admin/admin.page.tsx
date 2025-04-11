d9518d4eda964fa48a0fccd906645ba0cfcf7d06
Angela Yu <angelayu0530@gmail.com>
2025-04-11 12:36:44 -0400

    created an admin page

---
+++ js/src/app/admin/admin.page.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "@/components/ui/footer/Footer";
+import Header from "@/components/ui/header/Header"
+
+import AdminIndex from "./_components/admin";
+
+
+export default function AdminPage() {
+  return (
+    <>
+      <Header />
+      <AdminIndex />
+      <Footer />
+    </>
+  );
+}
