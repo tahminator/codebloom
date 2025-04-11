b1386268610a4edcdd3470ae4c2c50e9fb35f9e5
Angela Yu <angelayu0530@gmail.com>
2025-04-11 12:36:44 -0400

    created an admin page

---
+++ js/src/app/admin/_components/admin.tsx
@@ -0,0 +1,27 @@
+import { useAuthQuery } from "@/lib/api/queries/auth";
+import { Title } from "@mantine/core"; 
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export default function AdminIndex () {
+    const {data, status} = useAuthQuery();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (status === "success" && !data.isAdmin) {
+            navigate("/");
+        }
+    }, [data, status, navigate]);
+
+    if (status == "pending" || !data?.isAdmin){
+       return null;
+    }
+    return (
+        <div>
+            <Title order={1} ta="center">
+                Admin Page
+            </Title>
+        </div>
+    )
+}
