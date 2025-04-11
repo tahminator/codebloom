c5736c8383be68fa81340a71d9a11626c81f1c5b
Angela Yu <angelayu0530@gmail.com>
2025-04-11 12:43:13 -0400

    formatting fixes

--- js/src/app/admin/admin.page.tsx
+++ js/src/app/admin/admin.page.tsx
@@ -1,8 +1,7 @@
 import { Footer } from "@/components/ui/footer/Footer";
-import Header from "@/components/ui/header/Header"
+import Header from "@/components/ui/header/Header";
 
 import AdminIndex from "./_components/admin";
-
 
 export default function AdminPage() {
   return (
