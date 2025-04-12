f7940ee3f18bd3422f15856602165d94225a55da
Angela Yu <angelayu0530@gmail.com>
2025-04-12 01:20:34 -0400

    prettier fixes

--- js/src/app/admin/_components/admin.tsx
+++ js/src/app/admin/_components/admin.tsx
@@ -5,20 +5,25 @@ import { Loader, Title } from "@mantine/core";
 
 export default function AdminIndex() {
   const { data, status } = useAuthQuery();
-  if(status == "pending"){
+  if (status == "pending") {
     return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
         <Loader />
       </div>
     );
   }
-  if(status == "error"){
-    return <Toast message = {"Something went wrong."} />;
+  if (status == "error") {
+    return <Toast message={"Something went wrong."} />;
   }
-  const {isAdmin} = data;
-    if(!isAdmin){
-        return <ToastWithRedirect message = {"You are not authorized to view this page."} to = "/" />;
-        }
+  const { isAdmin } = data;
+  if (!isAdmin) {
+    return (
+      <ToastWithRedirect
+        message={"You are not authorized to view this page."}
+        to="/"
+      />
+    );
+  }
   return (
     <div>
       <Title order={1} ta="center">
