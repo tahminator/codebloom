dbdeebee64440178b05a33cde188f5179d064213
Angela Yu <angelayu0530@gmail.com>
2025-04-12 01:18:30 -0400

    fixed formatting

--- js/src/app/admin/_components/admin.tsx
+++ js/src/app/admin/_components/admin.tsx
@@ -1,21 +1,24 @@
+import Toast from "@/components/ui/toast/Toast";
+import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
 import { useAuthQuery } from "@/lib/api/queries/auth";
-import { Title } from "@mantine/core";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Loader, Title } from "@mantine/core";
 
 export default function AdminIndex() {
   const { data, status } = useAuthQuery();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (status === "success" && !data.isAdmin) {
-      navigate("/");
-    }
-  }, [data, status, navigate]);
-
-  if (status == "pending" || !data?.isAdmin) {
-    return null;
+  if(status == "pending"){
+    return (
+        <div className="flex flex-col items-center justify-center w-screen h-screen">
+        <Loader />
+      </div>
+    );
   }
+  if(status == "error"){
+    return <Toast message = {"Something went wrong."} />;
+  }
+  const {isAdmin} = data;
+    if(!isAdmin){
+        return <ToastWithRedirect message = {"You are not authorized to view this page."} to = "/" />;
+        }
   return (
     <div>
       <Title order={1} ta="center">
