c3e296936e4f6454ff0ff97233a2ba90f6df0df3
Angela Yu <angelayu0530@gmail.com>
2025-04-13 18:35:44 -0400

    fixed admin-button using GoToAdminButton

---
+++ js/src/components/ui/admin-button/AdminButton.tsx
@@ -0,0 +1,24 @@
+import { useAuthQuery } from "@/lib/api/queries/auth";
+import { Button } from "@mantine/core";
+
+export function GotoAdminPageButton() {
+    const { data, status } = useAuthQuery();
+    if (status !== "success") {
+        return null;
+    }
+    if (!data.isAdmin) {
+        return null;
+    }
+    return (
+        <Button
+            component="a"
+            href="/admin"
+            mt={0}
+            size="xs"
+            variant="outline"
+            style={{}}
+        >
+            Admin
+        </Button>
+    );
+}
