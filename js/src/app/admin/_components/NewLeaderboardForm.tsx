7afbfcbe40005f2b7d52b8f312957471c6175feb
Angela Yu <angelayu0530@gmail.com>
2025-04-29 15:18:04 -0400

    prettier fixes

--- js/src/app/admin/_components/NewLeaderboardForm.tsx
+++ js/src/app/admin/_components/NewLeaderboardForm.tsx
@@ -1,23 +1,24 @@
-import { Modal, Title } from '@mantine/core';
-import { useState } from 'react';
-
-
+import { Modal, Title } from "@mantine/core";
+import { useState } from "react";
 
 function NewLeaderboardForm() {
-    const [isModalOpen, setModalOpen] = useState(false);
-    const toggleModal = () => {
-        setModalOpen(prev => !prev);
-    };
+  const [isModalOpen, setModalOpen] = useState(false);
+  const toggleModal = () => {
+    setModalOpen((prev) => !prev);
+  };
 
-    return (
-        <div>
-            <button onClick={toggleModal}>Toggle Modal</button>
-            <Modal opened={isModalOpen} onClose={toggleModal} >
-            < Title order={6} ta="center"> Leaderboard (WIP) </Title>
-                <div></div>
-            </Modal>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={toggleModal}>Toggle Modal</button>
+      <Modal opened={isModalOpen} onClose={toggleModal}>
+        <Title order={6} ta="center">
+          {" "}
+          Leaderboard (WIP){" "}
+        </Title>
+        <div></div>
+      </Modal>
+    </div>
+  );
 }
 
 export default NewLeaderboardForm;
