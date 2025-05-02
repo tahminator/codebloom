74277318869bc1d2fa17e684579ea0f33f07fddc
Alisha Zaman <az2924@nyu.edu>
2025-05-01 23:06:03 -0400

    Added banner stating expiration of April leaderboard.

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -1,15 +1,16 @@
+import Banner from "@/components/ui/banner/Banner";
 import ReactQueryProvider from "@/lib/queryProvider";
-import { router } from "@/lib/router";
 import "@mantine/core/styles.css";
+import { router } from "@/lib/router";
 import { themeOverride } from "@/lib/theme";
-import { MantineProvider } from "@mantine/core";
 import "@mantine/notifications/styles.css";
+import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { StrictMode } from "react";
 
 import "./index.css";
 
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
@@ -17,10 +18,10 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReactQueryProvider>
       <MantineProvider theme={themeOverride} forceColorScheme={"dark"}>
-        {/* <Banner */}
-        {/*   message="The FIRST official leaderboard for the month of April will be starting very soon!" */}
-        {/*   counter={new Date("2025-04-09T18:00:00-04:00")} */}
-        {/* /> */}
+        <Banner
+          message="The FIRST official leaderboard for the month of April will be ending very soon!"
+          counter={new Date("2025-05-02T18:00:00-04:00")}
+        />
         <RouterProvider router={router} />
         <Notifications />
       </MantineProvider>
