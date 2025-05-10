a62038b872e4dc83a41f1edfef69dbd8dd20cd3b
Tahmid Ahmed <tahmid@tahmid.io>
2025-05-10 16:47:24 -0400

    Update leaderboard object models in Java to include shouldExpireBy

--- src/main/java/com/patina/codebloom/common/db/models/leaderboard/LeaderboardWithUsers.java
+++ src/main/java/com/patina/codebloom/common/db/models/leaderboard/LeaderboardWithUsers.java
@@ -8,14 +8,14 @@ import com.patina.codebloom.common.db.models.user.UserWithScore;
 public class LeaderboardWithUsers extends Leaderboard {
     private ArrayList<UserWithScore> users;
 
-    public LeaderboardWithUsers(final String name, final LocalDateTime createdAt, final ArrayList<UserWithScore> users) {
-        super(name, createdAt);
+    public LeaderboardWithUsers(final String name, final LocalDateTime createdAt, final ArrayList<UserWithScore> users, final LocalDateTime shouldExpireBy) {
+        super(name, createdAt, shouldExpireBy);
         this.users = users;
     }
 
     public LeaderboardWithUsers(final String id, final String name, final LocalDateTime createdAt, final LocalDateTime deletedAt,
-            final ArrayList<UserWithScore> users) {
-        super(id, name, createdAt, deletedAt);
+                    final ArrayList<UserWithScore> users, final LocalDateTime shouldExpireBy) {
+        super(id, name, createdAt, deletedAt, shouldExpireBy);
         this.users = users;
     }
 
