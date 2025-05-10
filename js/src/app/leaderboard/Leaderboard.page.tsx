d88624dd067196797c8373057afadb2930d77699
Tahmid Ahmed <tahmid@tahmid.io>
2025-05-10 19:02:57 -0400

    Separate the metadata from the leaderboard page + update skeletons to reflect changes

--- js/src/app/leaderboard/Leaderboard.page.tsx
+++ js/src/app/leaderboard/Leaderboard.page.tsx
@@ -1,13 +1,18 @@
 import Leaderboard from "@/app/leaderboard/_components/Leaderboard";
+import LeaderboardMetadata from "@/app/leaderboard/_components/LeaderboardMetadata/LeaderboardMetadata";
 import { Footer } from "@/components/ui/footer/Footer";
 import Header from "@/components/ui/header/Header";
+import { Box } from "@mantine/core";
 
 export default function LeaderboardPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex-grow">
-        <Leaderboard />
+        <Box p={"lg"}>
+          <LeaderboardMetadata showClock />
+          <Leaderboard />
+        </Box>
       </div>
       <Footer />
     </div>
