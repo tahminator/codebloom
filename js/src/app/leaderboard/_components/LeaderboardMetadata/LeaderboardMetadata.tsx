f453f37c8e6bcd998b17a7c4ff515e30ea3e8825
Tahmid Ahmed <tahmid@tahmid.io>
2025-05-10 17:29:21 -0400

    Add logic to show timer to LeaderboardMetadata (opt-in prop). Fixed a bug with useCountdown allowing negative values

--- js/src/app/leaderboard/_components/LeaderboardMetadata/LeaderboardMetadata.tsx
+++ js/src/app/leaderboard/_components/LeaderboardMetadata/LeaderboardMetadata.tsx
@@ -1,8 +1,29 @@
+import PrettyCounter from "@/components/ui/pretty-counter/PrettyCounter";
 import { useCurrentLeaderboardMetadataQuery } from "@/lib/api/queries/leaderboard";
+import useCountdown from "@/lib/hooks/useCountdown";
 import { Center, Skeleton, Title } from "@mantine/core";
+import { useEffect } from "react";
 
-export default function LeaderboardMetadata() {
+type LeaderboardMetadataOptions = {
+  showClock?: boolean;
+};
+
+export default function LeaderboardMetadata(
+  props: LeaderboardMetadataOptions = {},
+) {
+  const { showClock = false } = props;
+
   const { data, status } = useCurrentLeaderboardMetadataQuery();
+  const [countdown, reset] = useCountdown(-10);
+
+  useEffect(() => {
+    if (status === "success" && data.success && data.data.shouldExpireBy) {
+      const shouldExpireByDate = new Date(data.data.shouldExpireBy);
+      const expireSeconds = (shouldExpireByDate.getTime() - Date.now()) / 1000;
+      console.log(expireSeconds);
+      reset(expireSeconds);
+    }
+  }, [status, data, reset]);
 
   if (status === "pending") {
     return (
@@ -18,6 +39,19 @@ export default function LeaderboardMetadata() {
         >
           <Skeleton visible>Really long tVal name</Skeleton>
         </Title>
+        {showClock && (
+          <Title
+            order={6}
+            style={{
+              fontSize: "1rem",
+              fontWeight: "bold",
+              marginBottom: "1rem",
+            }}
+            className="text-center sm:text-lg"
+          >
+            <Skeleton visible>Really long tVal name</Skeleton>
+          </Title>
+        )}
       </Center>
     );
   }
@@ -57,8 +91,15 @@ export default function LeaderboardMetadata() {
   const leaderboardData = data.data;
 
   return (
-    <Title order={3} ta={"center"} mb={"xs"}>
-      {leaderboardData.name}
-    </Title>
+    <>
+      <Title order={3} ta={"center"} mb={"xs"}>
+        {leaderboardData.name}
+      </Title>
+      <Title order={6} ta={"center"} mb={"xs"}>
+        {showClock && leaderboardData.shouldExpireBy && countdown > 0 && (
+          <PrettyCounter size={"lg"} time={countdown} />
+        )}
+      </Title>
+    </>
   );
 }
