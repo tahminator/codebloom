49660b6df896062efea25d88c7a5747e3d4a9cbb
Tahmid Ahmed <tahmid@tahmid.io>
2025-05-11 11:18:54 -0400

    Implement optimistic updates and wire the logic back in to the UserAdminList.

--- js/src/lib/api/queries/admin/index.ts
+++ js/src/lib/api/queries/admin/index.ts
@@ -1,3 +1,6 @@
+import { ApiResponse } from "@/lib/api/common/apiResponse";
+import { Page } from "@/lib/api/common/page";
+import { User } from "@/lib/api/types/user";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useToggleAdminMutation = () => {
@@ -5,14 +8,50 @@ export const useToggleAdminMutation = () => {
 
   return useMutation({
     mutationFn: toggleUserAdmin,
-    onMutate: async (newAdminStatus) => {
+    onMutate: async (newData) => {
+      const { userId, toggleTo, metadata } = newData;
       await queryClient.cancelQueries({
-        queryKey: ["submission", "user", "all"],
+        queryKey: ["user", "all", metadata.page, metadata.debouncedQuery],
       });
 
-      const users = queryClient.getQueryData(["submission", "user", "all"]);
+      const previousApiResponse = queryClient.getQueryData([
+        "user",
+        "all",
+        metadata.page,
+        metadata.debouncedQuery,
+      ]) as ApiResponse<Page<User[]>>;
 
-      console.log(users);
+      // Impossible, just handle it to make TS happy and narrow the type.
+      if (!previousApiResponse.success) {
+        return;
+      }
+
+      const newUsers = previousApiResponse.data.data.map((user) =>
+        user.id === userId ? { ...user, admin: toggleTo } : user,
+      ) as User[];
+
+      const newPage = { ...previousApiResponse.data, data: newUsers } as Page<
+        User[]
+      >;
+
+      queryClient.setQueryData(
+        ["user", "all", metadata.page, metadata.debouncedQuery],
+        { ...previousApiResponse, data: newPage },
+      );
+
+      return { previousApiResponse };
+    },
+    onError: (_, newData, ctx) => {
+      const { metadata } = newData;
+      queryClient.setQueryData(
+        ["user", "all", metadata.page, metadata.debouncedQuery],
+        { ...ctx?.previousApiResponse },
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["user", "all"],
+      });
     },
   });
 };
@@ -23,14 +62,37 @@ async function toggleUserAdmin({
 }: {
   userId: string;
   toggleTo: boolean;
+  /**
+   * This metadata property isn't used in the fetcher function at all.
+   * Instead, it is used in order to pinpoint exactly what data we will be mutating
+   * in the optimistic update.
+   */
+  metadata: {
+    page: number;
+    debouncedQuery: string;
+  };
 }) {
-  const res = await fetch("/api/admin/user/toggle", {
+  // If you want to test the optimistic updates, uncomment the lines below.
+  //
+  // await new Promise((res, rej) => {
+  //   setTimeout(() => {
+  //     res(-1);
+  //   }, 10000);
+  // });
+  // throw new Error("hi");
+
+  const response = await fetch("/api/admin/user/admin/toggle", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       id: userId,
       toggleTo,
     }),
   });
 
-  return;
+  const json = (await response.json()) as ApiResponse<User>;
+
+  return json;
 }
