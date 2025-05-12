e2995b6f8db55669281d6279862bf249e3bb76fb
Angela Yu <angelayu0530@gmail.com>
2025-05-12 19:14:17 -0400

    added logic for new leaderboard form

--- js/src/app/admin/_components/NewLeaderboardForm.tsx
+++ js/src/app/admin/_components/NewLeaderboardForm.tsx
@@ -1,11 +1,53 @@
-import { Modal, Button } from "@mantine/core";
+import { AdminSchema } from "@/app/admin/_components/types";
+import Toast from "@/components/ui/toast/Toast";
+import { useCreateLeaderboardMutation } from "@/lib/api/queries/admin";
+import { useCurrentLeaderboardMetadataQuery } from "@/lib/api/queries/leaderboard";
+import { Modal, Button, TextInput, Loader } from "@mantine/core";
+import { zodResolver, useForm } from "@mantine/form";
 import { useState } from "react";
+import { z } from "zod";
 
 function NewLeaderboardForm() {
   const [isModalOpen, setModalOpen] = useState(false);
+  const { data, status } = useCurrentLeaderboardMetadataQuery();
+  const { mutate } = useCreateLeaderboardMutation();
+
+  const form = useForm({
+    validate: zodResolver(AdminSchema()),
+    initialValues: {
+      name: "",
+      confirmation: "",
+    },
+  });
+
   const toggleModal = () => {
     setModalOpen((prev) => !prev);
   };
+
+  const onSubmit = (values: z.infer<ReturnType<typeof AdminSchema>>) => {
+    mutate(values.name);
+  };
+
+  if (status === "pending") {
+    return (
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return <Toast message={"Something went wrong."} />;
+  }
+
+  if (!data || !("data" in data) || !data.data) {
+    return <Toast message={"No leaderboard data found."} />;
+  }
+  const currentLeaderboardName = data.data.name;
+
+  const isValid =
+    form.values.confirmation === currentLeaderboardName &&
+    form.values.name !== currentLeaderboardName;
 
   return (
     <div>
@@ -17,7 +59,28 @@ function NewLeaderboardForm() {
         onClose={toggleModal}
         title={"Create New Leaderboard"}
       >
-        <div></div>
+        <form onSubmit={form.onSubmit(onSubmit)}>
+          <TextInput
+            {...form.getInputProps("name")}
+            label="New leaderboard name"
+            error={form.errors.name}
+            mb="sm"
+          />
+          <TextInput
+            {...form.getInputProps("confirmation")}
+            label={`Type "${currentLeaderboardName}" to confirm`}
+            error={form.errors.confirmation}
+          />
+          <Button
+            type="submit"
+            size="xs"
+            mt="sm"
+            variant="outline"
+            disabled={!isValid}
+          >
+            Submit
+          </Button>
+        </form>
       </Modal>
     </div>
   );
