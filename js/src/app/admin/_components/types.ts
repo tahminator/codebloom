06e72e85d026b8d29c4ecceb051a072bc4a7924f
Angela Yu <angelayu0530@gmail.com>
2025-05-12 22:55:20 -0400

    lots of changes

--- js/src/app/admin/_components/types.ts
+++ js/src/app/admin/_components/types.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
-
-export const AdminSchema = () =>
+export const adminSchema = (leaderboardName: string) =>
   z.object({
     name: z.string().trim().min(1).max(16),
-    confirmation: z.string().trim(),
+    confirmation: z
+      .string()
+      .trim()
+      .refine((s) => s == leaderboardName),
   });
