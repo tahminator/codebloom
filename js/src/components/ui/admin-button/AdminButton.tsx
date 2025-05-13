a53d9df11ce9d9a99191e6a5ee2a43a98ca20d0c
Tahmid Ahmed <tahmid@tahmid.io>
2025-05-13 00:18:07 -0400

    Fixed the admin button in the footer using 'a' component instead of React Router's Link

--- js/src/components/ui/admin-button/AdminButton.tsx
+++ js/src/components/ui/admin-button/AdminButton.tsx
@@ -1,23 +1,20 @@
 import { useAuthQuery } from "@/lib/api/queries/auth";
 import { Button } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 export function GotoAdminPageButton() {
   const { data, status } = useAuthQuery();
+
   if (status !== "success") {
     return null;
   }
+
   if (!data.isAdmin) {
     return null;
   }
+
   return (
-    <Button
-      component="a"
-      href="/admin"
-      mt={0}
-      size="xs"
-      variant="outline"
-      style={{}}
-    >
+    <Button component={Link} to="/admin" mt={0} size="xs" variant="outline">
       Admin
     </Button>
   );
