a8b35f5d914ccfb1f471c8e77115ce11073b4b77
Alfardil Alam <fardilfayed@gmail.com>
2025-05-20 13:04:03 -0400

    fixed sentry reporting for the frontend and backend

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -1,9 +1,9 @@
 import ReactQueryProvider from "@/lib/queryProvider";
-import "@mantine/core/styles.css";
 import { router } from "@/lib/router";
+import "@mantine/core/styles.css";
 import { themeOverride } from "@/lib/theme";
-import "@mantine/notifications/styles.css";
 import { MantineProvider } from "@mantine/core";
+import "@mantine/notifications/styles.css";
 import { Notifications } from "@mantine/notifications";
 import * as Sentry from "@sentry/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -15,7 +15,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
 Sentry.init({
-  dsn: import.meta.env.DSN,
+  dsn: import.meta.env.VITE_DSN,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
