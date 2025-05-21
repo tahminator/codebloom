1f5b44b4c2ac770a0d59203829d5a8c5ada78e96
Alfardil Alam <fardilfayed@gmail.com>
2025-05-21 18:46:06 -0400

    updated it so that sentry is only turned on in production

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -14,12 +14,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
-Sentry.init({
-  dsn: import.meta.env.VITE_DSN,
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-});
+if (import.meta.env.VITE_MODE === "production") {
+  Sentry.init({
+    dsn: import.meta.env.VITE_DSN,
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true,
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
