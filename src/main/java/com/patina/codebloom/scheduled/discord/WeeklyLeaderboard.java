12952356639130606e9cfcdea4bcdb41389e0a22
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-05 00:54:50 -0400

    Implement JDAClient to interface with JDA logic instead of directly interfacing with JDA. Added some helper functions, including the ability to send a Patina leaderboard message to a channel and guild. In the future, this logic can be trivially modified to report school specific leaderboards to their respective Discord servers as well.

---
+++ src/main/java/com/patina/codebloom/scheduled/discord/WeeklyLeaderboard.java
@@ -0,0 +1,30 @@
+package com.patina.codebloom.scheduled.discord;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.patina.codebloom.jda.client.JDAClient;
+
+@Component
+public class WeeklyLeaderboard {
+    // private static final Logger LOGGER =
+    // LoggerFactory.getLogger(WeeklyLeaderboard.class);
+
+    private final JDAClient jdaClient;
+
+    WeeklyLeaderboard(final JDAClient jdaClient) {
+        this.jdaClient = jdaClient;
+    }
+
+    @Scheduled(cron = "0 0 13 ? * SAT")
+    public void sendWeeklyLeaderboard() {
+        try {
+            jdaClient.connect();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize JDAClient", e);
+        }
+
+        jdaClient.sendLeaderboardMessage(jdaClient.getPatinaGuildId(), jdaClient.getPatinaLeetcodeChannelId());
+    }
+
+}
