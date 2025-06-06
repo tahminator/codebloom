fb2d2f6c10571b609093cec032d67e8b7f3e1d60
Angela Yu <angelayu0530@gmail.com>
2025-06-05 20:47:08 -0400

    checkstyle fix

--- src/main/java/com/patina/codebloom/common/schools/SchoolEnum.java
+++ src/main/java/com/patina/codebloom/common/schools/SchoolEnum.java
@@ -6,7 +6,7 @@ public enum SchoolEnum {
 
     private final String emailDomain;
 
-    SchoolEnum(String emailDomain) {
+    SchoolEnum(final String emailDomain) {
         this.emailDomain = emailDomain;
     }
 
