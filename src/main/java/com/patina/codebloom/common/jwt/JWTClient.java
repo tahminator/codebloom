b83c8cc36b85d6f2d3f710f3e39c801d92c5cb5e
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-05 20:53:23 -0400

    Implement JWTClient

---
+++ src/main/java/com/patina/codebloom/common/jwt/JWTClient.java
@@ -0,0 +1,30 @@
+package com.patina.codebloom.common.jwt;
+
+import java.time.Duration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+/**
+ * Create and read JWTs.
+ */
+public interface JWTClient {
+    /**
+     * Create the JWT token, along with when it should expire.
+     */
+    <T> String encode(T obj, Duration expiresIn) throws JsonProcessingException;
+
+    /**
+     * Create the JWT token, default of 15 minute expiration.
+     */
+    <T> String encode(T obj) throws JsonProcessingException;
+
+    /**
+     * Parse the JWT token back into a valid Object. Will throw if expired or unable
+     * to verify JWT.
+     */
+    <T> T decode(String token, Class<T> clazz) throws JsonProcessingException, JWTVerificationException;
+}
