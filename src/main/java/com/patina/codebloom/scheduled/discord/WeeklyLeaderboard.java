608e6cdd839c0d7f0687921d4ecdec3b92a1c1f1
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-07 12:16:03 -0400

    Updated weeklyleaderboard to send at 12:30 PM, and added logs to help track what's wrong in production

--- src/main/java/com/patina/codebloom/scheduled/discord/WeeklyLeaderboard.java
+++ src/main/java/com/patina/codebloom/scheduled/discord/WeeklyLeaderboard.java
@@ -1,5 +1,7 @@
 package com.patina.codebloom.scheduled.discord;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -7,8 +9,7 @@ import com.patina.codebloom.jda.client.JDAClient;
 
 @Component
 public class WeeklyLeaderboard {
-    // private static final Logger LOGGER =
-    // LoggerFactory.getLogger(WeeklyLeaderboard.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeeklyLeaderboard.class);
 
     private final JDAClient jdaClient;
 
@@ -16,13 +17,16 @@ public class WeeklyLeaderboard {
         this.jdaClient = jdaClient;
     }
 
-    @Scheduled(cron = "0 0 12 ? * SAT")
+    @Scheduled(cron = "0 14 12 ? * SAT")
     public void sendWeeklyLeaderboard() {
+        LOGGER.info("WeeklyLeaderboard triggered.");
         try {
+            LOGGER.info("Connecting to JDA client...");
             jdaClient.connect();
         } catch (Exception e) {
             throw new RuntimeException("Failed to initialize JDAClient", e);
         }
+        LOGGER.info("JDDA Client should be connected now. Sending leaderboard...");
 
         jdaClient.sendLeaderboardMessage(jdaClient.getPatinaGuildId(), jdaClient.getPatinaLeetcodeChannelId());
     }
