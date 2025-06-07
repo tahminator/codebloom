d901e5a46373ef84ae41c3aab0903e9de61cb54b
Angela Yu <angelayu0530@gmail.com>
2025-06-07 12:56:39 -0400

    magic link class

---
+++ src/main/java/com/patina/codebloom/common/schools/magic/MagicLink.java
@@ -0,0 +1,25 @@
+package com.patina.codebloom.common.schools.magic;
+
+
+
+public class MagicLink {
+    private String email;
+    private String userId;
+    public MagicLink(final String email, final String userId) {
+        this.email = email;
+        this.userId = userId;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(final String email) {
+        this.email = email; 
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(final String userId) {
+        this.userId = userId;
+    }
+
+}
