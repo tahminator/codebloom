8a15486993d27d7dbb4d80aa05ec2ae030fefaf3
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-07 19:48:22 -0400

    Implement TestEmailClients

---
+++ src/test/java/com/patina/codebloom/email/TestEmailClients.java
@@ -0,0 +1,29 @@
+package com.patina.codebloom.email;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.patina.codebloom.common.email.client.codebloom.OfficialCodebloomEmail;
+import com.patina.codebloom.common.email.client.github.GithubOAuthEmail;
+import com.patina.codebloom.common.email.error.EmailException;
+
+@SpringBootTest
+public class TestEmailClients {
+    private final GithubOAuthEmail githubOAuthEmail;
+    private final OfficialCodebloomEmail officialCodebloomEmail;
+
+    @Autowired
+    public TestEmailClients(final GithubOAuthEmail githubOAuthEmail, final OfficialCodebloomEmail officialCodebloomEmail) {
+        this.githubOAuthEmail = githubOAuthEmail;
+        this.officialCodebloomEmail = officialCodebloomEmail;
+    }
+
+    @Test
+    void testConnections() throws EmailException {
+        githubOAuthEmail.testConnection();
+        officialCodebloomEmail.testConnection();
+    }
+}
