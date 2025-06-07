d242f8a362d6c1d582636c0d9620c3d8b5f3ca3e
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-07 19:27:35 -0400

    Implemented OfficialCodebloomEmail, and fixed up the Email contract a little to be more consistent. Updated .env.example + application.properties to reflect new env variables

---
+++ src/main/java/com/patina/codebloom/common/email/client/codebloom/OfficialCodebloomEmail.java
@@ -0,0 +1,75 @@
+package com.patina.codebloom.common.email.client.codebloom;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import com.patina.codebloom.common.email.Email;
+import com.patina.codebloom.common.email.Message;
+import com.patina.codebloom.common.email.error.EmailException;
+import com.patina.codebloom.common.email.options.SendEmailOptions;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+/**
+ * This is the official Codebloom email client which we use to interface with
+ * our actual users.
+ *
+ * For example, we use this client to send emails to users who are trying to
+ * verify their school status.
+ */
+@Component
+@EnableConfigurationProperties(OfficialCodebloomEmailProperties.class)
+public class OfficialCodebloomEmail extends Email {
+    private final OfficialCodebloomEmailProperties emailProperties;
+    private Session session;
+
+    public OfficialCodebloomEmail(final OfficialCodebloomEmailProperties emailProperties) {
+        this.emailProperties = emailProperties;
+        final Properties properties = new Properties();
+        properties.setProperty("mail.smtp.host", emailProperties.getHost());
+        properties.setProperty("mail.smtp.port", emailProperties.getPort());
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.starttls.required", "true");
+        properties.setProperty("mail.smtp.auth", "true");
+
+        session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailProperties.getUsername(), emailProperties.getPassword());
+            }
+        });
+    }
+
+    /**
+     * @deprecated - This is not supported.
+     */
+    @Override
+    public List<Message> getPastMessages() throws EmailException {
+        throw new EmailException("Reading messages is not supported");
+    }
+
+    @Override
+    public void sendMessage(SendEmailOptions sendEmailOptions) throws EmailException {
+        try {
+            jakarta.mail.Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(emailProperties.getUsername()));
+            message.setRecipient(jakarta.mail.Message.RecipientType.TO, new InternetAddress(sendEmailOptions.getRecipientEmail()));
+            message.setSubject(sendEmailOptions.getSubject());
+            message.setText(sendEmailOptions.getBody());
+
+            Transport.send(message);
+
+        } catch (Exception e) {
+            throw new EmailException("Something went wrong when sending message", e);
+        }
+    }
+
+}
---
+++ src/main/java/com/patina/codebloom/common/email/options/SendEmailOptions.java
@@ -0,0 +1,14 @@
+package com.patina.codebloom.common.email.options;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SendEmailOptions {
+    private final String recipientEmail;
+    private final String subject;
+    private final String body;
+}
