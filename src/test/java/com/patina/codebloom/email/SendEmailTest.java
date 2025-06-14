f3bbece4ddec9fe5acc837e21dbd2226318deaed
Angela Yu <angelayu0530@gmail.com>
2025-06-14 19:56:13 -0400

    slight changes + checkstyle fixes

--- src/test/java/com/patina/codebloom/email/SendEmailTest.java
+++ src/test/java/com/patina/codebloom/email/SendEmailTest.java
@@ -1,6 +1,6 @@
 package com.patina.codebloom.email;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
