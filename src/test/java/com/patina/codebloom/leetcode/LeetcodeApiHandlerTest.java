bc3d4a2cb7d5837d646a72256b0cee1c65988147
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-14 21:55:21 -0400

    Mark LeetcodeApiHandlerTest with the proper annotation in order to use non-static methods on @Before/After* annotations

--- src/test/java/com/patina/codebloom/leetcode/LeetcodeApiHandlerTest.java
+++ src/test/java/com/patina/codebloom/leetcode/LeetcodeApiHandlerTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,6 +22,7 @@ import com.patina.codebloom.common.leetcode.models.UserProfile;
 import io.restassured.RestAssured;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LeetcodeApiHandlerTest {
     private final LeetcodeApiHandler leetcodeApiHandler;
 
