36f703e18b84a0603d6f66f420aa5efa0df49cee
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-14 21:11:44 -0400

    HOTFIX: Disable all unnecessary background processes in CI

--- src/main/java/com/patina/codebloom/scheduled/potd/PotdSetter.java
+++ src/main/java/com/patina/codebloom/scheduled/potd/PotdSetter.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import com.patina.codebloom.common.leetcode.LeetcodeApiHandler;
 import com.patina.codebloom.common.leetcode.score.ScoreCalculator;
 
 @Component
+@Profile("!ci")
 public class PotdSetter {
     private static final Logger LOGGER = LoggerFactory.getLogger(PotdSetter.class);
 
