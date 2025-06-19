d579bb6f4c257b7a13a1c3a87f6bf5178640fc54
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-18 23:11:15 -0400

    Implement AnnouncementRepository tests

---
+++ src/test/java/com/patina/codebloom/db/announcement/AnnouncementRepositoryTest.java
@@ -0,0 +1,112 @@
+package com.patina.codebloom.db.announcement;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.patina.codebloom.common.db.models.announcement.Announcement;
+import com.patina.codebloom.common.db.repos.announcement.AnnouncementRepository;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
+public class AnnouncementRepositoryTest {
+    private AnnouncementRepository announcementRepository;
+    private Announcement testAnnouncement;
+
+    @Autowired
+    public AnnouncementRepositoryTest(final AnnouncementRepository announcementRepository) {
+        this.announcementRepository = announcementRepository;
+    }
+
+    @BeforeAll
+    void createTestAnnouncement() {
+        testAnnouncement = Announcement.builder()
+                        .id(null)
+                        .expiresAt(LocalDateTime.now().plusMinutes(5L))
+                        .showTimer(true)
+                        .build();
+        boolean isSuccessful = announcementRepository.createAnnouncement(testAnnouncement);
+
+        if (!isSuccessful) {
+            fail("Failed to create test announcement");
+        }
+    }
+
+    @AfterAll
+    void deleteTestAnnouncement() {
+        boolean isSuccessful = announcementRepository.deleteAnnouncementById(testAnnouncement.getId());
+
+        if (!isSuccessful) {
+            fail("Failed to delete test announcement");
+        }
+    }
+
+    /**
+     * @note this could possibly fail via race condition if you create a new
+     * announcement in the few seconds while this test suite is running, overriding
+     * the test announcement. If you do, let the test suite finish and let
+     * `@AfterAll` delete the test announcement and then just run the test suite
+     * again.
+     *
+     * We run this test first just to make sure that we don't run into that issue.
+     */
+    @Test
+    @Order(1)
+    void findMostRecentAnnouncement() {
+        Announcement possibleTestAnnouncement = announcementRepository.getRecentAnnouncement();
+
+        if (possibleTestAnnouncement == null) {
+            fail("most recent announcement doesn't exist, even though we created a test announcement");
+        }
+
+        /**
+         * @note - see method note
+         */
+        if (!testAnnouncement.equals(possibleTestAnnouncement)) {
+            fail("the most recent announcement does not equal to the test announcement");
+        }
+    }
+
+    @Test
+    void findAnnouncementById() {
+        Announcement possibleTestAnnouncement = announcementRepository.getAnnouncementById(testAnnouncement.getId());
+
+        if (possibleTestAnnouncement == null) {
+            fail("failed to find announcement by id");
+        }
+
+        if (!testAnnouncement.equals(possibleTestAnnouncement)) {
+            fail("the generated test announcement does not match the announcement fetched with get announcement by ID");
+        }
+    }
+
+    @Test
+    void findAllAnnouncements() {
+        List<Announcement> announcementsList = announcementRepository.getAllAnnouncements();
+
+        if (announcementsList == null) {
+            fail("list of all announcements is null");
+        }
+
+        if (announcementsList.size() == 0) {
+            fail("list of all announcements is 0, when it should be atleast 1 from the test announcement");
+        }
+
+        if (!announcementsList.contains(testAnnouncement)) {
+            fail("test announcement cannot be found in the list of all announcements");
+        }
+    }
+
+}
