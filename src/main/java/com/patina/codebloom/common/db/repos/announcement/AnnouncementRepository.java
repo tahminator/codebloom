0025fece9e21be98c2d8ec30956899af30efc263
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-19 00:45:03 -0400

    Checkstyle fixes

--- src/main/java/com/patina/codebloom/common/db/repos/announcement/AnnouncementRepository.java
+++ src/main/java/com/patina/codebloom/common/db/repos/announcement/AnnouncementRepository.java
@@ -1,23 +1,22 @@
 package com.patina.codebloom.common.db.repos.announcement;
 
-import java.sql.ResultSet;
 import java.util.List;
 
 import com.patina.codebloom.common.db.models.announcement.Announcement;
 
 public interface AnnouncementRepository {
-    public List<Announcement> getAllAnnouncements();
+    List<Announcement> getAllAnnouncements();
 
-    public Announcement getAnnouncementById(String id);
+    Announcement getAnnouncementById(String id);
 
-    public Announcement getRecentAnnouncement();
+    Announcement getRecentAnnouncement();
 
     /**
      * @note The id property of the object will be overriden.
      * @note the returned boolean indicates if the operation was a success or not.
      * 
      */
-    public boolean createAnnouncement(Announcement announcement);
+    boolean createAnnouncement(Announcement announcement);
 
-    public boolean deleteAnnouncementById(String id);
+    boolean deleteAnnouncementById(String id);
 }
