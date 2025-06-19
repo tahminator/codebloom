120f023478f08aa441831ecbaddf7adce686eebc
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-18 23:10:35 -0400

    Implement Announcement Java model to mirror database table

---
+++ src/main/java/com/patina/codebloom/common/db/models/announcement/Announcement.java
@@ -0,0 +1,25 @@
+package com.patina.codebloom.common.db.models.announcement;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Announcement {
+    // @Setter on some of the properties
+    // so we can override id property when new object
+    // created in database.
+    private @Setter String id;
+    private @Setter LocalDateTime createdAt;
+    @NotNull
+    private LocalDateTime expiresAt;
+    private boolean showTimer;
+}
