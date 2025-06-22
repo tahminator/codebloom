2294e4e3067e4e844280e3fdb434880bfb78c5b3
Alfardil Alam <fardilfayed@gmail.com>
2025-06-22 17:52:12 -0400

    created a test for POTDs

--- src/main/java/com/patina/codebloom/common/db/models/potd/POTD.java
+++ src/main/java/com/patina/codebloom/common/db/models/potd/POTD.java
@@ -2,6 +2,13 @@ package com.patina.codebloom.common.db.models.potd;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class POTD {
     private String id;
     private String title;
@@ -9,58 +16,10 @@ public class POTD {
     private float multiplier;
     private LocalDateTime createdAt;
 
-    public POTD(final String id, final String title, final String slug, final float multiplier, final LocalDateTime createdAt) {
-        this.id = id;
-        this.title = title;
-        this.slug = slug;
-        this.multiplier = multiplier;
-        this.createdAt = createdAt;
-    }
-
     public POTD(final String title, final String slug, final float multiplier, final LocalDateTime createdAt) {
         this.title = title;
         this.slug = slug;
         this.multiplier = multiplier;
-        this.createdAt = createdAt;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(final String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(final String slug) {
-        this.slug = slug;
-    }
-
-    public float getMultiplier() {
-        return multiplier;
-    }
-
-    public void setMultiplier(final float multiplier) {
-        this.multiplier = multiplier;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(final LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
