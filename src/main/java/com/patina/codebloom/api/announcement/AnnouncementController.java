82678047c55e7885afae1a882c6530809f6492eb
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-23 14:29:28 -0700

    Refactored the ability to create announcements into AdminController

--- src/main/java/com/patina/codebloom/api/announcement/AnnouncementController.java
+++ src/main/java/com/patina/codebloom/api/announcement/AnnouncementController.java
@@ -9,20 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
-import com.patina.codebloom.api.announcement.body.CreateAnnouncementBody;
 import com.patina.codebloom.common.db.models.announcement.Announcement;
 import com.patina.codebloom.common.db.repos.announcement.AnnouncementRepository;
 import com.patina.codebloom.common.dto.ApiResponder;
-import com.patina.codebloom.common.dto.autogen.UnsafeGenericFailureResponse;
 import com.patina.codebloom.common.security.Protector;
 import com.patina.codebloom.common.time.StandardizedLocalDateTime;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 
 @RestController
 @Tag(name = "Announcement routes", description = """
@@ -64,31 +58,4 @@ public class AnnouncementController {
         return ResponseEntity.ok(ApiResponder.success("Announcement found!", announcement));
     }
 
-    @Operation(summary = "Create a new announcement (only for admins).", responses = {
-            @ApiResponse(responseCode = "401", description = "Not authenticated", content = @Content(schema = @Schema(implementation = UnsafeGenericFailureResponse.class))),
-            @ApiResponse(responseCode = "200", description = "Announcement successfully created"),
-            @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content(schema = @Schema(implementation = UnsafeGenericFailureResponse.class)))
-    })
-    @PostMapping("")
-    public ResponseEntity<ApiResponder<Announcement>> createNewAnnouncement(
-                    @Valid @RequestBody final CreateAnnouncementBody createAnnouncementBody,
-                    final HttpServletRequest request) {
-        protector.validateAdminSession(request);
-
-        Announcement announcement = Announcement.builder()
-                        .expiresAt(createAnnouncementBody.getExpiresAt())
-                        .showTimer(createAnnouncementBody.isShowTimer())
-                        .message(createAnnouncementBody.getMessage())
-                        .build();
-
-        boolean isSuccessful = announcementRepository.createAnnouncement(announcement);
-
-        if (!isSuccessful) {
-            return ResponseEntity
-                            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                            .body(ApiResponder.failure("Hmm, something went wrong."));
-        }
-
-        return ResponseEntity.ok(ApiResponder.success("New announcement successfully created!", announcement));
-    }
 }
