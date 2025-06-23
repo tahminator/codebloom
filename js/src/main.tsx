5062dfbffc51a835fcb71cb840c886076fc66664
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-23 15:02:57 -0700

    Implemented the ability to load announcements on the frontend and show them if valid

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -1,15 +1,16 @@
+import BannerParent from "@/components/ui/banner/BannerParent";
 import ReactQueryProvider from "@/lib/queryProvider";
-import { router } from "@/lib/router";
 import "@mantine/core/styles.css";
+import { router } from "@/lib/router";
 import { themeOverride } from "@/lib/theme";
-import { MantineProvider } from "@mantine/core";
 import "@mantine/notifications/styles.css";
+import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import * as Sentry from "@sentry/react";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./index.css";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
@@ -27,10 +28,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReactQueryProvider>
       <MantineProvider theme={themeOverride} forceColorScheme={"dark"}>
-        {/* <Banner */}
-        {/*   message="The FIRST official leaderboard for the month of April will be ending very soon!" */}
-        {/*   counter={new Date("2025-05-02T18:00:00-04:00")} */}
-        {/* /> */}
+        <BannerParent />
         <RouterProvider router={router} />
         <Notifications />
       </MantineProvider>
