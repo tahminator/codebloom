2ee767384e71de2de28f60d0fc0037a2cc216311
Tahmid Ahmed <tahmid@tahmid.io>
2025-06-25 13:43:05 -0700

    Add hash code, toString, equals to Auth object

--- src/main/java/com/patina/codebloom/common/db/models/auth/Auth.java
+++ src/main/java/com/patina/codebloom/common/db/models/auth/Auth.java
@@ -4,13 +4,17 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode
+@ToString
 public class Auth {
     private String id;
     private String token;
