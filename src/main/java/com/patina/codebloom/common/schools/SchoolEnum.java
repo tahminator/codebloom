21a200f741719bd323619d1db9f95c62243c6407
Angela Yu <angelayu0530@gmail.com>
2025-06-28 12:59:57 -0400

    school enum changes with tag

--- src/main/java/com/patina/codebloom/common/schools/SchoolEnum.java
+++ src/main/java/com/patina/codebloom/common/schools/SchoolEnum.java
@@ -1,16 +1,22 @@
 package com.patina.codebloom.common.schools;
+import com.patina.codebloom.common.db.models.usertag.Tag;
 
 public enum SchoolEnum {
-    HUNTER("@myhunter.cuny.edu"),
-    NYU("@nyu.edu");
+    HUNTER("@myhunter.cuny.edu", Tag.Hunter),
+    NYU("@nyu.edu", Tag.Nyu);
 
     private final String emailDomain;
+    private final Tag internalTag;
 
-    SchoolEnum(final String emailDomain) {
+    SchoolEnum(final String emailDomain, final Tag internalTag) {
         this.emailDomain = emailDomain;
+        this.internalTag = internalTag;
     }
 
     public String getEmailDomain() {
         return emailDomain;
+    }
+    public Tag getInternalTag() {
+        return internalTag;
     }
 }
