1aca83a1476977e62786de3c408d9b2a324cebf6
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-02 19:40:24 -0400

    Add the ability to delete latest weekly message in db repo

--- src/main/java/com/patina/codebloom/common/db/repos/weekly/WeeklyMessageRepository.java
+++ src/main/java/com/patina/codebloom/common/db/repos/weekly/WeeklyMessageRepository.java
@@ -34,4 +34,6 @@ public interface WeeklyMessageRepository {
     boolean createLatestWeeklyMessage();
 
     boolean deleteWeeklyMessageById(String id);
+
+    boolean deleteLatestWeeklyMessage();
 }
