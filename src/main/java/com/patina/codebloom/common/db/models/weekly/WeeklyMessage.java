c01d2420b3531e395ae7aad60a294b7b016481bf
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-02 19:05:13 -0400

    Update WeeklyMessage repository to be inline with new db repository guidelines

--- src/main/java/com/patina/codebloom/common/db/models/weekly/WeeklyMessage.java
+++ src/main/java/com/patina/codebloom/common/db/models/weekly/WeeklyMessage.java
@@ -2,17 +2,19 @@ package com.patina.codebloom.common.db.models.weekly;
 
 import java.time.LocalDateTime;
 
+import groovy.transform.EqualsAndHashCode;
+import groovy.transform.ToString;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode
+@ToString
 public class WeeklyMessage {
     private String id;
     private LocalDateTime createdAt;
