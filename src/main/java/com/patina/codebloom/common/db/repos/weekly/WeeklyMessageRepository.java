b3de3c0acdafb3ec030d7f62c7299e97716fef61
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-02 19:05:13 -0400

    Update WeeklyMessage repository to be inline with new db repository guidelines

--- src/main/java/com/patina/codebloom/common/db/repos/weekly/WeeklyMessageRepository.java
+++ src/main/java/com/patina/codebloom/common/db/repos/weekly/WeeklyMessageRepository.java
@@ -5,10 +5,33 @@ import com.patina.codebloom.common.db.models.weekly.WeeklyMessage;
 public interface WeeklyMessageRepository {
     WeeklyMessage getLatestWeeklyMessage();
 
+    WeeklyMessage getWeeklyMessageById(String id);
+
+    /**
+     * @note - The provided object's methods will be overriden with any returned
+     * data from the database.
+     *
+     * 
+     * @param message - required fields:
+     * <ul>
+     * </ul>
+     * 
+     */
     boolean createLatestWeeklyMessage(WeeklyMessage message);
 
     /**
      * If you have no purpose for the newly created object, just use this.
+     *
+     * @note - The provided object's methods will be overriden with any returned
+     * data from the database.
+     *
+     * 
+     * @param message - required fields:
+     * <ul>
+     * </ul>
+     * 
      */
     boolean createLatestWeeklyMessage();
+
+    boolean deleteWeeklyMessageById(String id);
 }
