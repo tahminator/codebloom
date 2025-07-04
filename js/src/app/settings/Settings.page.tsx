268f4a592356e5279b3a12dd73d8fd239902d588
Alfardil Alam <fardilfayed@gmail.com>
2025-07-04 18:33:48 -0400

    created a settings page for the school verifying component and added a preliminary modal

---
+++ js/src/app/settings/Settings.page.tsx
@@ -0,0 +1,49 @@
+import { Footer } from "@/components/ui/footer/Footer";
+import Header from "@/components/ui/header/Header";
+import Toast from "@/components/ui/toast/Toast";
+import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
+import { useAuthQuery } from "@/lib/api/queries/auth";
+import { Box, Center, Loader, Title } from "@mantine/core";
+
+import SchoolVerifySettingsCard from "./_components/SchoolVerifySettingsCard";
+
+export default function SettingsPage() {
+  const { data, status } = useAuthQuery();
+
+  if (status === "pending") {
+    return (
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return <Toast message="Sorry, something went wrong." />;
+  }
+
+  const authenticated = !!data.user && !!data.session;
+
+  if (!authenticated) {
+    return (
+      <ToastWithRedirect to="/login" message="You are not authenticated!" />
+    );
+  }
+
+  return (
+    <>
+      <Header />
+      <Box p={"lg}"}>
+        <Box>
+          <Center>
+            <Title order={3} p="md">
+              Settings
+            </Title>
+          </Center>
+          <SchoolVerifySettingsCard user={data.user} />
+        </Box>
+      </Box>
+      <Footer />
+    </>
+  );
+}
