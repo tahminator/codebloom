9af8c84010ba33dde64d496145be6482f6417737
$NAME <tahmid@tahmid.io>
2025-07-06 13:53:12 -0400

    Update all DB models with schema defs (DESC)
    Update all DB models with schema definitions to always show as required, and set nullable to true (default false) for fields that are not guaranteed to be set on the round trip back from the database.

--- src/main/java/com/patina/codebloom/common/db/models/question/Question.java
+++ src/main/java/com/patina/codebloom/common/db/models/question/Question.java
@@ -2,17 +2,31 @@ package com.patina.codebloom.common.db.models.question;
 
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class Question {
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String id;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String userId;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String questionSlug;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String questionTitle;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private QuestionDifficulty questionDifficulty;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private int questionNumber;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String questionLink;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
     private String description;
 
     /**
@@ -20,19 +34,32 @@ public class Question {
      * some points, so there might be a case where we create the DB entry and then
      * pass it to a message queue to use AI and calculate a score.
      */
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
     private Integer pointsAwarded;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private float acceptanceRate;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime createdAt;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime submittedAt;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
     private String runtime;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
     private String memory;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
     private String code;
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
     private String language;
 
     // Not every submission will have this.
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
     private String submissionId;
 
     /*
