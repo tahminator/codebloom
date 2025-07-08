ceeb8d6da9d2fe083c6aa3bb1fdaf03d3e98cb0e
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-07 22:08:14 -0400

    Update Session repository to follow new db guidelines

--- src/main/java/com/patina/codebloom/common/db/models/Session.java
+++ src/main/java/com/patina/codebloom/common/db/models/Session.java
@@ -3,7 +3,19 @@ package com.patina.codebloom.common.db.models;
 import java.time.LocalDateTime;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
 public class Session {
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String id;
@@ -14,38 +26,8 @@ public class Session {
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime expiresAt;
 
-    public Session(final String id, final String userId, final LocalDateTime expiresAt) {
-        this.id = id;
-        this.userId = userId;
-        this.expiresAt = expiresAt;
-    }
-
-    public Session(final String userId, final LocalDateTime expiresAt) {
-        this.userId = userId;
-        this.expiresAt = expiresAt;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(final String id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(final String userId) {
-        this.userId = userId;
-    }
-
-    public LocalDateTime getExpiresAt() {
-        return expiresAt;
-    }
-
-    public void setExpiresAt(final LocalDateTime expiresAt) {
-        this.expiresAt = expiresAt;
-    }
+    // public Session(final String userId, final LocalDateTime expiresAt) {
+    // this.userId = userId;
+    // this.expiresAt = expiresAt;
+    // }
 }
