e155f3dac9c440c7ec7b784ffa3727ac16a8b081
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-07 21:26:51 -0400

    Add Empty type to the frontend

---
+++ js/src/lib/api/common/empty.ts
@@ -0,0 +1,19 @@
+import { components } from "@/lib/api/types/autogen/schema";
+
+/**
+ * Equivalent to an empty object: {}
+ *
+ * This is used as ApiResponse<Empty> which
+ * signifies that even if the operation was successful,
+ * the `payload` key will just be = {}.
+ *
+ * @example
+ * ```json
+ * {
+ *    success: true,
+ *    message: "Operation was successful!",
+ *    payload: {}
+ * }
+ * ```
+ */
+export type Empty = components["schemas"]["Empty"];
