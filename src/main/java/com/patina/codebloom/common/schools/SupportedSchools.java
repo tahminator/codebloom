71411adf27f6d4314a2ca621305a49b05493c719
Tahmid Ahmed <tahmid@tahmid.io>
2025-07-12 18:11:56 -0400

    Remove SupportedSchools list in favor of built-in Enum.values()

--- src/main/java/com/patina/codebloom/common/schools/SupportedSchools.java
+++
@@ -1,9 +0,0 @@
-package com.patina.codebloom.common.schools;
-
-import java.util.List;
-
-public class SupportedSchools {
-    public static List<SchoolEnum> getList() {
-        return List.of(SchoolEnum.HUNTER, SchoolEnum.NYU);
-    }
-}
