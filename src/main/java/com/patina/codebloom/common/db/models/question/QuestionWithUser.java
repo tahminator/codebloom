85511663a6e349359a83e2505565752b7d969411
Angela Yu <angelayu0530@gmail.com>
2025-07-13 14:12:16 -0400

    checkstyle changes

--- src/main/java/com/patina/codebloom/common/db/models/question/QuestionWithUser.java
+++ src/main/java/com/patina/codebloom/common/db/models/question/QuestionWithUser.java
@@ -2,12 +2,9 @@ package com.patina.codebloom.common.db.models.question;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-import java.time.LocalDateTime;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
