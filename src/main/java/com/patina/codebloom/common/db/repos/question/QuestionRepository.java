8a57903f778934fe5c6ab005fe0f462917be98b6
Angela Yu <angelayu0530@gmail.com>
2025-07-12 22:30:41 -0400

    changes to questionRepo due to Question using lomobok

--- src/main/java/com/patina/codebloom/common/db/repos/question/QuestionRepository.java
+++ src/main/java/com/patina/codebloom/common/db/repos/question/QuestionRepository.java
@@ -1,11 +1,28 @@
 package com.patina.codebloom.common.db.repos.question;
 
-import java.util.ArrayList;
-
 import com.patina.codebloom.common.db.models.question.Question;
 import com.patina.codebloom.common.db.models.question.QuestionWithUser;
+import java.util.ArrayList;
 
 public interface QuestionRepository {
+    /**
+     * @note - The provided object's methods will be overridden with any returned
+     * data from the database.
+     *
+     * @param question - required fields:
+     * <ul>
+     * <li>userId</li>
+     * <li>questionSlug</li>
+     * <li>questionDifficulty</li>
+     * <li>questionNumber</li>
+     * <li>questionLink</li>
+     * <li>questionTitle</li>
+     * <li>description</li>
+     * <li>pointsAwarded</li>
+     * <li>acceptanceRate</li>
+     * <li>submittedAt</li>
+     * </ul>
+     */
     Question createQuestion(Question question);
 
     Question getQuestionById(String id);
@@ -14,6 +31,24 @@ public interface QuestionRepository {
 
     ArrayList<Question> getQuestionsByUserId(String userId, int page, int pageSize, String query);
 
+    /**
+     * @note - The provided object's methods will be overridden with any returned
+     * data from the database.
+     *
+     * @param question - overridden fields:
+     * <ul>
+     * <li>questionTitle</li>
+     * <li>description</li>
+     * <li>pointsAwarded</li>
+     * <li>acceptanceRate</li>
+     * <li>runtime</li>
+     * <li>memory</li>
+     * <li>code</li>
+     * <li>language</li>
+     * <li>submissionId</li>
+     * </ul>
+     * @return updated question if sucessful 
+     */
     Question updateQuestion(Question question);
 
     boolean deleteQuestionById(String id);
