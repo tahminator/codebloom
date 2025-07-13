ce9145c4139d11fbc029e41f5eae8029ab6a2c60
Alfardil Alam <fardilfayed@gmail.com>
2025-07-13 02:39:43 -0400

    added a zod schema to validate email being verified. then created mutation to authenticate valid supported emails

--- js/src/app/settings/_components/SchoolEmailModal.tsx
+++ js/src/app/settings/_components/SchoolEmailModal.tsx
@@ -1,4 +1,9 @@
-import { Box, Center, Modal, Text } from "@mantine/core";
+import { useVerifySchoolMutation } from "@/lib/api/queries/auth/school";
+import { schoolVerificationForm } from "@/lib/api/schema/school";
+import { Box, Button, Modal, Text, TextInput } from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import { notifications } from "@mantine/notifications";
+import { z } from "zod";
 
 type SchoolModalProps = {
   enabled: boolean;
@@ -9,15 +14,61 @@ export default function SchoolEmailModal({
   enabled,
   toggle,
 }: SchoolModalProps) {
+  const { mutate, status } = useVerifySchoolMutation();
+  const form = useForm({
+    validate: zodResolver(schoolVerificationForm),
+    initialValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof schoolVerificationForm>) => {
+    notifications.show({
+      message: "Verifying email... ",
+      color: "blue",
+    });
+    mutate(
+      { email: values.email },
+      {
+        onSuccess: async (data) => {
+          notifications.show({
+            message: data.message,
+            color: data.success ? undefined : "red",
+          });
+          if (data.success) {
+            form.reset();
+          }
+        },
+      },
+    );
+  };
+
   return (
-    <Modal opened={enabled} onClose={toggle}>
-      <Box>
-        <Center>
-          <Text>
-            Verify your student email to gain access to school-specific
-            competitions!
-          </Text>
-        </Center>
+    <Modal opened={enabled} onClose={toggle} size={"lg"}>
+      <Box p={"lg"}>
+        <Text p="md">
+          Verify your student email to gain access to school-specific
+          competitions!
+        </Text>
+
+        <form onSubmit={form.onSubmit(onSubmit)}>
+          <TextInput
+            {...form.getInputProps("email")}
+            placeholder="Enter your school email"
+            error={form.errors.email}
+            p="md"
+          />
+
+          <Button
+            type="submit"
+            size="xs"
+            ml="md"
+            disabled={!form.isValid("confirmation") || status === "pending"}
+            loading={status === "pending"}
+          >
+            Submit
+          </Button>
+        </form>
       </Box>
     </Modal>
   );
