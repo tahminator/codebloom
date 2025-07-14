c272f66bce793d4646214841516232bbcd3ca82e
Alfardil Alam <fardilfayed@gmail.com>
2025-07-14 17:23:52 -0400

    moved effect to page level. changed to updating notification instead of stacking it

--- js/src/app/settings/Settings.page.tsx
+++ js/src/app/settings/Settings.page.tsx
@@ -3,12 +3,25 @@ import Header from "@/components/ui/header/Header";
 import Toast from "@/components/ui/toast/Toast";
 import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
 import { useAuthQuery } from "@/lib/api/queries/auth";
+import { useBackendCallbackParams } from "@/lib/hooks/useBackendCallbackParams";
 import { Box, Center, Loader, Title } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { useEffect } from "react";
 
 import SchoolVerifySettingsCard from "./_components/SchoolVerifySettingsCard";
 
 export default function SettingsPage() {
   const { data, status } = useAuthQuery();
+  const { success, message } = useBackendCallbackParams();
+
+  useEffect(() => {
+    if (typeof success === "boolean" && message) {
+      notifications.show({
+        message,
+        color: success ? "green" : "red",
+      });
+    }
+  }, [message, success]);
 
   if (status === "pending") {
     return (
