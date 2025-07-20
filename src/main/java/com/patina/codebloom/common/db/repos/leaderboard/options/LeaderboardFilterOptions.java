effcf6e0023a0a9bf991f500454e4c5c62e5b2fb
Angela Yu <angelayu0530@gmail.com>
2025-07-20 12:15:19 -0400

    created LeaderboardFilterOption

---
+++ src/main/java/com/patina/codebloom/common/db/repos/leaderboard/options/LeaderboardFilterOptions.java
@@ -0,0 +1,25 @@
+package com.patina.codebloom.common.db.repos.leaderboard.options;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import lombok.extern.jackson.Jacksonized;
+
+@Builder
+@Jacksonized
+@Getter
+@ToString
+@EqualsAndHashCode
+public class LeaderboardFilterOptions {
+    @Builder.Default
+    private final String id = "";
+    @Builder.Default
+    private final int page = 1;
+    @Builder.Default
+    private final int pageSize = 20;
+    @Builder.Default
+    private final String query = "";
+    @Builder.Default
+    private final boolean patina = false;
+}
