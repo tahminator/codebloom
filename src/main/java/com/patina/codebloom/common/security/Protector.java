c8293996edf36f4346a5d8f95cdc7c0cba896016
Alfardil Alam <fardilfayed@gmail.com>
2025-07-28 00:48:04 -0400

    profileUrl gets updated when leetcode username is set for the first time and also when user authenticates

--- src/main/java/com/patina/codebloom/common/security/Protector.java
+++ src/main/java/com/patina/codebloom/common/security/Protector.java
@@ -8,6 +8,8 @@ import com.patina.codebloom.common.db.models.Session;
 import com.patina.codebloom.common.db.models.user.PrivateUser;
 import com.patina.codebloom.common.db.repos.session.SessionRepository;
 import com.patina.codebloom.common.db.repos.user.UserRepository;
+import com.patina.codebloom.common.leetcode.LeetcodeApiHandler;
+import com.patina.codebloom.common.leetcode.models.UserProfile;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,10 +26,14 @@ public class Protector {
 
     private final SessionRepository sessionRepository;
     private final UserRepository userRepository;
+    private final LeetcodeApiHandler leetcodeApiHandler;
 
-    public Protector(final SessionRepository sessionRepository, final UserRepository userRepository) {
+    public Protector(final SessionRepository sessionRepository,
+                    final UserRepository userRepository,
+                    final LeetcodeApiHandler leetcodeApiHandler) {
         this.sessionRepository = sessionRepository;
         this.userRepository = userRepository;
+        this.leetcodeApiHandler = leetcodeApiHandler;
     }
 
     public AuthenticationObject validateSession(final HttpServletRequest request) {
@@ -48,6 +54,10 @@ public class Protector {
                 PrivateUser user = userRepository.getPrivateUserById(session.getUserId());
                 if (user == null) {
                     throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+                }
+                if (user.getLeetcodeUsername() != null) {
+                    UserProfile leetcodeUserProfile = leetcodeApiHandler.getUserProfile(user.getLeetcodeUsername());
+                    user.setProfileUrl(leetcodeUserProfile.getUserAvatar());
                 }
                 return new AuthenticationObject(user, session);
             }
