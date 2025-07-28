fb167f249cb6eb7d07b21f504700b89ca3e4d0e3
Alfardil Alam <fardilfayed@gmail.com>
2025-07-28 13:31:56 -0400

    updated the behavior to execute in CustomAuthenticationSuccessHandler

--- src/main/java/com/patina/codebloom/common/security/Protector.java
+++ src/main/java/com/patina/codebloom/common/security/Protector.java
@@ -8,8 +8,6 @@ import com.patina.codebloom.common.db.models.Session;
 import com.patina.codebloom.common.db.models.user.PrivateUser;
 import com.patina.codebloom.common.db.repos.session.SessionRepository;
 import com.patina.codebloom.common.db.repos.user.UserRepository;
-import com.patina.codebloom.common.leetcode.LeetcodeApiHandler;
-import com.patina.codebloom.common.leetcode.models.UserProfile;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,14 +24,11 @@ public class Protector {
 
     private final SessionRepository sessionRepository;
     private final UserRepository userRepository;
-    private final LeetcodeApiHandler leetcodeApiHandler;
 
-    public Protector(final SessionRepository sessionRepository,
-                    final UserRepository userRepository,
-                    final LeetcodeApiHandler leetcodeApiHandler) {
+    public Protector(final SessionRepository sessionRepository, final UserRepository userRepository) {
         this.sessionRepository = sessionRepository;
         this.userRepository = userRepository;
-        this.leetcodeApiHandler = leetcodeApiHandler;
+
     }
 
     public AuthenticationObject validateSession(final HttpServletRequest request) {
@@ -55,10 +50,7 @@ public class Protector {
                 if (user == null) {
                     throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
                 }
-                if (user.getLeetcodeUsername() != null) {
-                    UserProfile leetcodeUserProfile = leetcodeApiHandler.getUserProfile(user.getLeetcodeUsername());
-                    user.setProfileUrl(leetcodeUserProfile.getUserAvatar());
-                }
+
                 return new AuthenticationObject(user, session);
             }
         }
