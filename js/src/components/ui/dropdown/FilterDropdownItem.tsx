c7286e108bc69b5f5856f3683faf38267343780a
Tahmid Ahmed <tahmid@tahmid.io>
2025-08-02 05:40:17 -0400

    Update FilterDropdownItem to support switches as well instead of just checkboxes

--- js/src/components/ui/dropdown/FilterDropdownItem.tsx
+++ js/src/components/ui/dropdown/FilterDropdownItem.tsx
@@ -1,10 +1,11 @@
-import { Checkbox, Menu, MenuItemProps } from "@mantine/core";
+import { Checkbox, Menu, MenuItemProps, Switch } from "@mantine/core";
 import { ReactNode } from "react";
 
 interface FilterDropdownItemProps extends MenuItemProps {
   value: boolean;
   toggle: () => void;
   name: ReactNode;
+  switchMode?: boolean;
 }
 
 /**
@@ -20,9 +21,28 @@ export default function FilterDropdownItem({
   value,
   toggle,
   name,
+  disabled,
+  switchMode = false,
+  ...props
 }: FilterDropdownItemProps) {
   return (
-    <Menu.Item onClick={toggle} closeMenuOnClick={false}>
+    <Menu.Item
+      onClick={toggle}
+      closeMenuOnClick={false}
+      disabled={disabled}
+      {...props}
+      leftSection={
+        switchMode ?
+          <Switch
+            // Switch requires onClick to be passed in, while Checkbox doesn't
+            onClick={toggle}
+            checked={value}
+            disabled={disabled}
+            withThumbIndicator={false}
+          />
+        : <Checkbox checked={value} disabled={disabled} />
+      }
+    >
       <div
         style={{
           display: "flex",
@@ -31,7 +51,6 @@ export default function FilterDropdownItem({
           width: "100%",
         }}
       >
-        <Checkbox checked={value} />
         {name}
       </div>
     </Menu.Item>
