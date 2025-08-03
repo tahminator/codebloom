1378ac68ebe4a70acd9a7a62e6e81ffd0a526dbf
Tahmid Ahmed <tahmid@tahmid.io>
2025-08-03 04:32:18 -0400

    Replace PROD check with schoolFF in leaderboard page

--- js/src/app/leaderboard/_components/Leaderboard.tsx
+++ js/src/app/leaderboard/_components/Leaderboard.tsx
@@ -6,6 +6,7 @@ import CustomPagination from "@/components/ui/table/CustomPagination";
 import SearchBox from "@/components/ui/table/SearchBox";
 import Toast from "@/components/ui/toast/Toast";
 import { useCurrentLeaderboardUsersQuery } from "@/lib/api/queries/leaderboard";
+import { schoolFF } from "@/lib/ff";
 import getOrdinal from "@/lib/helper/ordinal";
 import { theme } from "@/lib/theme";
 import {
@@ -122,7 +123,7 @@ export default function LeaderboardIndex() {
             </Box>
           }
         />
-        {!import.meta.env.PROD && (
+        {schoolFF && (
           <>
             <FilterDropdownItem
               value={hunter}
