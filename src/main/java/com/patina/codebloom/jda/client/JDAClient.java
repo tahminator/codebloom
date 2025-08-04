83e2d540efe383967f8546a5bec7269331014b5c
Tahmid Ahmed <tahmid@tahmid.io>
2025-08-03 21:50:37 -0400

    Overhaul JDA + Implement Error Reporter (DESC)
    Overhaul JDA to help unify the logic for getting different guild/channel IDs + update methods to maintain unity & reduce tightly coupled behavior. Downstream consumers of the JDAClient have been updated as well as necessary. The GlobalExceptionHandler will now send a message to the error reporter Discord channel with a full stack trace of any RuntimeException.

--- src/main/java/com/patina/codebloom/jda/client/JDAClient.java
+++ src/main/java/com/patina/codebloom/jda/client/JDAClient.java
@@ -1,31 +1,18 @@
 package com.patina.codebloom.jda.client;
 
-import java.awt.Color;
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.Browser.NewContextOptions;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.ScreenshotType;
-import com.patina.codebloom.common.db.models.leaderboard.Leaderboard;
-import com.patina.codebloom.common.db.models.user.UserWithScore;
-import com.patina.codebloom.common.db.repos.leaderboard.LeaderboardRepository;
-import com.patina.codebloom.common.db.repos.leaderboard.options.LeaderboardFilterOptions;
-import com.patina.codebloom.common.time.StandardizedLocalDateTime;
 import com.patina.codebloom.jda.JDAInitializer;
-import com.patina.codebloom.jda.client.options.LeaderboardMessageOptions;
+import com.patina.codebloom.jda.client.options.EmbeddedMessageOptions;
+import com.patina.codebloom.jda.properties.patina.JDAPatinaProperties;
+import com.patina.codebloom.jda.properties.reporting.JDAReportingProperties;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
@@ -38,15 +25,24 @@ import net.dv8tion.jda.api.utils.FileUpload;
  * Use this client to interface with any required Discord bot logic.
  */
 @Component
+@Slf4j
+@EnableConfigurationProperties({ JDAPatinaProperties.class, JDAReportingProperties.class })
 public class JDAClient {
-    @Autowired
-    private JDAInitializer jdaInitializer;
-    @Autowired
-    private LeaderboardRepository leaderboardRepository;
+    private final JDAInitializer jdaInitializer;
     private JDA jda;
-    private static final Logger LOGGER = LoggerFactory.getLogger(JDAClient.class);
 
-    JDAClient() {
+    @Getter
+    private final JDAPatinaProperties jdaPatinaProperties;
+
+    @Getter
+    private final JDAReportingProperties jdaReportingProperties;
+
+    JDAClient(final JDAInitializer jdaInitializer,
+                    final JDAPatinaProperties jdaPatinaProperties,
+                    final JDAReportingProperties jdaReportingProperties) {
+        this.jdaInitializer = jdaInitializer;
+        this.jdaPatinaProperties = jdaPatinaProperties;
+        this.jdaReportingProperties = jdaReportingProperties;
     }
 
     private void isJdaReadyOrThrow() {
@@ -62,8 +58,8 @@ public class JDAClient {
         }
     }
 
-    public void connect() throws Exception {
-        jda = jdaInitializer.jda();
+    public void connect() throws InterruptedException {
+        jda = jdaInitializer.initializeJda();
     }
 
     public List<Guild> getGuilds() {
@@ -71,22 +67,10 @@ public class JDAClient {
         return jda.getGuilds();
     }
 
-    /**
-     * This is the recommended way to access Patina server's Discord ID.
-     */
-    public String getPatinaGuildId() {
+    public Guild getGuildById(final long guildId) {
+        String guildIdString = String.valueOf(guildId);
         isJdaReadyOrThrow();
-        return jdaInitializer.getJdaProperties().getId();
-    }
-
-    public long getPatinaLeetcodeChannelId() {
-        isJdaReadyOrThrow();
-        return jdaInitializer.getJdaProperties().getChannelIdAsLong();
-    }
-
-    public Guild getGuildById(final String guildId) {
-        isJdaReadyOrThrow();
-        return jda.getGuilds().stream().filter(g -> g.getId().equals(guildId)).findFirst().orElse(null);
+        return jda.getGuilds().stream().filter(g -> g.getId().equals(guildIdString)).findFirst().orElse(null);
     }
 
     public List<Member> getMemberListByGuildId(final String guildId) {
@@ -103,141 +87,42 @@ public class JDAClient {
     }
 
     /**
-     * Sends a message with the guild ID and channel ID of your choosing.
+     * Send a Rich Embed message with a file to the guild ID and channel ID of your
+     * choosing.
      *
      * Check LeaderboardMessageOptions for details on what is supported.
-     *
-     * @implNote - Use `LeaderboardMessageOptions.builder()` to create the options
-     * object.
      */
-    public void sendLeaderboardMessage(final LeaderboardMessageOptions options) {
+    public void sendEmbedWithImage(final EmbeddedMessageOptions options) {
         isJdaReadyOrThrow();
         Guild guild = getGuildById(options.getGuildId());
         if (guild == null) {
-            LOGGER.error("Guild does not exist to send leaderboard message.");
+            log.error("Guild does not exist.");
             return;
         }
         TextChannel channel = guild.getTextChannelById(options.getChannelId());
         if (channel == null) {
-            LOGGER.error("Channel does not exist on the given guild.");
+            log.error("Channel does not exist on the given guild.");
             return;
         }
 
-        MessageEmbed embed = new EmbedBuilder()
+        EmbedBuilder embedBuilder = new EmbedBuilder()
                         .setTitle(options.getTitle())
                         .setDescription(options.getDescription())
                         .setFooter(options.getFooterText(), options.getFooterIcon())
-                        .setImage("attachment://leaderboard.png")
-                        .setColor(options.getColor())
-                        .build();
+                        .setColor(options.getColor());
 
-        LOGGER.info("Message has been built, ready to send...");
+        if (options.getFileName().endsWith(".png") || options.getFileName().endsWith(".jpg")) {
+            embedBuilder.setImage(String.format("attachment://%s", options.getFileName()));
+        }
 
-        channel.sendFiles(FileUpload.fromData(options.getScreenshotBytes(), "leaderboard.png"))
+        MessageEmbed embed = embedBuilder.build();
+
+        log.info("Message has been built, ready to send...");
+
+        channel.sendFiles(FileUpload.fromData(options.getFileBytes(), options.getFileName()))
                         .setEmbeds(embed)
                         .queue();
 
-        LOGGER.info("Message has been queued");
-    }
-
-    /**
-     * Sends a screenshot of the Patina leaderboard (as well as tagging the top 3
-     * Patina users) to a given guild and channel of your choosing.
-     */
-    public void sendLeaderboardMessage(final String guildId, final long channelId) {
-        isJdaReadyOrThrow();
-        Guild guild = getGuildById(guildId);
-        if (guild == null) {
-            LOGGER.error("Guild does not exist to send leaderboard message.");
-            return;
-        }
-        TextChannel channel = guild.getTextChannelById(channelId);
-        if (channel == null) {
-            LOGGER.error("Channel does not exist on the given guild.");
-            return;
-        }
-
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true).setTimeout(40000));
-            BrowserContext context = browser.newContext(new NewContextOptions()
-                            .setUserAgent("Mozilla/5.0 (Linux; U; Android 4.4.1; SAMSUNG SM-J210G Build/KTU84P) AppleWebKit/536.31 (KHTML, like Gecko) Chrome/48.0.2090.359 Mobile Safari/601.9")
-                            .setStorageState(null));
-            context.clearCookies();
-            Page page = browser.newPage();
-            page.navigate("https://codebloom.patinanetwork.org/leaderboard?patina=true");
-            LOGGER.info("Loading page for screenshot...");
-
-            page.waitForTimeout(5_000);
-
-            byte[] screenshotBytes = page.screenshot(
-                            new Page.ScreenshotOptions().setType(ScreenshotType.PNG).setFullPage(true));
-            LeaderboardFilterOptions options = LeaderboardFilterOptions.builder()
-                                .page(1)
-                                .pageSize(5)
-                                .query("")
-                                .patina(true)
-                                .build();
-
-            List<UserWithScore> users = leaderboardRepository.getRecentLeaderboardUsers(options);
-
-            browser.close();
-
-            Leaderboard currentLeaderboard = leaderboardRepository.getRecentLeaderboardMetadata();
-            LocalDateTime shouldExpireByTime = Optional.ofNullable(currentLeaderboard.getShouldExpireBy())
-                            // this orElse will only trigger if leaderboard doesn't have expiration time.
-                            .orElse(StandardizedLocalDateTime.now());
-
-            Duration remaining = Duration.between(StandardizedLocalDateTime.now(), shouldExpireByTime);
-
-            long daysLeft = remaining.toDays();
-            long hoursLeft = remaining.toHours() % 24;
-            long minutesLeft = remaining.toMinutes() % 60;
-
-            String description = String.format("""
-                            Hey everyone! Here is a weekly update on the LeetCode leaderboard for our very own Patina members!
-
-                            🥇- <@%s> - %s pts
-                            🥈- <@%s> - %s pts
-                            🥉- <@%s> - %s pts
-
-                            To view the rest of the members, visit the website or check out the image embedded in this message!
-
-                            Just as a reminder, there's %d day(s), %d hour(s), and %d minute(s) left until the leaderboard closes, so keep grinding!
-
-
-                            See you next week!
-
-                            Beep boop,
-                            Codebloom
-                            <https://codebloom.patinanetwork.org>
-                            """,
-                            users.get(0).getDiscordId(),
-                            users.get(0).getTotalScore(),
-                            users.get(1).getDiscordId(),
-                            users.get(1).getTotalScore(),
-                            users.get(2).getDiscordId(),
-                            users.get(2).getTotalScore(),
-                            daysLeft,
-                            hoursLeft,
-                            minutesLeft);
-
-            MessageEmbed embed = new EmbedBuilder()
-                            .setTitle("🏆 Patina Leaderboard - " + currentLeaderboard.getName())
-                            .setDescription(description)
-                            .setFooter("Codebloom - LeetCode Leaderboard for Patina Network", "https://codebloom.patinanetwork.org/favicon.ico")
-                            .setImage("attachment://leaderboard.png")
-                            .setColor(new Color(69, 129, 103)).build();
-
-            LOGGER.info("Message has been built, ready to send...");
-
-            channel.sendFiles(FileUpload.fromData(screenshotBytes, "leaderboard.png"))
-                            .setEmbeds(embed)
-                            .queue();
-
-            LOGGER.info("Message has been queued");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        log.info("Message has been queued");
     }
 }
--- src/main/java/com/patina/codebloom/utilities/GlobalExceptionHandler.java
+++ src/main/java/com/patina/codebloom/utilities/GlobalExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.patina.codebloom.utilities;
 
+import java.awt.Color;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,18 +12,63 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.patina.codebloom.common.dto.ApiResponder;
+import com.patina.codebloom.common.env.Env;
+import com.patina.codebloom.common.time.StandardizedLocalDateTime;
+import com.patina.codebloom.jda.client.JDAClient;
+import com.patina.codebloom.jda.client.options.EmbeddedMessageOptions;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final JDAClient jdaClient;
+    private final Env env;
+
+    public GlobalExceptionHandler(final JDAClient jdaClient, final Env env) {
+        this.jdaClient = jdaClient;
+        this.env = env;
+    }
+
+    private byte[] getStackTraceAsBytes(final Throwable throwable) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        PrintStream ps = new PrintStream(baos, true, StandardCharsets.UTF_8)) {
+            throwable.printStackTrace(ps);
+            ps.flush();
+            return baos.toByteArray();
+        } catch (Exception e) {
+            return ("Failed to capture stack trace: " + e.getMessage()).getBytes(StandardCharsets.UTF_8);
+        }
+    }
 
     @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<?> handleResponseStatusException(final ResponseStatusException ex) {
+    public ResponseEntity<ApiResponder<?>> handleResponseStatusException(final ResponseStatusException ex) {
         return ResponseEntity.status(ex.getStatusCode()).body(ApiResponder.failure(ex.getReason()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(final RuntimeException rx) {
+    public ResponseEntity<ApiResponder<?>> handleRuntimeException(final RuntimeException rx) {
         rx.printStackTrace();
+
+        if (env.isProd()) {
+            String description = String.format("""
+                            An error occured in Codebloom.
+
+                            Active profile(s): %s
+                            Current Time: %s
+
+                            Check attachment for stack trace.""",
+                            env.getActiveProfiles(),
+                            StandardizedLocalDateTime.now().toString());
+            jdaClient.sendEmbedWithImage(
+                            EmbeddedMessageOptions.builder()
+                                            .guildId(jdaClient.getJdaReportingProperties().getGuildId())
+                                            .channelId(jdaClient.getJdaReportingProperties().getChannelId())
+                                            .title("Something went wrong!")
+                                            .description(description)
+                                            .color(Color.RED)
+                                            .fileName("stacktrace.txt")
+                                            .fileBytes(getStackTraceAsBytes(rx))
+                                            .build());
+        }
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponder.failure(rx.getMessage()));
     }
 }
