ce74762f310c24ddedeee8cc3d08a9f006db3281
Angela Yu <angelayu0530@gmail.com>
2025-08-07 16:04:29 -0400

    commitingso i can checkout prod

---
+++ db/migration/V32__Create-achivevement_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE achievement (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES "user"(id),
+    icon_url TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ
+);
