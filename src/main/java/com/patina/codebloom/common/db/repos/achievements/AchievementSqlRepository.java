e42f5465430a62aa0230324aba55b2c8c69ddb7f
Angela Yu <angelayu0530@gmail.com>
2025-08-09 11:14:11 -0400

    new repo file and changes to Achievement SQl

--- src/main/java/com/patina/codebloom/common/db/repos/achievements/AchievementSqlRepository.java
+++ src/main/java/com/patina/codebloom/common/db/repos/achievements/AchievementSqlRepository.java
@@ -1,4 +1,4 @@
-package com.patina.codebloom.common.db.repos.achivenents;
+package com.patina.codebloom.common.db.repos.achievements;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -15,22 +15,26 @@ import com.patina.codebloom.common.db.helper.NamedPreparedStatement;
 import com.patina.codebloom.common.db.models.achievements.Achievement;
 
 @Component
-public class AchievementSqlRepository {
+public class AchievementSqlRepository implements AchievementRepository {
     private final Connection conn;
 
     public AchievementSqlRepository(final DbConnection dbConnection) {
         this.conn = dbConnection.getConn();
     }
 
+    @Override
     public void createAchievement(final Achievement achievement) {
         String sql = """
-                            INSERT INTO achievement
+                            INSERT INTO Achievement
                                 (id, user_id, icon_url, title, description, is_active, created_at, deleted_at)
                             VALUES
-                               (?, ?, ?, ?, ?, ?,?, ?)
+                               (?, ? , ? , ? , ? , ? , ? , ? )
                             RETURNING created_at
                         """;
+        
         achievement.setId(UUID.randomUUID().toString());
+        achievement.setCreatedAt(OffsetDateTime.now());
+        
         try (NamedPreparedStatement stmt = new NamedPreparedStatement(conn, sql)) {
             stmt.setObject("id", UUID.fromString(achievement.getId()));
             stmt.setObject("user_id", UUID.fromString(achievement.getUserId()));
@@ -40,6 +44,7 @@ public class AchievementSqlRepository {
             stmt.setBoolean("is_active", achievement.isActive());
             stmt.setObject("created_at", achievement.getCreatedAt());
             stmt.setObject("deleted_at", achievement.getDeletedAt());
+            
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
                     achievement.setCreatedAt(rs.getObject("created_at", OffsetDateTime.class));
@@ -50,18 +55,20 @@ public class AchievementSqlRepository {
         }
     }
 
-    public boolean updateAchievement(final Achievement achievement) {
+    @Override
+    public boolean updateAchievementById(final Achievement achievement) {
         String sql = """
-                            UPDATE achievement
+                            UPDATE Achievement
                             SET
                                 icon_url = ?,
                                 title = ?,
                                 description = ?,
-                                is_active = ?,
+                                is_active = :?,
                                 deleted_at = ?,
                             WHERE
-                                id = ?
+                                id = ?,
                         """;
+        
         try (NamedPreparedStatement stmt = new NamedPreparedStatement(conn, sql)) {
             stmt.setString("icon_url", achievement.getIconUrl());
             stmt.setString("title", achievement.getTitle());
@@ -69,6 +76,7 @@ public class AchievementSqlRepository {
             stmt.setBoolean("is_active", achievement.isActive());
             stmt.setObject("deleted_at", achievement.getDeletedAt());
             stmt.setObject("id", UUID.fromString(achievement.getId()));
+            
             int rowsAffected = stmt.executeUpdate();
             return rowsAffected > 0;
         } catch (SQLException e) {
@@ -76,6 +84,7 @@ public class AchievementSqlRepository {
         }
     }
 
+    @Override
     public Achievement getAchievementById(final String id) {
         String sql = """
                             SELECT
@@ -90,11 +99,12 @@ public class AchievementSqlRepository {
                             FROM Achievement
                             WHERE id = ?
                         """;
+        
         try (NamedPreparedStatement stmt = new NamedPreparedStatement(conn, sql)) {
             stmt.setObject("id", UUID.fromString(id));
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
-                    return mapRow(rs);
+                    return parseResultSetToAchievement(rs);
                 }
             }
         } catch (SQLException e) {
@@ -103,26 +113,31 @@ public class AchievementSqlRepository {
         return null;
     }
 
+    @Override
     public List<Achievement> getAchievementsByUserId(final String userId) {
         String sql = """
                             SELECT
-                                id,
-                                user_id,
-                                icon_url,
-                                title,
-                                description,
-                                is_active,
-                                created_at,
-                                deleted_at
+                                a.id,
+                                a.user_id,
+                                a.icon_url,
+                                a.title,
+                                a.description,
+                                a.is_active,
+                                a.created_at,
+                                a.deleted_at
                             FROM Achievement a
-                            WHERE a.id = ?
+                            JOIN
+                            "User" u ON a."userId" = a.id
+                        WHERE
+                            "userId" = ?
                         """;
+        
         List<Achievement> achievements = new ArrayList<>();
         try (NamedPreparedStatement stmt = new NamedPreparedStatement(conn, sql)) {
             stmt.setObject("user_id", UUID.fromString(userId));
             try (ResultSet rs = stmt.executeQuery()) {
                 while (rs.next()) {
-                    achievements.add(mapRow(rs));
+                    achievements.add(parseResultSetToAchievement(rs));
                 }
             }
         } catch (SQLException e) {
@@ -131,15 +146,19 @@ public class AchievementSqlRepository {
         return achievements;
     }
 
+    @Override
     public boolean deleteAchievementById(final String id) {
-               String sql = """
+                String sql = """
                             DELETE FROM
                                 "Achievement"
                             WHERE
                                 id = ?
                         """;
+        
         try (NamedPreparedStatement stmt = new NamedPreparedStatement(conn, sql)) {
+            stmt.setObject("deleted_at", OffsetDateTime.now());
             stmt.setObject("id", UUID.fromString(id));
+            
             int rowsAffected = stmt.executeUpdate();
             return rowsAffected > 0;
         } catch (SQLException e) {
@@ -147,16 +166,16 @@ public class AchievementSqlRepository {
         }
     }
 
-    private Achievement mapRow(ResultSet rs) throws SQLException {
+    private Achievement parseResultSetToAchievement(ResultSet rs) throws SQLException {
         return Achievement.builder()
-                        .id(rs.getObject("id").toString())
-                        .userId(rs.getObject("user_id").toString())
-                        .iconUrl(rs.getString("icon_url"))
-                        .title(rs.getString("title"))
-                        .description(rs.getString("description"))
-                        .isActive(rs.getBoolean("is_active"))
-                        .createdAt(rs.getObject("created_at", OffsetDateTime.class))
-                        .deletedAt(rs.getObject("deleted_at", OffsetDateTime.class))
-                        .build();
+                .id(rs.getObject("id").toString())
+                .userId(rs.getObject("user_id").toString())
+                .iconUrl(rs.getString("icon_url"))
+                .title(rs.getString("title"))
+                .description(rs.getString("description"))
+                .isActive(rs.getBoolean("is_active"))
+                .createdAt(rs.getObject("created_at", OffsetDateTime.class))
+                .deletedAt(rs.getObject("deleted_at", OffsetDateTime.class))
+                .build();
     }
 }
