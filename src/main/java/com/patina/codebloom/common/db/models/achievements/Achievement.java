9854f3c88b718b64d9f1c9cf57e704a53539ce25
Angela Yu <angelayu0530@gmail.com>
2025-08-10 15:20:18 -0400

    Changes based off copoliot

--- src/main/java/com/patina/codebloom/common/db/models/achievements/Achievement.java
+++ src/main/java/com/patina/codebloom/common/db/models/achievements/Achievement.java
@@ -4,20 +4,24 @@ import java.time.OffsetDateTime;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Builder
 @Getter
+@Setter
 @ToString
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Achievement {
-    
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
-    private @Setter String id;
+    private String id;
 
     @NotNull
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
@@ -34,11 +38,12 @@ public class Achievement {
     @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true)
     private String description;
 
+    @Builder.Default
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
-    private boolean isActive;
+    private boolean isActive = true;
 
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
-    private @Setter OffsetDateTime createdAt;
+    private OffsetDateTime createdAt;
 
     @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true)
     private OffsetDateTime deletedAt;
