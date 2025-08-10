0229a4c8442dabd447918c046c06e30a1da22d7c
Angela Yu <angelayu0530@gmail.com>
2025-08-10 19:27:30 -0400

    naming changes

--- db/migration/V33__Create_achivevement_table.sql
+++ db/migration/V33__Create_achivevement_table.sql
@@ -1,10 +1,12 @@
-CREATE TABLE IF NOT EXISTS "Achievement" (
-    id UUID NOT NULL PRIMARY KEY,
-    userId UUID NOT NULL,
-    iconUrl TEXT NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT,
-    isActive BOOLEAN NOT NULL DEFAULT TRUE,
-    createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    deletedAt TIMESTAMPTZ
+DROP TABLE IF EXISTS "Achievement";
+
+CREATE TABLE "Achievement" (
+    id UUID PRIMARY KEY,
+    "userId" UUID NOT NULL,
+    "iconUrl" VARCHAR(255),
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+    "isActive" BOOLEAN DEFAULT true,
+    "createdAt" TIMESTAMPTZ DEFAULT NOW(),
+    "deletedAt" TIMESTAMPTZ
 );
