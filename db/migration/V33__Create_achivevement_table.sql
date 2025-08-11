30c00203ecff6ddbab2dd0bb0a310811a1492982
Angela Yu <angelayu0530@gmail.com>
2025-08-11 08:45:21 -0400

    changes based off pr review

--- db/migration/V33__Create_achivevement_table.sql
+++ db/migration/V33__Create_achivevement_table.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS "Achievement";
 CREATE TABLE "Achievement" (
     id UUID PRIMARY KEY,
     "userId" UUID NOT NULL,
+    CONSTRAINT "fk_user" FOREIGN KEY ("userId") REFERENCES "User"(id) ON DELETE CASCADE ON UPDATE CASCADE
     "iconUrl" VARCHAR(255),
     "title" VARCHAR(255) NOT NULL,
     "description" TEXT,
