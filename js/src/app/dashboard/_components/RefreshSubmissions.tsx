bf331d6a921b5abf1d0cc9d2af6077fdcff8ae2f
Nancy Huang <205217630+naanci@users.noreply.github.com>
2025-08-17 17:42:35 -0400

    Added a dismiss (x) button to hide the (register university email) button

--- js/src/app/dashboard/_components/RefreshSubmissions.tsx
+++ js/src/app/dashboard/_components/RefreshSubmissions.tsx
@@ -3,6 +3,7 @@ import useCountdown from "@/lib/hooks/useCountdown";
 import { Button, Center, Text } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { notifications } from "@mantine/notifications";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function RefreshSubmissions({
@@ -14,6 +15,19 @@ export default function RefreshSubmissions({
   const [countdown, resetCountdown] = useCountdown(0);
 
   const isDisabled = countdown > 0;
+
+  const [hideBtn, setHideBtn] = useState(false);
+
+  useEffect(() => {
+    if(localStorage.getItem("hideUniversityButton") === "true") {
+        setHideBtn(true);
+    }
+  }, []);
+
+  const handleHideButton = () => {
+    setHideBtn(true);
+    localStorage.setItem("hideUniversityButton", "true");
+  };
 
   const form = useForm({
     initialValues: {
@@ -55,20 +69,48 @@ export default function RefreshSubmissions({
           `Please wait ${countdown} seconds to refresh again.`
         : "Refresh your latest submissions!"}
       </Button>
-      {!schoolRegistered && (
+      {!schoolRegistered && !hideBtn && (
         <Center>
-          <Button
-            component={Link}
-            variant={"light"}
-            to={"/settings"}
-            size={"sm"}
-            mb={"sm"}
-            style={{
-              cursor: "pointer",
-            }}
-          >
+            <div style={{position: "relative", display: "incline-block"}}>
+                <Button
+                component={Link}
+                variant={"light"}
+                to={"/settings"}
+                size={"sm"}
+                mb={"sm"}
+                style={{
+                cursor: "pointer", }}
+                >
             Go to settings {"&"} register your university email
           </Button>
+
+          <button
+            type = "button"
+            onClick={handleHideButton}
+            style={{
+              top: -10,
+              right: -10,
+              zIndex: 2,
+              color: "white",
+              background: "#b91c1c",
+              width: 24,
+              height: 24,
+              fontSize: 14,
+              fontWeight: 700,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              lineHeight: 1,
+              textAlign: "center",
+              cursor: "pointer",
+              borderRadius: "50%",
+              border: "none",
+              position: "absolute",
+            }}
+            >
+                x
+            </button>
+            </div>
         </Center>
       )}
       <Text
@@ -77,7 +119,7 @@ export default function RefreshSubmissions({
           maxWidth: "640px",
         }}
         ta={"center"}
-      >
+        >
         Please note that we automatically query Leetcode's APIs behind the
         scenes to make sure you can stay on that LeetCode grind without having
         to press this button everytime!
