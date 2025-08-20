0901f42bd58b0c162ef8515f45cb88621d6f4844
Angela Yu <angelayu0530@gmail.com>
2025-08-20 16:32:13 -0400

    changes to achievement table correct way

---
+++ db/migration/V36__Change_achievement_table.SQL
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "Achievement"  (
+    id UUID PRIMARY KEY,
+    "userId" UUID NOT NULL,
+    CONSTRAINT "fk_user" FOREIGN KEY ("userId") REFERENCES "User"(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    "iconUrl" VARCHAR(255),
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+    "isActive" BOOLEAN DEFAULT true,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW (),
+    "deletedAt" TIMESTAMP WITH TIME ZONE NULL
+);
