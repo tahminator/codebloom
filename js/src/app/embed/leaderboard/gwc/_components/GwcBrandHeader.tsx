90ed18b1831c01c0f6c2f5f22fa3bc0bece0736f
Alfardil Alam <fardilfayed@gmail.com>
2025-08-25 02:12:15 -0400

    Added a GWC Embed for GWC to use on their website

---
+++ js/src/app/embed/leaderboard/gwc/_components/GwcBrandHeader.tsx
@@ -0,0 +1,15 @@
+import { Box, Center, Image, Text } from "@mantine/core";
+
+export default function GwcBrandHeader() {
+  return (
+    <Box p={"md"}>
+      <Center>
+        <Image src="/Gwc_Logo.png" alt="GWC" h={44} w="auto" radius="xs" />
+        <Text fw={700} size="xl" p={"md"}>
+          ×
+        </Text>
+        <Image src="/logo.png" alt="Codebloom" h={44} w="auto" radius="xs" />
+      </Center>
+    </Box>
+  );
+}
