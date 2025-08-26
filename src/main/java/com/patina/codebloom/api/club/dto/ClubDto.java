4e2ed8307cc159005a42f44829fd6ef59677f9c8
Arshadul Monir <arshadulmonir@gmail.com>
2025-08-25 23:00:57 -0400

    changed club dto to class

--- src/main/java/com/patina/codebloom/api/club/dto/ClubDto.java
+++ src/main/java/com/patina/codebloom/api/club/dto/ClubDto.java
@@ -2,12 +2,16 @@ package com.patina.codebloom.api.club.dto;
 
 import com.patina.codebloom.common.db.models.usertag.Tag;
 
-// Club data that is safe to expose
-public record ClubDto(
-        String id,
-        String name,
-        String description,
-        String slug,
-        String splashIconUrl,
-        Tag tag 
-) {}
+import lombok.Value;
+import lombok.Builder;
+
+@Value
+@Builder
+public class ClubDto {
+    String id;
+    String name;
+    String description;
+    String slug;
+    String splashIconUrl;
+    Tag tag;
+}
