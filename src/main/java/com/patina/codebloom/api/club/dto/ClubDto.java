6c0838d0c9789a56287fe1ad3ce8b387dd7759f9
Arshadul Monir <arshadulmonir@gmail.com>
2025-08-29 22:32:01 -0400

    Backend checkstyle

--- src/main/java/com/patina/codebloom/api/club/dto/ClubDto.java
+++ src/main/java/com/patina/codebloom/api/club/dto/ClubDto.java
@@ -4,14 +4,16 @@ import com.patina.codebloom.common.db.models.usertag.Tag;
 
 import lombok.Value;
 import lombok.Builder;
+import lombok.Getter;
 
 @Value
+@Getter
 @Builder
 public class ClubDto {
-    String id;
-    String name;
-    String description;
-    String slug;
-    String splashIconUrl;
-    Tag tag;
+    private String id;
+    private String name;
+    private String description;
+    private String slug;
+    private String splashIconUrl;
+    private Tag tag;
 }
