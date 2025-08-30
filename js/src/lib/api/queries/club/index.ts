534af6b11834d8de45d9312b97d4e2e5c41b2733
Arshadul Monir <arshadulmonir@gmail.com>
2025-08-29 22:41:51 -0400

    Frontend checkstyle

--- js/src/lib/api/queries/club/index.ts
+++ js/src/lib/api/queries/club/index.ts
@@ -1,53 +1,52 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import { UnknownApiResponse } from "../../common/apiResponse";
 import { ClubDto } from "../../types/club";
 import { UserTag } from "../../types/usertag";
 
-export const useClubQuery = ({ clubSlug } : { clubSlug?: string }) => {
-    return useQuery({
-        queryKey: ["club", clubSlug],
-        queryFn: () => fetchClubDto({ clubSlug })
-    });
-}
+export const useClubQuery = ({ clubSlug }: { clubSlug?: string }) => {
+  return useQuery({
+    queryKey: ["club", clubSlug],
+    queryFn: () => fetchClubDto({ clubSlug }),
+  });
+};
 
 async function fetchClubDto({ clubSlug }: { clubSlug?: string }) {
-    const response = await fetch(`/api/club/${clubSlug}`)
+  const response = await fetch(`/api/club/${clubSlug}`);
 
-    const json = (await response.json()) as UnknownApiResponse<ClubDto>
+  const json = (await response.json()) as UnknownApiResponse<ClubDto>;
 
-    return json
+  return json;
 }
 
 export const useVerifyPasswordMutation = () => {
-    return useMutation({
-        mutationFn: verifyPassword,
-    })
-}
+  return useMutation({
+    mutationFn: verifyPassword,
+  });
+};
 
 async function verifyPassword({
-    userId,
-    password,
-    clubSlug,
+  userId,
+  password,
+  clubSlug,
 }: {
-    userId: string;
-    password: string;
-    clubSlug: string;
+  userId: string;
+  password: string;
+  clubSlug: string;
 }) {
-    const response = await fetch("/api/club/verify", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            userId,
-            password,
-            clubSlug
+  const response = await fetch("/api/club/verify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userId,
+      password,
+      clubSlug,
+    }),
+  });
 
-        })
-    });
+  const json = (await response.json()) as UnknownApiResponse<UserTag>;
 
-    const json = (await response.json()) as UnknownApiResponse<UserTag>;
-
-    return json;
+  return json;
 }
