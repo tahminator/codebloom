df349a56af48f68e72015b581bbd9c456082c660
Alfardil Alam <fardilfayed@gmail.com>
2025-09-02 20:17:30 -0400

    configured justfile and email generation path

--- email.sh
+++ email.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
 cd email \
   && pnpm i \
   && pnpm email export --dir emails --outDir out \
-  && mkdir -p ../src/main/resources/static \
-  && cp out/template.html ../src/main/resources/static
+  && mkdir -p ../src/main/resources/static/email \
+  && cp out/template.html ../src/main/resources/static/email
