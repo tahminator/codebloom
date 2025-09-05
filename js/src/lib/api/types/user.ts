c73c69862f242ecb8991fee779da2c459dd73c60
Arshadul Monir <arshadulmonir@gmail.com>
2025-09-05 19:45:06 -0400

    prettier changes

--- js/src/lib/api/types/user.ts
+++ js/src/lib/api/types/user.ts
@@ -3,4 +3,5 @@ import { components } from "@/lib/api/types/autogen/schema";
 export type User = components["schemas"]["User"];
 export type PrivateUser = components["schemas"]["PrivateUser"];
 export type UserWithScore = components["schemas"]["UserWithScore"];
-export type AuthenticationObject = components["schemas"]["AuthenticationObject"];
+export type AuthenticationObject =
+  components["schemas"]["AuthenticationObject"];
