d805778763ae9367e65364d66fafb5d3dba22f85
Angela Yu <angelayu0530@gmail.com>
2025-09-10 17:47:19 -0400

    pr fixes to user.ts

--- js/src/lib/api/types/user.ts
+++ js/src/lib/api/types/user.ts
@@ -3,3 +3,10 @@ import { components } from "@/lib/api/types/autogen/schema";
 export type User = components["schemas"]["User"];
 export type PrivateUser = components["schemas"]["PrivateUser"];
 export type UserWithScore = components["schemas"]["UserWithScore"];
+export type UserTag = components["schemas"]["UserTag"];
+
+
+import { UserTagTag } from "@/lib/api/types/autogen/schema";
+export { UserTagTag };
+
+export type UserTagTagWithoutGwc = Exclude<UserTagTag, UserTagTag.Gwc>;
