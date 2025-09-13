3295148dbd5035fada8bcfe970f2589653c0a2c3
Tahmid Ahmed <tahmid@tahmid.io>
2025-09-13 19:47:16 -0400

    Add ApiUtils & ApiTypeUtils, a collection of helper methods and types to standardize certain behaviors. (DESC)
    Some examples include getting metadata from a specific tag enum, like a pretty name or a string path to an icon, or stripping unsupported tags, and etc.

--- js/src/lib/api/types/user.ts
+++ js/src/lib/api/types/user.ts
@@ -1,9 +1,5 @@
-import { components, UserTagTag } from "@/lib/api/types/autogen/schema";
+import { components } from "@/lib/api/types/autogen/schema";
+
 export type User = components["schemas"]["User"];
 export type PrivateUser = components["schemas"]["PrivateUser"];
 export type UserWithScore = components["schemas"]["UserWithScore"];
-export type UserTag = components["schemas"]["UserTag"];
-
-export { UserTagTag };
-//TODO update this to be a little less brittle
-export type UserTagTagWithoutGwc = Exclude<UserTagTag, UserTagTag.Gwc>;
---
+++ js/src/lib/api/utils/types.ts
@@ -0,0 +1,41 @@
+import { UserTag, UserTagTag } from "@/lib/api/types/usertag";
+
+/**
+ * A collection of helpful types to help transform & use data returned from the API.
+ */
+export namespace ApiTypeUtils {
+  export type UserTagTagMetadata = {
+    /**
+     * Pretty name for the given tag.
+     */
+    name: string;
+
+    /**
+     * Path to tag's icon.
+     */
+    icon: string;
+
+    /**
+     * Extra information about the given tag.
+     */
+    alt: string;
+  };
+
+  /**
+   * The `tag` enum inside of `UserTag`, but stripped of all unsupported tags.
+   *
+   * @see {@link UserTagTag}
+   * @see {@link FilteredUserTag}
+   */
+  export type FilteredUserTagTag = Exclude<UserTagTag, UserTagTag.Gwc>;
+
+  /**
+   * Type override of the `UserTag` object, but stripped of all unsupported `tag` enums.
+   *
+   * @see {@link UserTag}
+   * @see {@link FilteredUserTagTag}
+   */
+  export type FilteredUserTag = UserTag & {
+    tag: FilteredUserTagTag;
+  };
+}
