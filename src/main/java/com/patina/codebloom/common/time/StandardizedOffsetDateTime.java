9a533547c88bd870779f930cab94df6e3c1a80fc
Nancy Huang <205217630+naanci@users.noreply.github.com>
2025-09-14 00:30:15 -0400

    check style in StandardizedOffsetDateTime

--- src/main/java/com/patina/codebloom/common/time/StandardizedOffsetDateTime.java
+++ src/main/java/com/patina/codebloom/common/time/StandardizedOffsetDateTime.java
@@ -8,6 +8,7 @@ public class StandardizedOffsetDateTime {
     public static OffsetDateTime nowOffset() {
         return OffsetDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.MILLIS);
     }
+
     public static OffsetDateTime from(OffsetDateTime dateTime) {
         return dateTime.withOffsetSameInstant(ZoneOffset.UTC).truncatedTo(ChronoUnit.MILLIS);
     }
