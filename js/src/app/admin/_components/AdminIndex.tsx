7e7be7dd1c9d00c80802b144b886ae8a72530180
Alfardil Alam <fardilfayed@gmail.com>
2025-09-14 22:46:31 -0400

    using auth query in the admin page. changed logic of deletion button on admin page'

--- js/src/app/admin/_components/AdminIndex.tsx
+++ js/src/app/admin/_components/AdminIndex.tsx
@@ -1,43 +1,11 @@
 import NewAnnouncementModal from "@/app/admin/_components/announcements/NewAnnouncementModal";
 import UserAdminList from "@/app/admin/_components/users/UserAdminList";
-import Toast from "@/components/ui/toast/Toast";
-import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
-import { useAuthQuery } from "@/lib/api/queries/auth";
-import { Flex, Loader, Title } from "@mantine/core";
+import { Flex, Title } from "@mantine/core";
 
-import DeleteAnnouncementModal from "./announcements/DeleteAnnouncementModal";
+import DeleteAnnouncementButton from "./announcements/DeleteAnnouncementButton";
 import AllLeaderboardsPage from "./leaderboards/pagination/AllLeaderboardAdmin";
-import { useLatestAnnouncement } from "@/lib/api/queries/announcement";
 
 export default function AdminIndex() {
-  const { data, status } = useAuthQuery();
-  const { data: announcementData } = useLatestAnnouncement();
-
-  if (status == "pending") {
-    return (
-      <div className="flex flex-col items-center justify-center w-screen h-screen">
-        <Loader />
-      </div>
-    );
-  }
-
-  if (status == "error") {
-    return <Toast message={"Something went wrong."} />;
-  }
-
-  const { isAdmin } = data;
-
-  if (!isAdmin) {
-    return (
-      <ToastWithRedirect
-        message={"You are not authorized to view this page."}
-        to="/"
-      />
-    );
-  }
-
-  const id = announcementData?.payload?.id;
-
   return (
     <Flex w={"98vw"} h={"100vh"} direction={"column"}>
       <Title order={1} ta="center">
@@ -47,7 +15,7 @@ export default function AdminIndex() {
         <NewAnnouncementModal />
       </Flex>
       <Flex w={"100%"} direction={"row"} justify={"center"}>
-        <DeleteAnnouncementModal id={id} />
+        <DeleteAnnouncementButton />
       </Flex>
       <Flex w={"100%"} direction={{ base: "column", sm: "row" }}>
         <Flex
