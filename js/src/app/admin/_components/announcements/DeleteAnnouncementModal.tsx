badaa4bc2d20b107a5bc7ec83962e6199e4d377c
Alfardil Alam <fardilfayed@gmail.com>
2025-09-15 09:32:14 -0400

    changed modal to pass in id directly instead of using form

--- js/src/app/admin/_components/announcements/DeleteAnnouncementModal.tsx
+++ js/src/app/admin/_components/announcements/DeleteAnnouncementModal.tsx
@@ -1,6 +1,5 @@
 import { useDeleteAnnouncementMutation } from "@/lib/api/queries/admin";
 import { Modal, Text, Button } from "@mantine/core";
-import { useForm } from "@mantine/form";
 import { notifications } from "@mantine/notifications";
 
 export default function DeleteAnnouncementModal({
@@ -8,36 +7,21 @@ export default function DeleteAnnouncementModal({
   opened,
   onClose,
 }: {
-  id?: string;
+  id: string;
   opened: boolean;
   onClose: () => void;
 }) {
   const { mutate, status } = useDeleteAnnouncementMutation();
 
-  const form = useForm({
-    initialValues: {
-      id: id,
-    },
-  });
-
   const onSubmit = async () => {
-    if (!form.values.id) {
-      notifications.show({
-        message: "No active announcement to disable.",
-        color: "red",
-      });
-      return;
-    }
-
     mutate(
-      { id: form.values.id },
+      { id: id },
       {
         onSuccess: async (data) => {
           notifications.show({
             message: data.message,
             color: data.success ? undefined : "red",
           });
-          form.reset();
           onClose();
         },
       },
@@ -46,7 +30,7 @@ export default function DeleteAnnouncementModal({
 
   return (
     <Modal opened={opened} onClose={onClose} title="Disable Announcement">
-      <form onSubmit={form.onSubmit(onSubmit)}>
+      <form onSubmit={onSubmit}>
         <Text mt={12}>
           Are you sure you want to disable the latest announcement?
         </Text>
