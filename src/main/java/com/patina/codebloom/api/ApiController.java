7f1c0a449bd3d4cff9a116464c8244746d1c1339
Tahmid Ahmed <tahmid@tahmid.io>
2025-09-20 18:47:08 -0400

    test log inside of /api (to test on staging)

--- src/main/java/com/patina/codebloom/api/ApiController.java
+++ src/main/java/com/patina/codebloom/api/ApiController.java
@@ -1,11 +1,16 @@
 package com.patina.codebloom.api;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patina.codebloom.common.dto.ApiResponder;
+import com.patina.codebloom.common.reporter.Reporter;
+import com.patina.codebloom.common.reporter.report.Report;
+import com.patina.codebloom.common.reporter.report.location.Location;
 import com.patina.codebloom.utilities.ServerMetadataObject;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,9 +22,22 @@ import jakarta.servlet.http.HttpServletRequest;
 @Tag(name = "Basic server metadata")
 public class ApiController {
 
+    final Reporter reporter;
+
+    public ApiController(final Reporter reporter) {
+        this.reporter = reporter;
+    }
+
     @Operation(summary = "Basic metadata about the server")
     @GetMapping()
     public ResponseEntity<ApiResponder<ServerMetadataObject>> apiIndex(final HttpServletRequest request) {
+        reporter.log(
+                        Report.builder()
+                                        .data("This is a test")
+                                        .environments(List.of("Testenv"))
+                                        .location(Location.BACKEND)
+                                        .build());
+
         return ResponseEntity.ok().body(ApiResponder.success("Hello from Codebloom!", new ServerMetadataObject()));
     }
 
