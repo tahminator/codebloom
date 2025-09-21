df6c779be004cbc8ddbe9bc37e08e8f088fb744a
Arshadul Monir <arshadulmonir@gmail.com>
2025-09-21 14:22:16 -0400

    Removed Comments

--- js/src/app/user/[userId]/_components/UserProfile/UserTags/UserTagsSkeleton.tsx
+++ js/src/app/user/[userId]/_components/UserProfile/UserTags/UserTagsSkeleton.tsx
@@ -3,7 +3,6 @@ import { Skeleton } from "@mantine/core";
 export default function UserTagsSkeleton() {
   return (
     <div className="flex flex-wrap gap-2">
-      {/* Fake tag pills */}
       <Skeleton height={40} width={40} />
       <Skeleton height={40} width={40} />
       <Skeleton height={40} width={40} />
