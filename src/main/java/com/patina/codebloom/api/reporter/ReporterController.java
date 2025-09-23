8647f52989220a12b990d6392944e41d1ddd167b
Tahmid Ahmed <tahmid@tahmid.io>
2025-09-23 00:55:28 -0400

    Implement a frontend reporter that will pick up on any frontend render runtime errors. It's rudimentary (only minified info), but it's a start and more importantly, will give us an alert if something is going wrong

--- src/main/java/com/patina/codebloom/api/reporter/ReporterController.java
+++ src/main/java/com/patina/codebloom/api/reporter/ReporterController.java
@@ -1,50 +1,89 @@
 package com.patina.codebloom.api.reporter;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import com.patina.codebloom.api.reporter.body.IngestErrorsBody;
+import com.patina.codebloom.api.reporter.body.IngestLogBody;
 import com.patina.codebloom.common.dto.ApiResponder;
 import com.patina.codebloom.common.dto.Empty;
+import com.patina.codebloom.common.env.Env;
 import com.patina.codebloom.common.reporter.Reporter;
+import com.patina.codebloom.common.reporter.report.Report;
+import com.patina.codebloom.common.reporter.report.location.Location;
+import com.patina.codebloom.common.url.ServerUrlUtils;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@Tag(name = "Error reporting route", description = "This controller allows the frontend to report any runtime errors, which will then be ingested by the server.")
-@RequestMapping("/api/reporting")
+@Tag(name = "Reporter route", description = """
+                This controller allows the frontend to report anything at runtime (log or error), which will then be ingested by the server.""")
+@RequestMapping("/api/reporter")
 public class ReporterController {
-    private final Reporter errorReporter;
+    private final Reporter reporter;
+    private final Env env;
+    private final ServerUrlUtils serverUrlUtils;
 
-    public ReporterController(final Reporter errorReporter) {
-        this.errorReporter = errorReporter;
+    public ReporterController(final Reporter reporter, final Env env, final ServerUrlUtils serverUrlUtils) {
+        this.reporter = reporter;
+        this.env = env;
+        this.serverUrlUtils = serverUrlUtils;
     }
 
-    @PostMapping("")
-    public ResponseEntity<ApiResponder<Empty>> ingestErrors(final @RequestBody IngestErrorsBody ingestErrorsBody) {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        // List<String> traces = ingestErrorsBody.getTraces();
-        //
-        // if (traces.size() > 3) {
-        // String combined = String.join("\n\n", traces);
-        // errorReporter.report(Report.builder()
-        // .location(Location.FRONTEND)
-        // .stackTrace(combined.getBytes())
-        // .build());
-        // } else {
-        // for (String trace : traces) {
-        // errorReporter.report(Report.builder()
-        // .location(Location.FRONTEND)
-        // .stackTrace(trace.getBytes())
-        // .build());
-        // }
-        // }
-        //
-        // return ResponseEntity.ok(ApiResponder.success("Received!", Empty.of()));
+    private boolean validateOrigin(final HttpServletRequest request) {
+        if (!env.isProd()) {
+            return true;
+        }
+
+        String origin = request.getHeader("Origin");
+        String serverUrl = serverUrlUtils.getUrl();
+
+        System.out.println(String.format("%s %s", origin, serverUrl));
+
+        if (origin == null) {
+            return false;
+        }
+
+        if (!origin.equals(serverUrl)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @PostMapping("/error")
+    public ResponseEntity<ApiResponder<Empty>> ingestErrors(final @RequestBody IngestErrorsBody ingestErrorBody, final HttpServletRequest request) {
+        if (!validateOrigin(request)) {
+            // don't let the bad actor know it failed.
+            return ResponseEntity.ok(ApiResponder.success("ok", Empty.of()));
+        }
+
+        reporter.error(Report.builder()
+                        .data(ingestErrorBody.getTrace())
+                        .environments(env.getActiveProfiles())
+                        .location(Location.FRONTEND)
+                        .build());
+        return ResponseEntity.ok(ApiResponder.success("ok", Empty.of()));
+    }
+
+    @PostMapping("/log")
+    public ResponseEntity<ApiResponder<Empty>> ingestLog(final @RequestBody IngestLogBody ingestLogBody, final HttpServletRequest request) {
+        if (!validateOrigin(request)) {
+            // don't let the bad actor know it failed.
+            return ResponseEntity.ok(ApiResponder.success("ok", Empty.of()));
+        }
+
+        reporter.log(
+                        Report.builder()
+                                        .data(ingestLogBody.getInfo())
+                                        .environments(env.getActiveProfiles())
+                                        .location(Location.FRONTEND)
+                                        .build());
+        return ResponseEntity.ok(ApiResponder.success("ok", Empty.of()));
     }
 }
