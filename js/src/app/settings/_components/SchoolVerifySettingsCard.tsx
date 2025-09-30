97fdec9be60362e8d39e352673996488efbdf358
Alisha Zaman <az2924@nyu.edu>
2025-09-30 16:58:51 -0400

    Added title and description for error and school page

--- js/src/app/settings/_components/SchoolVerifySettingsCard.tsx
+++ js/src/app/settings/_components/SchoolVerifySettingsCard.tsx
@@ -25,37 +25,41 @@ export default function SchoolVerifySettingsCard({
   const [modalOpen, setModalOpen] = useState(false);
   const toggleModal = () => setModalOpen((prev) => !prev);
   return (
-    <Box>
-      <Card withBorder padding={"md"} radius={"md"}>
-        <Box m={"md"}>
-          <Title order={3} p={"md"}>
-            Verify School
-          </Title>
-          <Text pt={"md"} pl={"md"}>
-            Verify your school email here for access to our school specific
-            leaderboards.
-          </Text>
-          <Text pt={"md"} pl={"md"}>
-            Supported schools:
-            <List>
-              {ApiUtils.getAllSupportedTagEnumMetadata().map((school) => (
-                <List.Item key={school.name}>
-                  <Flex gap={"sm"} direction={"row"}>
-                    <Image src={school.icon} alt={school.alt} h={20} w={20} />
-                    {school.name}
-                  </Flex>
-                </List.Item>
-              ))}
-            </List>
-          </Text>
-          <Box pt={"sm"} pl={"md"}>
-            <Button mt="sm" onClick={toggleModal} disabled={schoolExists}>
-              {schoolExists ? "You are already verified!" : "Verify Now"}
-            </Button>
+    <>
+      <DocumentTitle title={`CodeBloom - Verify School`} />
+      <DocumentDescription description={`CodeBloom - Verify school email`} />
+      <Box>
+        <Card withBorder padding={"md"} radius={"md"}>
+          <Box m={"md"}>
+            <Title order={3} p={"md"}>
+              Verify School
+            </Title>
+            <Text pt={"md"} pl={"md"}>
+              Verify your school email here for access to our school specific
+              leaderboards.
+            </Text>
+            <Text pt={"md"} pl={"md"}>
+              Supported schools:
+              <List>
+                {ApiUtils.getAllSupportedTagEnumMetadata().map((school) => (
+                  <List.Item key={school.name}>
+                    <Flex gap={"sm"} direction={"row"}>
+                      <Image src={school.icon} alt={school.alt} h={20} w={20} />
+                      {school.name}
+                    </Flex>
+                  </List.Item>
+                ))}
+              </List>
+            </Text>
+            <Box pt={"sm"} pl={"md"}>
+              <Button mt="sm" onClick={toggleModal} disabled={schoolExists}>
+                {schoolExists ? "You are already verified!" : "Verify Now"}
+              </Button>
+            </Box>
+            <SchoolEmailModal enabled={modalOpen} toggle={toggleModal} />
           </Box>
-          <SchoolEmailModal enabled={modalOpen} toggle={toggleModal} />
-        </Box>
-      </Card>
-    </Box>
+        </Card>
+      </Box>
+    </>
   );
 }
