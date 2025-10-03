361bdc295bfbf242f9da8a68d9b810dfc6813552
Tahmid Ahmed <tahmid@tahmid.io>
2025-10-02 23:24:08 -0400

    Add JSDoc to Api generic type

--- js/src/lib/api/types/index.ts
+++ js/src/lib/api/types/index.ts
@@ -4,4 +4,16 @@ type Schemas = components["schemas"];
 
 export type TApiSchemaKey = keyof Schemas;
 
+/**
+ * Select an API type. Check example below on how to use `Api`.
+ *
+ * @example
+ * ```ts
+ * // old way
+ * const json = (await response.json()) as UnknownApiResponse<UserDto>; // UserDto must be exported manually.
+ *
+ * // new way
+ * const json = (await response.json()) as UnknownApiResponse<Api<"UserDto">>; // Access any generated API type.
+ * ```
+ */
 export type Api<T extends TApiSchemaKey> = components["schemas"][T];
