0bcf6c52bcb68afe443a2c8bdb4431442fc0b37e
Tahmid Ahmed <tahmid@tahmid.io>
2025-10-02 23:29:32 -0400

    Checkstyle fix + allow checkstyle opt out via comment

--- src/main/java/com/patina/codebloom/common/db/models/user/UserWithScore.java
+++ src/main/java/com/patina/codebloom/common/db/models/user/UserWithScore.java
@@ -1,9 +1,5 @@
 package com.patina.codebloom.common.db.models.user;
 
-import java.util.ArrayList;
-
-import com.patina.codebloom.common.db.models.usertag.UserTag;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
