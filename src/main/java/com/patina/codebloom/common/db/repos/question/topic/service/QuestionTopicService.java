0b0b68a26663fec3135ff87c1b7ecc96733b2ba9
Arshadul Monir <arshadulmonir@gmail.com>
2025-10-03 00:37:35 -0400

    removed unused function

--- src/main/java/com/patina/codebloom/common/db/repos/question/topic/service/QuestionTopicService.java
+++ src/main/java/com/patina/codebloom/common/db/repos/question/topic/service/QuestionTopicService.java
@@ -9,14 +9,6 @@ import com.patina.codebloom.common.db.models.question.topic.LeetcodeTopicEnum;
 @Service
 public class QuestionTopicService {
 
-    public LeetcodeTopicEnum stringtoEnum(final String topic) {
-        try {
-            return LeetcodeTopicEnum.valueOf(topic);
-        } catch (IllegalArgumentException e) {
-            return LeetcodeTopicEnum.UNKNOWN;
-        }
-    }
-
     public LeetcodeTopicEnum[] stringsToEnums(final Set<String> topics) {
         return topics.stream()
                         .map(s -> {
