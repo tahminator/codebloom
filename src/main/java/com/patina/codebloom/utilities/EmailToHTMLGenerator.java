9aa2a2fe3b9d7b889dcef2199bab6a1e1f4975ba
Alfardil Alam <fardilfayed@gmail.com>
2025-10-04 15:53:23 -0400

    added implementation

--- src/main/java/com/patina/codebloom/utilities/EmailToHTMLGenerator.java
+++ src/main/java/com/patina/codebloom/utilities/EmailToHTMLGenerator.java
@@ -1,6 +1,7 @@
 package com.patina.codebloom.utilities;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
@@ -11,10 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 @Profile("dev")
 @Component
 @Slf4j
-public class EmailToHTMLGenerator {
+public class EmailToHTMLGenerator implements CommandLineRunner {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Override
     public void run(final String... args) throws Exception {
         log.info("Generating HTML from React email template.");
         Process process = new ProcessBuilder("just", "email-gen")
