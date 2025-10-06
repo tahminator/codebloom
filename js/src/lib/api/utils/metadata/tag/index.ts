d3435b645f8cefe4d3ce70d706c3b200f872e9b6
Arshadul Monir <arshadulmonir@gmail.com>
2025-10-05 20:21:41 -0400

    added apiKey back to tag metadata

--- js/src/lib/api/utils/metadata/tag/index.ts
+++ js/src/lib/api/utils/metadata/tag/index.ts
@@ -1,4 +1,4 @@
-import { UserTagTag } from "@/lib/api/types/usertag";
+import { UserTagTag } from "@/lib/api/types/autogen/schema";
 import { ApiTypeUtils } from "@/lib/api/utils/types";
 
 /**
@@ -11,60 +11,70 @@ export const TAG_METADATA_LIST: Record<
   Hunter: {
     shortName: "Hunter",
     name: "Hunter College",
+    apiKey: "hunter",
     icon: "/brands/Hunter_Logo.jpeg",
     alt: "Hunter College Logo",
   },
   Nyu: {
     shortName: "NYU",
     name: "New York University",
+    apiKey: "nyu",
     icon: "/brands/NYU_Logo.png",
     alt: "NYU Logo",
   },
   Baruch: {
     shortName: "Baruch",
     name: "Baruch College",
+    apiKey: "baruch",
     icon: "/brands/Baruch_Logo.png",
     alt: "Baruch College Logo",
   },
   Rpi: {
     shortName: "RPI",
     name: "Rensselaer Polytechnic Institute",
+    apiKey: "rpi",
     icon: "/brands/Rpi_Logo.png",
     alt: "RPI Logo",
   },
   Patina: {
     shortName: "Patina",
     name: "Patina Network",
+    apiKey: "patina",
     icon: "/brands/Patina_Logo.png",
     alt: "Patina Logo",
   },
   Gwc: {
-    shortName: "GWC@Hunter",
+    shortName: "GWC @ Hunter",
     name: "Hunter College - GWC",
+    apiKey: "gwc",
     icon: "/brands/Gwc_Logo.png",
     alt: "GWC Logo",
   },
   Sbu: {
     shortName: "SBU",
     name: "Stony Brook University",
+    apiKey: "sbu",
     icon: "/brands/SBU_shield.png",
     alt: "Stony Brook University Logo",
   },
   Columbia: {
     shortName: "Columbia",
     name: "Columbia University",
+    apiKey: "columbia",
     icon: "/brands/Columbia_logo.png",
     alt: "Columbia University Logo",
   },
   Ccny: {
     shortName: "CCNY",
     name: "City College of New York",
+    apiKey: "ccny",
     icon: "/brands/CCNY_logo.png",
     alt: "City College of New York Logo",
   },
   Cornell: {
     shortName: "Cornell",
     name: "Cornell University",
+    apiKey: "cornell",
     icon: "/brands/Cornell_Logo.png",
     alt: "Cornell University Logo",
   },
