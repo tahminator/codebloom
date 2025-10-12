d52fcd112d32aefe70cf6a4c5717b22aba343572
Angela Yu <angelayu0530@gmail.com>
2025-10-11 20:49:20 -0400

    fixed import

--- src/test/java/com/patina/codebloom/scheduled/leetcode/LeetcodeQuestionProcessServiceTest.java
+++ src/test/java/com/patina/codebloom/scheduled/leetcode/LeetcodeQuestionProcessServiceTest.java
@@ -1,6 +1,9 @@
 package com.patina.codebloom.scheduled.leetcode;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 
