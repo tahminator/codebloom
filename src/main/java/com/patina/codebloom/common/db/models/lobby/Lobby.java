435204117073980663031c1332a20bd6f3d6772c
Angela Yu <angelayu0530@gmail.com>
2025-10-15 11:41:55 -0400

    Lobby class

---
+++ src/main/java/com/patina/codebloom/common/db/models/lobby/Lobby.java
@@ -0,0 +1,40 @@
+package com.patina.codebloom.common.db.models.lobby;
+
+import java.time.OffsetDateTime;
+
+import com.patina.codebloom.common.db.helper.annotations.NotNullColumn;
+import com.patina.codebloom.common.db.helper.annotations.NullColumn;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+public class Lobby {
+    @NotNullColumn
+    private String id;
+
+    @NotNullColumn
+    private String joinCode;
+
+    @NotNullColumn
+    private LobbyStatus status;
+
+    @NotNullColumn
+    private OffsetDateTime createdAt;
+
+    @NotNullColumn
+    private OffsetDateTime expiresAt;
+
+    @NotNullColumn
+    private Integer playerCount;
+
+    @NullColumn
+    private String winnerId;
+}
