e809ef44a4fe384b85442e8ab96fe4abe7e6123b
Tahmid Ahmed <tahmid@tahmid.io>
2025-10-16 03:26:44 -0400

    HOTFIX: Update deploy-stg to use build image script, check if docker pat shows up

--- .github/scripts/build-image.sh
+++ .github/scripts/build-image.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$DIR/local-db.sh"
@@ -28,6 +29,8 @@ TAGS=(
 
 echo "Building image with tags:"
 printf '  - %s\n' "${TAGS[@]}"
+
+[ -n "${DOCKER_HUB_PAT:-}" ] && echo "DOCKER_HUB_PAT found" || echo "DOCKER_HUB_PAT missing or empty"
 
 echo "${DOCKER_HUB_PAT}" | docker login -u "tahminator" --password-stdin
 
