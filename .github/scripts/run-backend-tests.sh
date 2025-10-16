30a93b179323e64e3387227509bea3ddbe2a5b91
Tahmid Ahmed <tahmid@tahmid.io>
2025-10-16 04:04:13 -0400

    Maven batch mode

--- .github/scripts/run-backend-tests.sh
+++ .github/scripts/run-backend-tests.sh
@@ -14,7 +14,7 @@ echo "JAVA_HOME=$JAVA_HOME"
 
 ./mvnw checkstyle:check -Denv.SKIP=true
 
-./mvnw exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="install-deps"
+./mvnw -B exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="install-deps"
 ./mvnw -B exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="install"
 
 corepack enable pnpm
