bcf9e3b2416804106b0587fda67f92b92998aaed
Tahmid Ahmed <tahmid@tahmid.io>
2025-10-16 02:09:39 -0400

    Remove dirty context tests that may load poisoned application context

--- src/test/java/com/patina/codebloom/leetcode/LeetcodeClientTest.java
+++ src/test/java/com/patina/codebloom/leetcode/LeetcodeClientTest.java
@@ -12,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.patina.codebloom.common.leetcode.LeetcodeClient;
 import com.patina.codebloom.common.leetcode.models.LeetcodeDetailedQuestion;
@@ -23,6 +25,8 @@ import com.patina.codebloom.common.leetcode.models.UserProfile;
 import com.patina.codebloom.common.leetcode.throttled.ThrottledLeetcodeClient;
 
 @SpringBootTest
+@ActiveProfiles("ci")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class LeetcodeClientTest {
     private final LeetcodeClient leetcodeClient;
 
