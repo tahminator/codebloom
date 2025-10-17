ccd6d50057a2f5e1c543c120495ac45e0ee37df2
Angela Yu <angelayu0530@gmail.com>
2025-10-17 19:36:47 -0400

    LobbyPlayer Setter and Getters

---
+++ src/main/java/com/patina/codebloom/common/db/models/lobby/player/LobbyPlayer.java
@@ -0,0 +1,30 @@
+package com.patina.codebloom.common.db.models.lobby.player;
+import java.time.OffsetDateTime;
+
+import com.patina.codebloom.common.db.helper.annotations.NotNullColumn;
+import com.patina.codebloom.common.db.helper.annotations.NullColumn;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+public class LobbyPlayer {
+    @NotNullColumn
+    private String id;
+
+    @NotNullColumn
+    private String lobbyId;
+
+    @NotNullColumn
+    private String playerId;
+
+    @NotNullColumn
+    private int points;
+}
