f3aacaa58d05b2b9b027632972e062a0f77a7538
Arshadul Monir <arshadulmonir@gmail.com>
2025-10-24 12:16:22 -0400

    Revert "added score reporting properties to configuration properties"
    This reverts commit bbc3a9195e38aef08dc3ad3a5435a63a1b2e085e.

--- src/main/java/com/patina/codebloom/jda/client/JDAClient.java
+++ src/main/java/com/patina/codebloom/jda/client/JDAClient.java
@@ -29,7 +29,7 @@ import net.dv8tion.jda.api.utils.FileUpload;
  */
 @Component
 @Slf4j
-@EnableConfigurationProperties({ JDAPatinaProperties.class, JDAErrorReportingProperties.class, JDALogReportingProperties.class, JDAScoreReportingProperties.class })
+@EnableConfigurationProperties({ JDAPatinaProperties.class, JDAErrorReportingProperties.class, JDALogReportingProperties.class })
 public class JDAClient {
     private final JDAInitializer jdaInitializer;
     private JDA jda;
