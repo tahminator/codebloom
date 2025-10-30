7bf9862a80c2aa9f1ef92e328098411a34edc342
Arshadul Monir <arshadulmonir@gmail.com>
2025-10-30 15:44:21 -0400

    checkstyle fix

--- src/main/java/com/patina/codebloom/common/leetcode/score/MultiplierFunctions.java
+++ src/main/java/com/patina/codebloom/common/leetcode/score/MultiplierFunctions.java
@@ -1,15 +1,15 @@
 package com.patina.codebloom.common.leetcode.score;
 
 public class MultiplierFunctions {
-    public static float purpleFunction(float acceptanceRate) {
+    public static float purpleFunction(final float acceptanceRate) {
         return 1.0f - acceptanceRate / 400;
     }
 
-    public static float orangeFunction(float acceptanceRate) {
+    public static float orangeFunction(final float acceptanceRate) {
         return 1.0f / (acceptanceRate / 100 + 1);
     }
 
-    public static float greenFunction(float acceptanceRate) {
+    public static float greenFunction(final float acceptanceRate) {
         return (float) (1.0 / (2 * Math.pow(acceptanceRate / 100 + 1, 2)) + 0.5);
     }
 }
