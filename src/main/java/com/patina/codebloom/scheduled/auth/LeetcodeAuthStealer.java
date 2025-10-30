5a6ec359b13fecbb35f5496269d31a20c3de51cc
Tahmid Ahmed <tahmid@tahmid.io>
2025-10-30 01:44:21 -0400

    Fixed a bug where  was not correctly stopping threads from doing non-atomic/safe things. (DESC)
    The bug was that threads in different pools would not respect the  keyword and would try to read the cookie when it's not supposed to. For example, thread 1 from pool 1 would see that cookie is not available in db (and redis if ci) and enter the long process of fetching. Previously, when pool 2 thread 1 would not wait and try to read the cookie before the previous pool's thread is done, and so then this thread would enter the long process of fetching as well. This is obviously an annoying issue (controllers use 1 pool from what I recall, but it does not share that pool with Async or Scheduled methods which have their own pool) and so the fix would be to use a ReentrantLock (specifically a rw lock since we do infrequent writes and many reads at once) and make that lock static so it is shared ACROSS thread pools (it will only be initialized once when the class is loaded into the JVM). This now fixes the issue, but as a result, the syntax required to use a lock is a bit \'uglier\'. It also required there to be better boundaries between when the actual cookie is being fetched from leetcode vs the async method on start that checks the database for tokens.

--- src/main/java/com/patina/codebloom/scheduled/auth/LeetcodeAuthStealer.java
+++ src/main/java/com/patina/codebloom/scheduled/auth/LeetcodeAuthStealer.java
@@ -6,10 +6,9 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -35,9 +34,12 @@ import com.patina.codebloom.common.reporter.report.Report;
 import com.patina.codebloom.common.reporter.report.location.Location;
 import com.patina.codebloom.common.time.StandardizedOffsetDateTime;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class LeetcodeAuthStealer {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LeetcodeAuthStealer.class);
+    private static final ReentrantReadWriteLock LOCK = new ReentrantReadWriteLock();
 
     private volatile String cookie;
     private volatile String csrf;
@@ -81,39 +83,88 @@ public class LeetcodeAuthStealer {
      * queries such as used to retrieve code from our user submissions.
      */
     @Scheduled(initialDelay = 0, fixedDelay = 1, timeUnit = TimeUnit.HOURS)
-    public synchronized String stealAuthCookie() {
-        Auth mostRecentAuth = authRepository.getMostRecentAuth();
+    public void stealAuthCookie() {
+        LOCK.writeLock().lock();
+        try {
+            Auth mostRecentAuth = authRepository.getMostRecentAuth();
 
-        // The auth token should be refreshed every day.
-        if (mostRecentAuth != null && mostRecentAuth.getCreatedAt().isAfter(StandardizedOffsetDateTime.now().minus(4, ChronoUnit.HOURS))) {
-            LOGGER.info("Auth token already exists, using token from database.");
-            cookie = mostRecentAuth.getToken();
-            csrf = mostRecentAuth.getCsrf();
+            // The auth token should be refreshed every day.
+            if (mostRecentAuth != null && mostRecentAuth.getCreatedAt().isAfter(StandardizedOffsetDateTime.now().minus(4, ChronoUnit.HOURS))) {
+                log.info("Auth token already exists, using token from database.");
+                cookie = mostRecentAuth.getToken();
+                csrf = mostRecentAuth.getCsrf();
+                if (env.isCi()) {
+                    log.info("in ci, stealing token and putting it in cache for 1 day");
+                    jedisClient.setAuth(cookie, 4, ChronoUnit.HOURS); // 4 hours.
+                }
+                return;
+            }
+
             if (env.isCi()) {
-                LOGGER.info("in ci, stealing token and putting it in cache for 1 day");
-                jedisClient.setAuth(cookie, 4, ChronoUnit.HOURS); // 4 hours.
+                log.info("in ci env, checking redis client...");
+                Optional<String> authToken = jedisClient.getAuth();
+
+                log.info("auth token in redis = {}", authToken.isPresent());
+
+                if (authToken.isPresent()) {
+                    log.info("auth token found in redis client");
+                    cookie = authToken.get();
+                    csrf = null; // don't care in ci.
+                    return;
+                }
+
+                log.info("auth token not found in redis client");
             }
-            return null;
+        } finally {
+            LOCK.writeLock().unlock();
         }
 
-        if (env.isCi()) {
-            LOGGER.info("in ci env, checking redis client...");
-            Optional<String> authToken = jedisClient.getAuth();
+        log.info("Auth token is missing/expired. Attempting to receive token...");
 
-            LOGGER.info("auth token in redis = {}", authToken.isPresent());
+        stealCookieImpl();
+    }
 
-            if (authToken.isPresent()) {
-                LOGGER.info("auth token found in redis client");
-                cookie = authToken.get();
-                csrf = null; // don't care in ci.
-                return null;
-            }
+    /**
+     * There are some cases where leetcode.com may not respect the token anymore. If
+     * that is the case, it is best to try to steal a new cookie and replace the
+     * current one.
+     *
+     * You may await the `CompletableFuture` and receive the brand new token, or
+     * call-and-forget.
+     */
+    @Async
+    public CompletableFuture<Optional<String>> reloadCookie() {
+        return CompletableFuture.completedFuture(Optional.ofNullable(stealCookieImpl()));
+    }
 
-            LOGGER.info("auth token not found in redis client");
+    public String getCookie() {
+        LOCK.readLock().lock();
+        try {
+            return cookie;
+        } finally {
+            LOCK.readLock().unlock();
+        }
+    }
+
+    /**
+     * It's fine if this is null for some requests; it isn't a requirement to fetch
+     * data from the GraphQL layer of leetcode.com
+     */
+    public String getCsrf() {
+        if (csrf == null && !reported) {
+            reported = true;
+            reporter.log(Report.builder()
+                            .environments(env.getActiveProfiles())
+                            .location(Location.BACKEND)
+                            .data("CSRF token is missing inside of LeetcodeAuthStealer. This may be something to look into.")
+                            .build());
         }
 
-        LOGGER.info("Auth token is missing/expired. Attempting to receive token...");
+        return csrf;
+    }
 
+    private String stealCookieImpl() {
+        LOCK.writeLock().lock();
         try (Playwright playwright = Playwright.create();
                         Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true).setTimeout(40000));
                         BrowserContext context = browser.newContext(new NewContextOptions()
@@ -121,25 +172,25 @@ public class LeetcodeAuthStealer {
                                         .setStorageState(null))) {
             context.clearCookies();
 
-            LOGGER.info("Loaded browser context");
+            log.info("Loaded browser context");
 
             Page page = context.newPage();
 
             page.navigate("https://leetcode.com/accounts/github/login/?next=%2F");
 
-            LOGGER.info("Navigated to leetcode.com login");
+            log.info("Navigated to leetcode.com login");
 
             page.waitForLoadState(LoadState.NETWORKIDLE);
 
             page.fill("#login_field", githubUsername);
             page.fill("#password", githubPassword);
 
-            LOGGER.info("Filled in credentials, clicking login...");
+            log.info("Filled in credentials, clicking login...");
 
             page.click("input[name=\"commit\"]");
 
             if (page.isVisible("#device-verification-prompt") || page.isVisible("#session-otp-input-description")) {
-                LOGGER.info("2FA Required");
+                log.info("2FA Required");
                 List<Message> messages;
 
                 page.waitForTimeout(10000);
@@ -147,7 +198,7 @@ public class LeetcodeAuthStealer {
                 try {
                     messages = email.getPastMessages();
                 } catch (Exception e) {
-                    LOGGER.info("Failed to retrieve past messages");
+                    log.info("Failed to retrieve past messages");
                     throw new RuntimeException("Failed to retrieve past messages", e);
                 }
 
@@ -155,7 +206,7 @@ public class LeetcodeAuthStealer {
                 for (Message m : messages) {
                     // Don't break so we can get the newest available code.
                     if (m.getSubject().equals("[GitHub] Please verify your device")) {
-                        LOGGER.info("Found verification email");
+                        log.info("Found verification email");
                         target = m;
                     }
                 }
@@ -166,7 +217,7 @@ public class LeetcodeAuthStealer {
 
                 String code = CodeExtractor.extractCode(target.getMessage());
 
-                LOGGER.info("Found code in email, will fill now...");
+                log.info("Found code in email, will fill now...");
 
                 if (code == null) {
                     throw new RuntimeException("Code was not found in the email. Manual intervention required.");
@@ -174,7 +225,7 @@ public class LeetcodeAuthStealer {
 
                 page.fill("input[name='otp']", code);
 
-                LOGGER.info("Page filled!");
+                log.info("Page filled!");
 
             }
 
@@ -184,12 +235,12 @@ public class LeetcodeAuthStealer {
                     // Click the second button (which is the actual authorize button)
                     buttons.get(1).click();
                 }
-                LOGGER.info("Authorization button clicked");
+                log.info("Authorization button clicked");
             }
 
             page.waitForURL("https://leetcode.com/");
 
-            LOGGER.info("Back to leetcode.com!");
+            log.info("Back to leetcode.com!");
 
             // if (page.isVisible("h1:has-text(\"Login Cancelled\")")) {
             // page.navigate("https://leetcode.com/accounts/github/login/?next=%2F");
@@ -203,58 +254,27 @@ public class LeetcodeAuthStealer {
 
                 String sessionToken = cookieMap.get("LEETCODE_SESSION");
                 if (sessionToken != null) {
-                    LOGGER.info("Cookie found!");
+                    log.info("Cookie found!");
                     authRepository.createAuth(Auth
                                     .builder()
                                     .token(sessionToken)
                                     .csrf(cookieMap.get("csrftoken"))
                                     .build());
                     if (env.isCi()) {
-                        LOGGER.info("in ci, stored in redis as well");
+                        log.info("in ci, stored in redis as well");
                         jedisClient.setAuth(sessionToken, 4, ChronoUnit.HOURS); // 4 hours.
                     }
                     this.cookie = sessionToken;
                     return sessionToken;
                 }
             } else {
-                LOGGER.info("Should be authenticated but not authenticated.");
+                log.info("Should be authenticated but not authenticated.");
             }
+        } finally {
+            LOCK.writeLock().unlock();
         }
 
         return null;
     }
 
-    /**
-     * There are some cases where leetcode.com may not respect the token anymore. If
-     * that is the case, it is best to try to steal a new cookie and replace the
-     * current one.
-     *
-     * You may await the `CompletableFuture` and receive the brand new token, or
-     * call-and-forget.
-     */
-    @Async
-    public synchronized CompletableFuture<Optional<String>> reloadCookie() {
-        return CompletableFuture.completedFuture(Optional.ofNullable(stealAuthCookie()));
-    }
-
-    public synchronized String getCookie() {
-        return cookie;
-    }
-
-    /**
-     * It's fine if this is null for some requests; it isn't a requirement to fetch
-     * data from the GraphQL layer of leetcode.com
-     */
-    public synchronized String getCsrf() {
-        if (csrf == null && !reported) {
-            reported = true;
-            reporter.log(Report.builder()
-                            .environments(env.getActiveProfiles())
-                            .location(Location.BACKEND)
-                            .data("CSRF token is missing inside of LeetcodeAuthStealer. This may be something to look into.")
-                            .build());
-        }
-
-        return csrf;
-    }
 }
