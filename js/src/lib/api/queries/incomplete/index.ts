cc21b2365a42200dd091a413b52c9f8b90e66ab4
Arshadul Monir <arshadulmonir@gmail.com>
2025-11-02 11:39:23 -0500

    use and fetch functions

---
+++ js/src/lib/api/queries/incomplete/index.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { ApiURL } from "../../common/apiURL";
+
+export const useIncompleteQuestionQuery = () => {
+  return useQuery({
+    queryKey: ["incomplete"],
+    queryFn: fetchIncompleteQuestions,
+  });
+};
+
+async function fetchIncompleteQuestions() {
+
+  const { url, method, res } = ApiURL.create(
+    "/api/admin/questions/incomplete",
+    {
+      method: "GET",
+    },
+  );
+
+  const response = await fetch(url, {
+    method,
+  });
+
+  return res(await response.json());
+}
