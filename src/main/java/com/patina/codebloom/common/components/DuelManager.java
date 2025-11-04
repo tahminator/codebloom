fcd180ebbfc9addc5f5b20c49d057ba546d37c89
Tahmid Ahmed <tahmid@tahmid.io>
2025-11-03 19:29:37 -0500

    Add DuelController and DuelManager

---
+++ src/main/java/com/patina/codebloom/common/components/DuelManager.java
@@ -0,0 +1,10 @@
+package com.patina.codebloom.common.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DuelManager {
+    public DuelManager() {
+    }
+
+}
