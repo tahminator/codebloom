5716d9e0593690d2863596d3e3aba7147894c701
Angela Yu <angelayu0530@gmail.com>
2025-11-06 18:14:10 -0500

    Lobby Dto

---
+++ src/main/java/com/patina/codebloom/api/duel/dto/LobbyDto.java
@@ -0,0 +1,47 @@
+package com.patina.codebloom.api.duel.dto;
+
+import java.time.OffsetDateTime;
+
+import com.patina.codebloom.common.db.models.lobby.Lobby;
+import com.patina.codebloom.common.db.models.lobby.LobbyStatus;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.jackson.Jacksonized;
+
+@Getter
+@Builder
+@Jacksonized
+@ToString
+@EqualsAndHashCode
+public class LobbyDto {
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private String id;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private String joinCode;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private LobbyStatus status;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private OffsetDateTime expiresAt;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private OffsetDateTime createdAt;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+    private int playerCount;
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, nullable = true)
+    private String winnerId;
+
+    public static LobbyDto fromLobby(final Lobby lobby) {
+        return LobbyDto.builder()
+                        .id(lobby.getId())
+                        .joinCode(lobby.getJoinCode())
+                        .status(lobby.getStatus())
+                        .expiresAt(lobby.getExpiresAt())
+                        .createdAt(lobby.getCreatedAt())
+                        .playerCount(lobby.getPlayerCount())
+                        .winnerId(lobby.getWinnerId())
+                        .build();
+    }
+}
