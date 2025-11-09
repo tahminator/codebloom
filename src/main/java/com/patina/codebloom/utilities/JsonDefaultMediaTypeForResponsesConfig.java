21b374d0e93ff73af55013ce94680e091dd89572
Alisha Zaman <az2924@nyu.edu>
2025-11-08 21:40:21 -0500

    Change Content-Type to application/json

--- src/main/java/com/patina/codebloom/utilities/JsonDefaultMediaTypeForResponsesConfig.java
+++ src/main/java/com/patina/codebloom/utilities/JsonDefaultMediaTypeForResponsesConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,11 +16,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class JsonDefaultMediaTypeForResponsesConfig implements WebMvcConfigurer {
     @Override
+    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+        configurer
+                        .favorParameter(false)
+                        .ignoreAcceptHeader(false)
+                        .defaultContentType(MediaType.APPLICATION_JSON)
+                        .mediaType("json", MediaType.APPLICATION_JSON)
+                        .mediaType("html", MediaType.TEXT_HTML);
+    }
+
+    @Override
     public void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
         for (HttpMessageConverter<?> converter : converters) {
             if (converter instanceof MappingJackson2HttpMessageConverter jacksonConverter) {
                 jacksonConverter.setSupportedMediaTypes(
-                                List.of(MediaType.APPLICATION_JSON, MediaType.ALL));
+                                List.of(MediaType.APPLICATION_JSON));
             }
         }
     }
