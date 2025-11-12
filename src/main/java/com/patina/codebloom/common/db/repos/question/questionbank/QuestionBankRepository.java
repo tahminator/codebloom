71e2bd0d78a52f2845dee87b67bcb1ca7da09d08
Arshadul Monir <arshadulmonir@gmail.com>
2025-11-12 14:25:57 -0500

    declared repository functions

--- src/main/java/com/patina/codebloom/common/db/repos/question/questionbank/QuestionBankRepository.java
+++ src/main/java/com/patina/codebloom/common/db/repos/question/questionbank/QuestionBankRepository.java
@@ -1,10 +1,31 @@
 package com.patina.codebloom.common.db.repos.question.questionbank;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.patina.codebloom.common.db.models.question.Question;
+import com.patina.codebloom.common.db.models.question.bank.QuestionBank;
+import com.patina.codebloom.common.db.models.question.QuestionDifficulty;
+import com.patina.codebloom.common.db.models.question.topic.QuestionTopic;
 
-    public interface QuestionBankRepository {
-        // List<QuestionBank> createQuestionBank(QuestionBank questionBank);
+
+
+public interface QuestionBankRepository {
+    QuestionBank createQuestion(QuestionBank question);
+
+    QuestionBank getQuestionById(String id);
+    
+    QuestionBank getQuestionBySlug(String slug);
+    
+    QuestionBank updateQuestion(QuestionBank question);
+    
+    boolean deleteQuestionById(String id);
+
+    ArrayList<QuestionBank> getAllIncompleteQuestions();
+
+    QuestionBank getRandomQuestion();
+
+    List<QuestionBank> getQuestionsByTopic(QuestionTopic topic);
+
+    List<QuestionBank> getQuestionsByDifficulty(QuestionDifficulty difficulty);
 
 }
