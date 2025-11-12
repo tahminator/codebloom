482eab5cfc617e19af0b3ad772706f50a919c14f
Arshadul Monir <arshadulmonir@gmail.com>
2025-11-12 13:09:30 -0500

    Created question bank object

--- src/main/java/com/patina/codebloom/common/db/models/question/bank/QuestionBank.java
+++ src/main/java/com/patina/codebloom/common/db/models/question/bank/QuestionBank.java
@@ -1,8 +1,12 @@
 package com.patina.codebloom.common.db.models.question.bank;
 
+import java.util.List;
+
+import com.patina.codebloom.common.db.helper.annotations.JoinColumn;
 import com.patina.codebloom.common.db.helper.annotations.NotNullColumn;
 import com.patina.codebloom.common.db.models.question.QuestionDifficulty;
-import com.patina.codebloom.common.db.models.question.topic.LeetcodeTopicEnum;
+import com.patina.codebloom.common.db.models.question.topic.QuestionTopic;
+import com.patina.codebloom.common.db.repos.question.topic.QuestionTopicRepository;
 
 public class QuestionBank {
     @NotNullColumn
@@ -15,8 +19,18 @@ public class QuestionBank {
     private QuestionDifficulty difficulty;
 
     @NotNullColumn
-    private LeetcodeTopicEnum topics;
+    private String questionTitle;
 
-    // @NotNullColumn
-    // private 
+    @NotNullColumn
+    private String description;
+
+    @NotNullColumn
+    private float acceptanceRate;
+
+    /**
+     * Join field, update/create with {@link QuestionTopicRepository}
+     */
+    @JoinColumn
+    private List<QuestionTopic> topics;
+
 }
