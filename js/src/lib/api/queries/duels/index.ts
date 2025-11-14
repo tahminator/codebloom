0d150c847317dc2ebe9628a6abd0c778b99093d4
alfardil <fardilfayed@gmail.com>
2025-11-14 16:30:58 -0500

    format changes and req addition

--- js/src/lib/api/queries/duels/index.ts
+++ js/src/lib/api/queries/duels/index.ts
@@ -3,48 +3,44 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { ApiURL } from "../../common/apiURL";
 
 export const useCreateParty = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn: createParty,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["party"] });
-        },
-    });
+  return useMutation({
+    mutationFn: createParty,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["party"] });
+    },
+  });
 };
 
 async function createParty() {
-    const { url, method, res } = ApiURL.create("/api/duel/party/create", {
-        method: "POST",
-    });
-    const response = await fetch(url, {
-        method,
-    });
-    return res(await response.json());
+  const { url, method, res } = ApiURL.create("/api/duel/party/create", {
+    method: "POST",
+  });
+  const response = await fetch(url, {
+    method,
+  });
+  return res(await response.json());
 }
-
 
 export const useJoinParty = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn: joinParty,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["party"] });
-        }
-    })
-}
+  return useMutation({
+    mutationFn: joinParty,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["party"] });
+    },
+  });
+};
 
-async function joinParty(joinPartyBody: { code: string }) {
-    const { url, method, res } = ApiURL.create(
-        "/api/duel/lobby/join",
-        {
-            method: "POST",
-        }
-    );
-    const response = await fetch(url, {
-        method,
-        body: joinPartyBody.code
-    })
-    return res(await response.json());
+async function joinParty(joinPartyBody: { partyCode: string }) {
+  const { url, method, req, res } = ApiURL.create("/api/duel/lobby/join", {
+    method: "POST",
+  });
+  const response = await fetch(url, {
+    method,
+    body: req({ partyCode: joinPartyBody.partyCode }),
+  });
+  return res(await response.json());
 }
