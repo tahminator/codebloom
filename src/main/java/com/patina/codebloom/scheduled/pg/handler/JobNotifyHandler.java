540fa343e031babad9c1748b0c72a22f0c501d9f
Tahmid Ahmed <tahmid@tahmid.io>
2025-11-13 20:02:42 -0500

    Added notify trigger on lobby upsert. Implement shared notify listener and abstracted handlers based on their channels. Commented tests out for now (will need to be redone).

---
+++ src/main/java/com/patina/codebloom/scheduled/pg/handler/JobNotifyHandler.java
@@ -0,0 +1,26 @@
+package com.patina.codebloom.scheduled.pg.handler;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import com.patina.codebloom.scheduled.leetcode.LeetcodeQuestionProcessService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@Profile("!ci")
+public class JobNotifyHandler {
+    private final LeetcodeQuestionProcessService leetcodeQuestionProcessService;
+
+    public JobNotifyHandler(
+                    final LeetcodeQuestionProcessService leetcodeQuestionProcessService) {
+        this.leetcodeQuestionProcessService = leetcodeQuestionProcessService;
+    }
+
+    @Async
+    public void handle(final String payload) {
+        leetcodeQuestionProcessService.drainQueue();
+    }
+}
