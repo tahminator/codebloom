e151d0a74c6d5095d11466e7d6a1758e38bcdda3
alfardil <fardilfayed@gmail.com>
2025-11-20 15:04:15 -0500

    added default method

--- src/main/java/com/patina/codebloom/common/leetcode/LeetcodeClient.java
+++ src/main/java/com/patina/codebloom/common/leetcode/LeetcodeClient.java
@@ -14,6 +14,20 @@ public interface LeetcodeClient {
 
     LeetcodeQuestion findQuestionBySlug(String slug);
 
+    /**
+     * Default method that doesn't require a limit. The default limit will return 20
+     * submissions.
+     * 
+     * @param username
+     */
+    List<LeetcodeSubmission> findSubmissionsByUsername(String username);
+
+    /**
+     * This method specifies a submissions limit to adhere to.
+     * 
+     * @param username
+     * @param limit
+     */
     List<LeetcodeSubmission> findSubmissionsByUsername(String username, int limit);
 
     /**
