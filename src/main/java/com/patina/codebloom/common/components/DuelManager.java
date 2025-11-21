c70e1bb640ef0b6201d873ee5ebc7abf1772ef0b
Angela Yu <angelayu0530@gmail.com>
2025-11-21 04:09:23 -0500

    added a new method to get unqiue questions in LobbyPlayerQuestion + wrote tests + linked it to dueldata

--- src/main/java/com/patina/codebloom/common/components/DuelManager.java
+++ src/main/java/com/patina/codebloom/common/components/DuelManager.java
@@ -4,14 +4,18 @@ import com.patina.codebloom.common.db.repos.lobby.LobbyRepository;
 import com.patina.codebloom.common.dto.lobby.DuelData;
 import com.patina.codebloom.common.dto.lobby.LobbyDto;
 import org.springframework.stereotype.Component;
+import com.patina.codebloom.common.db.repos.lobby.player.LobbyPlayerQuestionRepository;
 
 @Component
 public class DuelManager {
 
     private final LobbyRepository lobbyRepository;
+    private final LobbyPlayerQuestionRepository lobbyPlayerQuestionRepository;
 
-    public DuelManager(final LobbyRepository lobbyRepository) {
+    public DuelManager(final LobbyRepository lobbyRepository,
+                    final LobbyPlayerQuestionRepository lobbyPlayerQuestionRepository) {
         this.lobbyRepository = lobbyRepository;
+        this.lobbyPlayerQuestionRepository = lobbyPlayerQuestionRepository;
     }
 
     public DuelData generateDuelData(final String lobbyId) {
@@ -20,6 +24,9 @@ public class DuelManager {
             .map(LobbyDto::fromLobby)
             .orElse(null);
 
-        return DuelData.builder().lobby(fetchedLobby).build();
+        return DuelData.builder()
+                        .lobby(fetchedLobby)
+                        .questions(lobbyPlayerQuestionRepository.findUniqueQuestionsByLobbyId(lobbyId))
+                        .build();
     }
 }
