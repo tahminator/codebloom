d7852968e6254ebca25f93dc34340e368b354772
Angela Yu <angelayu0530@gmail.com>
2025-11-21 04:09:23 -0500

    added a new method to get unqiue questions in LobbyPlayerQuestion + wrote tests + linked it to dueldata

--- src/main/java/com/patina/codebloom/common/dto/lobby/DuelData.java
+++ src/main/java/com/patina/codebloom/common/dto/lobby/DuelData.java
@@ -1,5 +1,9 @@
 package com.patina.codebloom.common.dto.lobby;
 
+import java.util.List;
+
+import com.patina.codebloom.common.db.models.lobby.player.LobbyPlayerQuestion;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -16,6 +20,9 @@ public class DuelData {
 
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private LobbyDto lobby;
+
+    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private List<LobbyPlayerQuestion> questions;
 
     public static final DuelData DEFAULT = builder().build();
 }
