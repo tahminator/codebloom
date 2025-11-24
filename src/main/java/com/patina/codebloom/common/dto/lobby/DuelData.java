a28f8b9354d954d3323d821342b815b2019d75c0
Angela Yu <angelayu0530@gmail.com>
2025-11-24 13:29:40 -0500

    checkstyle fixes
    prettier changes

--- src/main/java/com/patina/codebloom/common/dto/lobby/DuelData.java
+++ src/main/java/com/patina/codebloom/common/dto/lobby/DuelData.java
@@ -1,11 +1,9 @@
 package com.patina.codebloom.common.dto.lobby;
 
+import com.patina.codebloom.common.db.models.question.Question;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.Map;
-
-import com.patina.codebloom.common.db.models.question.Question;
-
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,10 +20,18 @@ public class DuelData {
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private LobbyDto lobby;
 
-    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true, description = "Unique questions being worked on in this duel")
+    @Schema(
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+        nullable = true,
+        description = "Unique questions being worked on in this duel"
+    )
     private List<Question> questions;
 
-    @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, nullable = true, description = "Map of player IDs to the questions they have solved")
+    @Schema(
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+        nullable = true,
+        description = "Map of player IDs to the questions they have solved"
+    )
     private Map<String, List<Question>> playerQuestions;
 
     public static final DuelData DEFAULT = builder().build();
