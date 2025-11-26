7a0c55d762d46c7a33a36af4473047c34a2fe23f
Tahmid Ahmed <tahmid@tahmid.io>
2025-11-26 03:10:51 -0500

    Add package.json/pnpm-lock.yaml back to VCS (anti-pattern to remove) & update pom.xml/Justfile/CI scripts to adjust accordingly. Rename prettier/lint to fmt for consistency

--- .github/scripts/run-backend-compile-tests.sh
+++ .github/scripts/run-backend-compile-tests.sh
@@ -5,11 +5,11 @@ RED='\033[0;31m'
 
 # fmt
 corepack enable pnpm
-pnpm i prettier prettier-plugin-java
+pnpm i --frozen-lockfile
 
 fmt_status=$?
-if ! pnpx prettier -c "**/*.java"; then
-    echo -e "${RED}Prettier failed. On your local machine, please run \`just backend-prettier-fix\`"
+if ! pnpm run fmt; then
+    echo -e "${RED}Prettier failed. On your local machine, please run \`just backend-fmt-fix\`"
     exit 1
 fi
 
