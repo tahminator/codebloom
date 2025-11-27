0a91c5552182370fe86639bc050dc2e03df49545
Tahmid Ahmed <tahmid@tahmid.io>
2025-11-27 01:44:17 -0500

    Add javadoc to LogExecutionTime

--- src/main/java/com/patina/codebloom/common/utils/log/LogExecutionTime.java
+++ src/main/java/com/patina/codebloom/common/utils/log/LogExecutionTime.java
@@ -5,6 +5,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to log the execution time of a method.
+ *
+ * <p>When applied to a method, logs the execution time to SLF4J by default.
+ * Optionally, if {@code reportToDiscord} is set to {@code true}, the execution
+ * time will also be reported to Discord for monitoring purposes.
+ *
+ *
+ * <b>Example usage:</b>
+ * <pre>{@code
+ * @LogExecutionTime(reportToDiscord = true)
+ * public void processData() {
+ *     // method implementation
+ * }
+ * }</pre>
+ *
+ * @apiNote <b>Performance implications:</b> Reporting to Discord is expensive, so <b>use it sparingly</b>.
+ *
+ * @see LogExecutionTimeAspect
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogExecutionTime {
