7c0a523efe5f964728d5e4e6a84f14bbcfaea169
Tahmid Ahmed <tahmid@tahmid.io>
2025-12-02 15:12:58 -0500

    Remove Prettier logic from LazyVim config file. cleaned up old Eclipse formatter code + simplified LazyVim config file. Added auto config to run spotless on save in neovim

--- .lazy.lua
+++ .lazy.lua
@@ -3,37 +3,15 @@ local format =
 
 return {
     {
-        "stevearc/conform.nvim",
-        optional = true,
-        opts = function(_, opts)
-            opts.formatters_by_ft = opts.formatters_by_ft or {}
-            for _, ft in ipairs({ "java" }) do
-                opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-                table.insert(opts.formatters_by_ft[ft], "prettier")
-            end
-
-            opts.formatters.prettier = {
-                command = "prettier",
-            }
-        end,
-    },
-    {
         "nvimtools/none-ls.nvim",
         optional = true,
         event = "LazyFile",
         dependencies = { "mason.nvim" },
-        config = function()
+        opts = function()
             local null_ls = require("null-ls")
 
-            -- Setup null-ls with full override (ignores LazyVim defaults)
-            null_ls.setup({
-                debug = true,
-                root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+            return {
                 sources = {
-                    null_ls.builtins.formatting.fish_indent,
-                    null_ls.builtins.diagnostics.fish,
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.shfmt,
                     null_ls.builtins.diagnostics.checkstyle.with({
                         args = { "-f", "sarif", "$FILENAME" },
                         parser = require("lint.parser").from_errorformat(format, {
@@ -42,43 +20,7 @@ return {
                         extra_args = { "-c", "./checkstyle.xml" },
                     }),
                 },
-            })
-
-            -- Register with LazyVim for formatting
-            LazyVim.format.register({
-                name = "none-ls.nvim",
-                priority = 300, -- Higher priority than built-in formatter
-                primary = true,
-                format = function(buf)
-                    return LazyVim.lsp.format({
-                        bufnr = buf,
-                        filter = function(client)
-                            return client.name == "null-ls"
-                        end,
-                    })
-                end,
-                sources = function(buf)
-                    local ret = require("null-ls.sources").get_available(vim.bo[buf].filetype, "NULL_LS_FORMATTING")
-                        or {}
-                    return vim.tbl_map(function(source)
-                        return source.name
-                    end, ret)
-                end,
-            })
+            }
         end,
-    },
-    {
-        "mfussenegger/nvim-jdtls",
-        opts = {
-            settings = {
-                java = {
-                    format = {
-                        settings = {
-                            url = "./java-formatter.xml",
-                        },
-                    },
-                },
-            },
-        },
     },
 }
