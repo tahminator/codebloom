52250fc60ed950c013b34b18d076450dd4f48bbe
Tahmid Ahmed <tahmid@tahmid.io>
2025-12-02 15:13:29 -0500

    Remove Prettier logic & add logic to run spotless in CI

--- .github/scripts/run-backend-compile-tests.sh
+++ .github/scripts/run-backend-compile-tests.sh
@@ -4,14 +4,7 @@ set -euo pipefail
 RED='\033[0;31m'
 
 # fmt
-corepack enable pnpm
-pnpm i --frozen-lockfile
-
-fmt_status=$?
-if ! pnpm run fmt; then
-    echo -e "${RED}Prettier failed. On your local machine, please run \`just backend-fmt-fix\`"
-    exit 1
-fi
+./mvnw spotless:check
 
 # lint
 ./mvnw checkstyle:check
