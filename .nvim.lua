b7e3855b11a21459078656993c6875e70dccf682
Tahmid Ahmed <tahmid@tahmid.io>
2025-12-02 15:12:58 -0500

    Remove Prettier logic from LazyVim config file. cleaned up old Eclipse formatter code + simplified LazyVim config file. Added auto config to run spotless on save in neovim

---
+++ .nvim.lua
@@ -0,0 +1,30 @@
+vim.o.autoread = true
+
+local running = false
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.java",
+    callback = function(args)
+        if running then
+            return
+        end
+        running = true
+
+        vim.fn.jobstart({ "./mvnw", "spotless:apply", "-DspotlessFiles=" .. args.file }, {
+            stdout_buffered = true,
+            stderr_buffered = true,
+            on_exit = function(_, code)
+                vim.schedule(function()
+                    if code ~= 0 then
+                        vim.notify("spotless failed (" .. code .. ")", vim.log.levels.ERROR)
+                        running = false
+                        return
+                    end
+
+                    vim.cmd("checktime")
+
+                    running = false
+                end)
+            end,
+        })
+    end,
+})
