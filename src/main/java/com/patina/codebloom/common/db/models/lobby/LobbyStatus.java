71a57e3d99422daa6856ef0c23253c959229effb
Tahmid Ahmed <tahmid@tahmid.io>
2025-12-07 19:54:53 -0500

    509: Update all enums to include a schema description that is 1:1 to the enum (this is now required for the frontend generation step

--- src/main/java/com/patina/codebloom/common/db/models/lobby/LobbyStatus.java
+++ src/main/java/com/patina/codebloom/common/db/models/lobby/LobbyStatus.java
@@ -1,5 +1,8 @@
 package com.patina.codebloom.common.db.models.lobby;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "LobbyStatus")
 public enum LobbyStatus {
     CLOSED,
     AVAILABLE,
