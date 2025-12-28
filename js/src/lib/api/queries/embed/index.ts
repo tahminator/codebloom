dc6ee95b8134db4d5821a8076bc6221a3d85ed44
Arshadul Monir <arshadulmonir@gmail.com>
2025-12-28 11:24:19 -0500

    535: Displayed POTD embed at embed endpoint

---
+++ js/src/lib/api/queries/embed/index.ts
@@ -0,0 +1,22 @@
+import { ApiURL } from "@/lib/api/common/apiURL";
+import { useQuery } from "@tanstack/react-query";
+
+export const useFetchPotdEmbedQuery = () => {
+  return useQuery({
+    queryKey: ["potd", new Date().getDay()],
+    queryFn: fetchPotdEmbed,
+  });
+};
+
+async function fetchPotdEmbed() {
+  const { url, method, res } = ApiURL.create("/api/leetcode/potd/embed", {
+    method: "GET",
+  });
+  const response = await fetch(url, {
+    method,
+  });
+
+  const json = res(await response.json());
+
+  return json;
+}
