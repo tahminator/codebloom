71b77cad3408cc42a02f9382ef7e0086131f0a02
Tahmid Ahmed <tahmid@tahmid.io>
2025-12-28 16:42:44 -0500

    487: Add two duel pages, party creation page under /duel/create and the actual duel page under /duel/{lobbyCode}

---
+++ js/src/app/duel/[lobbyCode]/Duel.page.tsx
@@ -0,0 +1,53 @@
+import PageShell from "@/components/ui/page/PageShell";
+import ToastWithRedirect from "@/components/ui/toast/ToastWithRedirect";
+import { useAuthQuery } from "@/lib/api/queries/auth";
+import { Flex, Loader } from "@mantine/core";
+import { useParams } from "react-router";
+
+export default function DuelPage() {
+  const { lobbyCode } = useParams<{ lobbyCode: string }>();
+  const { data, status } = useAuthQuery();
+
+  if (status === "pending") {
+    return (
+      <PageShell>
+        <Flex
+          direction={"column"}
+          align={"center"}
+          justify={"center"}
+          w={"98vw"}
+          h={"90vh"}
+        >
+          <Loader />
+        </Flex>
+      </PageShell>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <ToastWithRedirect to={-1} message={"Sorry, something went wrong."} />
+    );
+  }
+
+  const authenticated = !!data.user && !!data.session;
+
+  if (!authenticated) {
+    return (
+      <ToastWithRedirect to={"/login"} message={"Please log in to continue."} />
+    );
+  }
+
+  const onboarded = !!data.user.leetcodeUsername;
+
+  if (!onboarded) {
+    return (
+      <ToastWithRedirect
+        to="/onboarding"
+        message="Please finish the onboarding to gain access."
+      />
+    );
+  }
+
+  return <PageShell>{lobbyCode}</PageShell>;
+}
