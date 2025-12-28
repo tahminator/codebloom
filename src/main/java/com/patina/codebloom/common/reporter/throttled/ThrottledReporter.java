c98298874cd9de38afa0d56d37711526c472a9ec
alfardil <fardilfayed@gmail.com>
2025-12-27 20:25:52 -0500

    521: moved changes to our reporter class and modified test to use reorter

--- src/main/java/com/patina/codebloom/common/reporter/throttled/ThrottledReporter.java
+++ src/main/java/com/patina/codebloom/common/reporter/throttled/ThrottledReporter.java
@@ -9,13 +9,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import lombok.NonNull;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /** Attaches a rate limiter over {@link Reporter} to avoid too many user submission reports */
@@ -23,9 +18,6 @@ import org.springframework.stereotype.Component;
 public class ThrottledReporter extends Reporter {
 
     private final Bucket rateLimiter;
-    private final Map<String, ReportCounter> counters = new ConcurrentHashMap<>();
-    private static final int OCCURENCE_THRESHOLD = 3;
-    private static final long TIME_LIMIT_MINUTES = 30;
 
     private Bucket initializeBucket() {
         var bandwidth = Bandwidth.builder()
@@ -43,10 +35,6 @@ public class ThrottledReporter extends Reporter {
     public ThrottledReporter(final JDAClient jdaClient) {
         super(jdaClient);
         this.rateLimiter = initializeBucket();
-    }
-
-    protected long now() {
-        return System.currentTimeMillis();
     }
 
     /** Convert the stacktrace of a {@linkplain Throwable} into a string. */
@@ -68,9 +56,7 @@ public class ThrottledReporter extends Reporter {
         if (!checkToken()) {
             return;
         }
-        if (shouldReport(key)) {
-            super.error(key, report);
-        }
+        super.error(key, report);
     }
 
     /** Report a log. */
@@ -80,39 +66,6 @@ public class ThrottledReporter extends Reporter {
         if (!checkToken()) {
             return;
         }
-        if (shouldReport(key)) {
-            super.log(key, report);
-        }
-    }
-    /**
-     * Determine if a report should be sent. If threshold is reached within the time limit, the error will be reported.
-     *
-     * @param key a non-null identifer to group report types
-     * @return {@code true} if the report should be sent, {@code false} otherwise
-     */
-    public boolean shouldReport(@NonNull String key) {
-        long now = now();
-
-        ReportCounter counter = counters.compute(key, (k, v) -> {
-            if (v == null || now > v.getExpireTime()) {
-                return new ReportCounter(
-                        new AtomicInteger(1),
-                        now + Duration.ofMinutes(TIME_LIMIT_MINUTES).toMillis());
-            }
-            v.getCount().incrementAndGet();
-            return v;
-        });
-
-        if (counter.getCount().get() >= OCCURENCE_THRESHOLD) {
-            return counters.remove(key, counter);
-        }
-        return false;
-    }
-
-    /** Clean up anything that hadn't been reported every 30 minutes. */
-    @Scheduled(fixedRate = TIME_LIMIT_MINUTES, timeUnit = TimeUnit.MINUTES)
-    public void cleanUp() {
-        long now = now();
-        counters.entrySet().removeIf(entry -> now > entry.getValue().getExpireTime());
+        super.log(key, report);
     }
 }
