847dd87213331b0bcfe60e03709e3673123fd466
alfardil <fardilfayed@gmail.com>
2026-01-16 13:32:56 -0500

    612: fixing how we call and use simpleredisprovider + simpleredis

--- src/main/java/org/patinanetwork/codebloom/api/auth/AuthController.java
+++ src/main/java/org/patinanetwork/codebloom/api/auth/AuthController.java
@@ -37,6 +37,7 @@ import org.patinanetwork.codebloom.common.schools.magic.MagicLink;
 import org.patinanetwork.codebloom.common.security.AuthenticationObject;
 import org.patinanetwork.codebloom.common.security.Protector;
 import org.patinanetwork.codebloom.common.security.annotation.Protected;
+import org.patinanetwork.codebloom.common.simpleredis.SimpleRedis;
 import org.patinanetwork.codebloom.common.simpleredis.SimpleRedisProvider;
 import org.patinanetwork.codebloom.common.simpleredis.SimpleRedisSlot;
 import org.patinanetwork.codebloom.common.url.ServerUrlUtils;
@@ -69,7 +70,7 @@ public class AuthController {
     private final UserTagRepository userTagRepository;
     private final Reporter reporter;
     private final ReactEmailClient reactEmailClient;
-    private final SimpleRedisProvider simpleRedis;
+    private final SimpleRedis<Long> simpleRedis;
 
     public AuthController(
             final SessionRepository sessionRepository,
@@ -81,7 +82,7 @@ public class AuthController {
             final UserTagRepository userTagRepository,
             final Reporter reporter,
             final ReactEmailClient reactEmailClient,
-            final SimpleRedisProvider simpleRedis) {
+            final SimpleRedisProvider simpleRedisProvider) {
         this.sessionRepository = sessionRepository;
         this.protector = protector;
         this.userRepository = userRepository;
@@ -91,7 +92,7 @@ public class AuthController {
         this.userTagRepository = userTagRepository;
         this.reporter = reporter;
         this.reactEmailClient = reactEmailClient;
-        this.simpleRedis = simpleRedis;
+        this.simpleRedis = simpleRedisProvider.select(SimpleRedisSlot.VERIFICATION_EMAIL_SENDING);
     }
 
     @Operation(
@@ -182,10 +183,8 @@ public class AuthController {
                     "The email is not part of our supported schools domains: " + supportedSchools);
         }
 
-        // 10 second rate limit
-        var slot = SimpleRedisSlot.VERIFICATION_EMAIL_SENDING;
-        if (simpleRedis.containsKey(slot, userId)) {
-            long timeThen = (long) simpleRedis.get(slot, userId);
+        if (simpleRedis.containsKey(userId)) {
+            long timeThen = simpleRedis.get(userId);
             long timeNow = System.currentTimeMillis();
             long difference = (timeNow - timeThen) / 1000;
 
@@ -197,7 +196,7 @@ public class AuthController {
             }
         }
 
-        simpleRedis.put(slot, userId, System.currentTimeMillis());
+        simpleRedis.put(userId, System.currentTimeMillis());
 
         MagicLink magicLink = new MagicLink(email, userId);
         try {
