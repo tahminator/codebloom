bd77e305d9b6035f75606b065290b1b82ec867b4
alfardil <fardilfayed@gmail.com>
2026-01-16 18:10:46 -0500

    612: added javaDocs and added flag to check redis key

--- src/main/java/org/patinanetwork/codebloom/common/simpleredis/SimpleRedisSlot.java
+++ src/main/java/org/patinanetwork/codebloom/common/simpleredis/SimpleRedisSlot.java
@@ -1,9 +1,13 @@
 package org.patinanetwork.codebloom.common.simpleredis;
 
 import io.github.bucket4j.Bucket;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Slots for Redis databases that are supported. The slots internally map to an index and the class of the value stored.
+ */
 @Getter
 @AllArgsConstructor
 public final class SimpleRedisSlot<V> {
@@ -13,4 +17,7 @@ public final class SimpleRedisSlot<V> {
     public static final SimpleRedisSlot<Long> SUBMISSION_REFRESH = new SimpleRedisSlot<>(0, Long.class);
     public static final SimpleRedisSlot<Long> VERIFICATION_EMAIL_SENDING = new SimpleRedisSlot<>(1, Long.class);
     public static final SimpleRedisSlot<Bucket> GLOBAL_RATE_LIMIT = new SimpleRedisSlot<>(2, Bucket.class);
+
+    public static final List<SimpleRedisSlot<?>> ALL =
+            List.of(SUBMISSION_REFRESH, VERIFICATION_EMAIL_SENDING, GLOBAL_RATE_LIMIT);
 }
