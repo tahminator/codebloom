37ee9781b26266f934adaf145b5fbcb345aa1cff
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-18 20:00:04 -0500

    641:

--- .github/scripts/redeploy/db/index.ts
+++ .github/scripts/redeploy/db/index.ts
@@ -2,7 +2,34 @@ import type { Environment } from "redeploy/types";
 
 import { $ } from "bun";
 
-export async function _migrateDb(environment: Environment) {
-  environment.toUpperCase();
+export async function _migrateDb({
+  environment,
+  envVariables,
+  sha,
+}: {
+  environment: Environment;
+  envVariables: Map<string, string>;
+  sha?: string;
+}): Promise<void> {
   await $`git fetch origin main:main`;
+
+  if (environment === "staging") {
+    if (!sha) {
+      throw new Error(
+        "SHA must be available in ENV if script is being run in staging environment.",
+      );
+    }
+    const diffOutput = await $`git diff --name-only main...${sha}`.text();
+    const files = diffOutput.split("\n");
+
+    const hasDbChanges = files.some((file) => file.startsWith("db/"));
+    if (!hasDbChanges) {
+      console.log("in staging, skipping db migration.");
+      return;
+    }
+  }
+
+  await $.env(
+    Object.fromEntries(envVariables),
+  )`./mvnw flyway:migrate -Dflyway.locations=filesystem:./db/migration`;
 }
