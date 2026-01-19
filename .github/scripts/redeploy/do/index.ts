0da982a8bbb9ea22aae3644e04900756d19afc43
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-18 20:00:04 -0500

    641:

---
+++ .github/scripts/redeploy/do/index.ts
@@ -0,0 +1,109 @@
+import type { Environment } from "redeploy/types";
+
+import {
+  DigitalOceanApiKeyAuthenticationProvider,
+  FetchRequestAdapter,
+  createDigitalOceanClient,
+  type App_variable_definition,
+  type App_response,
+} from "@digitalocean/dots";
+import { _createAppAndgetAppId } from "redeploy/do/apps/create";
+import { _getAppId } from "redeploy/do/apps/get";
+
+import { prodSpec, stgSpec } from "../../../../.do/specs";
+
+export async function _migrateDo({
+  token,
+  environment,
+  projectId,
+  envVariables,
+}: {
+  token: string;
+  environment: Environment;
+  projectId: string;
+  envVariables: Map<string, string>;
+}): Promise<void> {
+  const authProvider = new DigitalOceanApiKeyAuthenticationProvider(token);
+  const adapter = new FetchRequestAdapter(authProvider);
+  const client = createDigitalOceanClient(adapter);
+
+  const envs: App_variable_definition[] = envVariables
+    .entries()
+    .map(([key, value]) => {
+      const env: App_variable_definition = {
+        key,
+        value,
+        scope: "RUN_TIME",
+        type: "SECRET",
+      };
+      return env;
+    })
+    .toArray();
+
+  const spec = environment === "staging" ? stgSpec(envs) : prodSpec(envs);
+
+  const appId = await (async () => {
+    const v = await _getAppId(client, projectId, spec);
+
+    if (v) {
+      return v;
+    }
+
+    const v2 = await _createAppAndgetAppId(client, projectId, spec);
+    if (!v2) {
+      throw new Error(
+        "App not found and failed to create as well. Please alert Codebloom team.",
+      );
+    }
+
+    return v2;
+  })();
+
+  let res: App_response | undefined;
+  try {
+    res = await client.v2.apps.byApp_Id(appId).put({
+      spec,
+      updateAllSourceVersions: true,
+    });
+  } catch (e) {
+    console.error(e);
+    return process.exit(1);
+  }
+
+  const pendingDeploymentId = res?.app?.pendingDeployment?.id;
+
+  if (!pendingDeploymentId) {
+    console.error("Failed to find pending deployment.");
+    process.exit(1);
+  }
+
+  console.log("Deployment ID:", pendingDeploymentId);
+
+  let ready = false;
+  const attempts = 60;
+
+  for (let i = 1; i <= attempts; i++) {
+    const res = await client.v2.apps
+      .byApp_Id(appId)
+      .deployments.byDeployment_id(pendingDeploymentId)
+      .get();
+
+    const phase = res?.deployment?.phase ?? "UNKNOWN";
+
+    console.log("Deployment phase:", phase);
+
+    if (phase === "ACTIVE") {
+      console.log("Deployment has completed!");
+      ready = true;
+      break;
+    }
+
+    console.log(`Waiting for deployment to complete... (${i}/${attempts})`);
+    await Bun.sleep(10000);
+  }
+
+  if (!ready) {
+    console.error("Deployment did not reach a valid state within 10 minutes.");
+    process.exit(1);
+  }
+}
--- .github/scripts/redeploy/index.ts
+++ .github/scripts/redeploy/index.ts
@@ -1,18 +1,10 @@
 import type { Environment } from "redeploy/types";
 
-import {
-  createDigitalOceanClient,
-  DigitalOceanApiKeyAuthenticationProvider,
-  FetchRequestAdapter,
-  type App_response,
-  type App_variable_definition,
-} from "@digitalocean/dots";
 import { getEnvVariables } from "load-secrets/env/load";
-import { _createAppAndgetAppId } from "redeploy/apps/create";
-import { _getAppId } from "redeploy/apps/get";
 import { _migrateDb } from "redeploy/db";
+import { _migrateDo } from "redeploy/do";
 
-import { prodSpec, stgSpec } from "../../../.do/specs";
+const sha = process.env.SHA;
 
 const projectId = (() => {
   const v = process.env.DIGITALOCEAN_PROJECT_ID;
@@ -44,93 +36,22 @@ const environment: Environment = (() => {
 })();
 
 async function main() {
-  const authProvider = new DigitalOceanApiKeyAuthenticationProvider(token);
-  const adapter = new FetchRequestAdapter(authProvider);
-  const client = createDigitalOceanClient(adapter);
-
   // should already be called
   // await $`git-crypt unlock`;
-  const loaded = await getEnvVariables([environment]);
+  const envVariables = await getEnvVariables([environment]);
 
-  const envs: App_variable_definition[] = loaded
-    .entries()
-    .map(([key, value]) => {
-      const env: App_variable_definition = {
-        key,
-        value,
-        scope: "RUN_TIME",
-        type: "SECRET",
-      };
-      return env;
-    })
-    .toArray();
+  await _migrateDb({
+    envVariables,
+    environment,
+    sha,
+  });
 
-  const spec = environment === "staging" ? stgSpec(envs) : prodSpec(envs);
-
-  const appId = await (async () => {
-    const v = await _getAppId(client, projectId, spec);
-
-    if (v) {
-      return v;
-    }
-
-    const v2 = await _createAppAndgetAppId(client, projectId, spec);
-    if (!v2) {
-      throw new Error(
-        "App not found and failed to create as well. Please alert Codebloom team.",
-      );
-    }
-
-    return v2;
-  })();
-
-  let res: App_response | undefined;
-  try {
-    res = await client.v2.apps.byApp_Id(appId).put({
-      spec,
-      updateAllSourceVersions: true,
-    });
-  } catch (e) {
-    console.error(e);
-    return process.exit(1);
-  }
-
-  const pendingDeploymentId = res?.app?.pendingDeployment?.id;
-
-  if (!pendingDeploymentId) {
-    console.error("Failed to find pending deployment.");
-    process.exit(1);
-  }
-
-  console.log("Deployment ID:", pendingDeploymentId);
-
-  let ready = false;
-  const attempts = 60;
-
-  for (let i = 1; i <= attempts; i++) {
-    const res = await client.v2.apps
-      .byApp_Id(appId)
-      .deployments.byDeployment_id(pendingDeploymentId)
-      .get();
-
-    const phase = res?.deployment?.phase ?? "UNKNOWN";
-
-    console.log("Deployment phase:", phase);
-
-    if (phase === "ACTIVE") {
-      console.log("Deployment has completed!");
-      ready = true;
-      break;
-    }
-
-    console.log(`Waiting for deployment to complete... (${i}/${attempts})`);
-    await Bun.sleep(10000);
-  }
-
-  if (!ready) {
-    console.error("Deployment did not reach a valid state within 10 minutes.");
-    process.exit(1);
-  }
+  await _migrateDo({
+    projectId,
+    token,
+    environment,
+    envVariables,
+  });
 }
 
 main()
