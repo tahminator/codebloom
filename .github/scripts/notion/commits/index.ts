cb946f15428bd9b10ae32da8aa349f21a1d7de92
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-19 00:42:29 -0500

    641: gk3ng3g

--- .github/scripts/notion/commits/index.ts
+++ .github/scripts/notion/commits/index.ts
@@ -1,1 +1,48 @@
-export async function _checkCommits(taskId: number) {}
+import { $ } from "bun";
+import { sendMessage } from "utils/send-message";
+
+export async function _checkCommits(taskId: number) {
+  const taskIdString = taskId.toString();
+
+  const res = await $`gh pr view ${taskId} --json commits`.text();
+  const { commits } = JSON.parse(res) as {
+    commits?: { messageHeadline: string }[];
+  };
+
+  if (!commits || commits.length === 0) {
+    console.log("No commits found in PR");
+    return;
+  }
+
+  const failedCommits = commits
+    .map((commit) => commit.messageHeadline)
+    .filter((message) => !message.startsWith(taskIdString));
+
+  if (failedCommits.length == 0) {
+    console.log("All commits are valid");
+    return;
+  }
+
+  for (const message of failedCommits) {
+    console.warn(`Invalid commit: ${message}`);
+  }
+
+  const failedList = failedCommits.join("\n");
+
+  await sendMessage(
+    taskId,
+    `
+### Commit Validation Failed
+The following commits do not start with the required Notion ID \`${taskIdString}\`:
+
+\`\`\`
+${failedList}
+\`\`\`
+
+Please rebase and update your commit messages.
+All messages should be of the following format: \`${taskIdString}: Example commit\``.trim(),
+  );
+
+  console.error("One or more commits do not match the Notion ID.");
+  process.exit(1);
+}
