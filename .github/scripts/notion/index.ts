2761c3fdea173046011e8f6974106b53ba60c7a8
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-20 02:10:37 -0500

    641: Overhauled CI with Bun Shell / Official SDKs (DESC)
    Rewrote a significant amount of the CI logic using Bun shell & official
    SDKs (@digitalocean/sdk, @notionhq/client, etc.). This includes
    "upserting" apps in DigitalOcean so APP_IDs can be determined at
    runtime.

---
+++ .github/scripts/notion/index.ts
@@ -0,0 +1,75 @@
+import { getEnvVariables } from "load-secrets/env/load";
+import { _checkCommits } from "notion/commits";
+import { checkNotionPrAndGetTask } from "notion/pr";
+
+export * from "./pr";
+
+const prId = (() => {
+  const v = process.env.PR_ID;
+  if (!v) {
+    throw new Error("PR_ID is required");
+  }
+  const n = Number(v);
+  if (isNaN(n)) {
+    throw new Error("PR_ID is not a number");
+  }
+  return n;
+})();
+
+const getGhaOutput = process.env.GET_GHA_OUTPUT === "true";
+const githubOutputFile = process.env.GITHUB_OUTPUT;
+
+async function main() {
+  const { notionDbId, notionSecret } = parseCiEnv(
+    await getEnvVariables(["ci"]),
+  );
+
+  const { taskId, taskContent } = await checkNotionPrAndGetTask(
+    notionSecret,
+    prId,
+    notionDbId,
+  );
+
+  console.log(taskContent);
+
+  if (getGhaOutput && githubOutputFile) {
+    const w = Bun.file(githubOutputFile).writer();
+    await w.write(`context<<EOF\n${taskContent}\nEOF\n`);
+    await w.flush();
+    await w.end();
+  }
+
+  await _checkCommits(taskId, prId);
+}
+
+function parseCiEnv(ciEnv: Record<string, string>) {
+  const notionDbId = (() => {
+    const v = ciEnv["NOTION_TASK_DB_ID"];
+    if (!v) {
+      throw new Error("Missing NOTION_TASK_DB_ID from .env.ci");
+    }
+    return v;
+  })();
+
+  const notionSecret = (() => {
+    const v = ciEnv["NOTION_SECRET"];
+    if (!v) {
+      throw new Error("Missing NOTION_SECRET from .env.ci");
+    }
+    return v;
+  })();
+
+  return {
+    notionDbId,
+    notionSecret,
+  };
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
---
+++ .github/scripts/utils/send-message/gha.ts
@@ -0,0 +1,37 @@
+import { sendMessage } from "utils/send-message";
+
+const prId = (() => {
+  const v = process.env.PR_ID;
+  if (!v) {
+    throw new Error("PR_ID is required");
+  }
+  const n = Number(v);
+  if (Number.isNaN(n)) {
+    throw new Error("PR_ID must be a number");
+  }
+  return n;
+})();
+
+const message = (() => {
+  const v = process.env.MESSAGE;
+  if (!v) {
+    throw new Error("MESSAGE is required");
+  }
+  return v;
+})();
+
+/**
+ * @deprecated usable but is not recommended.
+ */
+export async function main() {
+  await sendMessage(prId, message);
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
