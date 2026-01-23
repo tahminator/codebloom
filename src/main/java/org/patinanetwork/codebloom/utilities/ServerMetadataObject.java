8aacf06f2971978dd17f5a39036a6dadd44b9166
Nancy Huang <205217630+naanci@users.noreply.github.com>
2026-01-23 00:52:17 -0500

    634: switch null check method

--- src/main/java/org/patinanetwork/codebloom/utilities/ServerMetadataObject.java
+++ src/main/java/org/patinanetwork/codebloom/utilities/ServerMetadataObject.java
@@ -2,6 +2,7 @@ package org.patinanetwork.codebloom.utilities;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import com.google.common.base.Strings;
 
 public class ServerMetadataObject {
 
@@ -12,7 +13,7 @@ public class ServerMetadataObject {
 
     public ServerMetadataObject(final String commitSha) {
         this.name = "Codebloom";
-        this.version = (commitSha == null || commitSha.isBlank()) ? "unknown" : commitSha;
+        this.version = Strings.isNullOrEmpty(commitSha) ? "unknown" : commitSha;
         this.description = "LeetCode leaderboard for Patina Network members to track progress and motivate each other.";
         this.authors = new ArrayList<>(Arrays.asList(
                 "Alisha Zaman", "Alfardil Alam", "Angela Yu", "Tahmid Ahmed", "Arshadul Monir", "Nancy Huang"));
