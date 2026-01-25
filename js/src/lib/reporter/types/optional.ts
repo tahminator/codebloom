e3449fa6e9daf510d11756fab96858d2811ee5d8
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-25 04:15:29 -0500

    635: Add query key generation logic to ApiURL, replace custom types with type-fest

--- js/src/lib/reporter/types/optional.ts
+++
@@ -1,8 +0,0 @@
-export type OptionalIfEmpty<T, R = never> = keyof T extends R ? R : T;
-
-export type IsNever<T> = [T] extends [never] ? true : false;
-
-export type If<Type extends boolean, IfBranch, ElseBranch> =
-  IsNever<Type> extends true ? ElseBranch
-  : Type extends true ? IfBranch
-  : ElseBranch;
