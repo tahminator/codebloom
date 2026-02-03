54b2aa43dedd5b17e23946030bd0bc0cfe7c4a3c
Tahmid Ahmed <tahmid@tahmid.io>
2026-02-02 19:58:40 -0500

    642: Wire up vitest & RTL

--- js/vite.config.ts
+++ js/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -19,5 +20,14 @@ export default defineConfig({
     alias: {
       "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      enabled: true,
+      provider: "v8",
+    },
+    setupFiles: ["src/lib/test/defaults.tsx"],
   },
 });
