086be6c495a9fa94948b9fbe651d26350e160830
Tahmid Ahmed <tahmid@tahmid.io>
2026-02-05 00:32:25 -0500

    746: Add explicit test exclusions for frontend and backend for Sonar

---
+++ exclusions.ts
@@ -0,0 +1,13 @@
+/**
+ * See https://github.com/tahminator/codebloom/blob/main/.github/scripts/utils/upload.ts
+ * for test exclusion usages.
+ */
+
+const backendBaseDir = "src/main/java/org/patinanetwork/codebloom";
+const frontendBaseDir = "js/src";
+
+export const backendExclusions = [
+  `${backendBaseDir}/common/dto/**`,
+  `${backendBaseDir}/playwright/**`,
+];
+export const frontendExclusions = [];
