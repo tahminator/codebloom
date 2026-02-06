e18c7b98268504be9c037a35ab4d7d480e1e2d0a
Angela Yu <angelayu0530@gmail.com>
2026-02-05 21:49:52 -0500

    735: Pr changes

--- src/main/java/org/patinanetwork/codebloom/common/utils/leaderboard/LeaderboardUtils.java
+++ src/main/java/org/patinanetwork/codebloom/common/utils/leaderboard/LeaderboardUtils.java
@@ -2,19 +2,10 @@ package org.patinanetwork.codebloom.common.utils.leaderboard;
 
 import java.util.List;
 import org.patinanetwork.codebloom.common.db.models.user.UserWithScore;
-import org.patinanetwork.codebloom.common.page.Indexed;
 
 public final class LeaderboardUtils {
 
-    private LeaderboardUtils() {}
-
     public static List<UserWithScore> filterUsersWithPoints(final List<UserWithScore> users) {
         return users.stream().filter(user -> user.getTotalScore() > 0).toList();
-    }
-
-    public static List<Indexed<UserWithScore>> filterIndexedUsersWithPoints(final List<Indexed<UserWithScore>> users) {
-        return users.stream()
-                .filter(indexed -> indexed.getItem().getTotalScore() > 0)
-                .toList();
     }
 }
