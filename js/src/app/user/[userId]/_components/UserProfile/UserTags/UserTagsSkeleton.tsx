5e4eed5cefc4b91448cb60e2ea08b540dc0ab391
haokingluo <hao5k6032@gmail.com>
2026-02-05 22:10:28 -0500

    720: fix leaderboard, user profile skeletons

--- js/src/app/user/[userId]/_components/UserProfile/UserTags/UserTagsSkeleton.tsx
+++ js/src/app/user/[userId]/_components/UserProfile/UserTags/UserTagsSkeleton.tsx
@@ -1,16 +1,30 @@
-import { Flex, Skeleton } from "@mantine/core";
+import { Flex, Skeleton, Divider, Text} from "@mantine/core";
 
 export default function UserTagsSkeleton() {
   return (
     <>
-      <Flex wrap="wrap" gap="sm">
-        <Skeleton height={40} width={40} />
-        <Skeleton height={40} width={40} />
-      </Flex>
-      <Flex wrap="wrap" gap="sm">
-        <Skeleton height={40} width={40} />
-        <Skeleton height={40} width={40} />
-        <Skeleton height={40} width={40} />
+      <Flex direction={"column"} align={"center"} gap={"sm"} w={"100%"}>
+        <Divider w={"70%"} />
+          <Text size={"sm"}>
+            <Skeleton>
+              Leaderboard
+            </Skeleton>
+          </Text>
+        <Flex wrap="wrap" gap="sm">
+          <Skeleton height={40} width={40} />
+          <Skeleton height={40} width={40} />
+        </Flex>
+        <Divider w={"70%"} />
+        <Text size={"sm"}>
+          <Skeleton>
+            Achievements
+          </Skeleton>
+        </Text>
+        <Flex wrap="wrap" gap="sm">
+          <Skeleton height={40} width={40} />
+          <Skeleton height={40} width={40} />
+          <Skeleton height={40} width={40} />
+        </Flex>
       </Flex>
     </>
   );
