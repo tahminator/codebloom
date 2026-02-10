42bd509681f64f7195d497447a31dbcba7c89a16
haokingluo <hao5k6032@gmail.com>
2026-02-09 19:16:08 -0500

    720: add frontend tests
    Added minimum frontend tests for UserSubmissions and ProfilePicture

---
+++ js/src/app/user/[userId]/_components/UserProfile/ProfilePicture/ProfilePicture.test.tsx
@@ -0,0 +1,22 @@
+import { MOCK_USER_ID } from "@/__mock__/user/index.ts";
+import { screen, cleanup } from '@testing-library/react';
+import { TestUtils, TestUtilTypes } from "@/lib/test";
+import ProfilePicture from "./ProfilePicture.tsx";
+
+describe('ProfilePicture succeeded', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  let renderProviderFn: TestUtilTypes.RenderWithAllProvidersFn | null = null;
+  beforeEach(() => {
+    renderProviderFn = TestUtils.getRenderWithAllProvidersFn();
+  });
+
+  it("should render skeleton profile picture initially", () => {
+    renderProviderFn?.(<ProfilePicture userId={MOCK_USER_ID}/>);
+    const element = screen.getByTestId("user-profile-skeleton-picture");
+    expect(element).toBeInTheDocument();
+    expect(element).toBeVisible();
+  })
+})
---
+++ js/src/app/user/[userId]/submissions/_components/UserSubmissions/UserSubmissions.test.tsx
@@ -0,0 +1,22 @@
+import { MOCK_USER_ID } from "@/__mock__/user/index.ts";
+import { screen, cleanup } from '@testing-library/react';
+import { TestUtils, TestUtilTypes } from "@/lib/test";
+import UserSubmissions from "./UserSubmissions.tsx";
+
+describe('UserSubmissions succeeded', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  let renderProviderFn: TestUtilTypes.RenderWithAllProvidersFn | null = null;
+  beforeEach(() => {
+    renderProviderFn = TestUtils.getRenderWithAllProvidersFn();
+  });
+
+  it("should render skeleton stack of submissions initially", () => {
+    renderProviderFn?.(<UserSubmissions userId={MOCK_USER_ID}/>);
+    const element = screen.getByTestId("user-profile-skeleton-submissions-stack");
+    expect(element).toBeInTheDocument();
+    expect(element).toBeVisible();
+  })
+})
