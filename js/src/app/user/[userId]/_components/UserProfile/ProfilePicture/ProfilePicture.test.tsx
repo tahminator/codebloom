60fff72dee4de51bf38e0d4ca158bb6c89bd555d
haokingluo <hao5k6032@gmail.com>
2026-02-09 23:59:11 -0500

    720: prettier

--- js/src/app/user/[userId]/_components/UserProfile/ProfilePicture/ProfilePicture.test.tsx
+++ js/src/app/user/[userId]/_components/UserProfile/ProfilePicture/ProfilePicture.test.tsx
@@ -1,9 +1,9 @@
 import { MOCK_USER_ID } from "@/__mock__/user/index.ts";
-import { screen, cleanup } from '@testing-library/react';
+import { screen, cleanup } from "@testing-library/react";
 import { TestUtils, TestUtilTypes } from "@/lib/test";
 import ProfilePicture from "./ProfilePicture.tsx";
 
-describe('ProfilePicture succeeded', () => {
+describe("ProfilePicture succeeded", () => {
   afterEach(() => {
     cleanup();
   });
@@ -14,9 +14,9 @@ describe('ProfilePicture succeeded', () => {
   });
 
   it("should render skeleton profile picture initially", () => {
-    renderProviderFn?.(<ProfilePicture userId={MOCK_USER_ID}/>);
+    renderProviderFn?.(<ProfilePicture userId={MOCK_USER_ID} />);
     const element = screen.getByTestId("user-profile-skeleton-picture");
     expect(element).toBeInTheDocument();
     expect(element).toBeVisible();
-  })
-})
+  });
+});
--- js/src/app/user/[userId]/_components/UserProfile/ProfilePicture/ProfilePictureSkeleton.tsx
+++ js/src/app/user/[userId]/_components/UserProfile/ProfilePicture/ProfilePictureSkeleton.tsx
@@ -1,5 +1,12 @@
 import { Skeleton } from "@mantine/core";
 
 export default function ProfilePictureSkeleton() {
-  return <Skeleton data-testid="user-profile-skeleton-picture" height={150} width={150} mx={"md"} />;
+  return (
+    <Skeleton
+      data-testid="user-profile-skeleton-picture"
+      height={150}
+      width={150}
+      mx={"md"}
+    />
+  );
 }
