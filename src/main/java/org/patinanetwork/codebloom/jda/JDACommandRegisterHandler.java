f19fee8d1910cb0f0749655cc6f4dd73c9c9642f
Tahmid Ahmed <tahmid@tahmid.io>
2026-02-14 00:43:57 -0500

    697: Implement slash command registration correctly according to JDA docs

--- src/main/java/org/patinanetwork/codebloom/jda/JDACommandRegisterHandler.java
+++ src/main/java/org/patinanetwork/codebloom/jda/JDACommandRegisterHandler.java
@@ -2,6 +2,8 @@ package org.patinanetwork.codebloom.jda;
 
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import org.patinanetwork.codebloom.jda.command.JDASlashCommand;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +18,11 @@ public class JDACommandRegisterHandler extends ListenerAdapter {
     @Override
     public void onReady(final ReadyEvent event) {
         for (var guild : event.getJDA().getGuilds()) {
-            JDASlashCommand.list().stream().forEach(c -> {
-                guild.updateCommands().queue();
-                guild.upsertCommand(c.getCommand(), c.getDescription()).queue();
-            });
+            var commands = JDASlashCommand.list().stream()
+                    .map(c -> Commands.slash(c.getCommand(), c.getDescription()))
+                    .toArray(SlashCommandData[]::new);
+
+            guild.updateCommands().addCommands(commands).queue();
         }
     }
 }
