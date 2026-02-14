2867b36518eee90dd9ea8511a01d17f4133b8e09
Tahmid Ahmed <tahmid@tahmid.io>
2026-02-14 01:16:15 -0500

    697: update tests

--- src/test/java/org/patinanetwork/codebloom/jda/JDACommandRegisterHandlerTest.java
+++ src/test/java/org/patinanetwork/codebloom/jda/JDACommandRegisterHandlerTest.java
@@ -6,12 +6,14 @@ import java.util.List;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
-import net.dv8tion.jda.api.requests.restaction.CommandCreateAction;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.patinanetwork.codebloom.jda.command.JDASlashCommand;
 
 @ExtendWith(MockitoExtension.class)
 class JDACommandRegisterHandlerTest {
@@ -30,20 +32,23 @@ class JDACommandRegisterHandlerTest {
 
         Guild guild1 = mock(Guild.class);
 
-        CommandCreateAction action1 = mock(CommandCreateAction.class);
+        CommandListUpdateAction action1 = mock(CommandListUpdateAction.class);
 
         when(readyEvent.getJDA()).thenReturn(jda);
         when(jda.getGuilds()).thenReturn(List.of(guild1));
-
-        when(guild1.upsertCommand("leaderboard", "Shows the current weekly leaderboard"))
-                .thenReturn(action1);
-
-        CommandListUpdateAction clearAction = mock(CommandListUpdateAction.class);
-        when(guild1.updateCommands()).thenReturn(clearAction);
+        when(guild1.updateCommands()).thenReturn(action1);
+        when(action1.addCommands(any(SlashCommandData[].class))).thenReturn(action1);
 
         listener.onReady(readyEvent);
 
-        verify(guild1).upsertCommand("leaderboard", "Shows the current weekly leaderboard");
+        verify(guild1).updateCommands();
+
+        ArgumentCaptor<SlashCommandData[]> commandsCaptor = ArgumentCaptor.forClass(SlashCommandData[].class);
+        verify(action1).addCommands(commandsCaptor.capture());
         verify(action1).queue();
+
+        SlashCommandData[] capturedCommands = commandsCaptor.getValue();
+        assert capturedCommands.length == JDASlashCommand.list().size();
+        assert capturedCommands[0].getName().equals("leaderboard");
     }
 }
