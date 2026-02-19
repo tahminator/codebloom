9789eed7991e99b82c130693186171187a4ec271
Tahmid Ahmed <tahmid@tahmid.io>
2026-02-19 10:34:31 -0500

    700: (HOTFIX) Fix incorrect logic for slash commands by renaming file as is expected

--- .github/scripts/load-slash-commands/commands.ts
+++ .github/scripts/load-slash-commands/commands.ts
@@ -1,2 +1,5 @@
 // each COMMAND must accept certain input arguments or it will fail.
+// NOTE: YOU MUST NAME THE WORKFLOW FILE `${name}-command.yml`
+//
+// https://github.com/peter-evans/slash-command-dispatch/blob/main/docs/workflow-dispatch.md
 export const COMMANDS = ["deploy", "ai", "copy"] as const;
