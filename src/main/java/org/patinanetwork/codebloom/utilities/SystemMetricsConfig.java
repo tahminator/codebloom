98e6702b658e1c02aefd46d055c192abdca4f65d
Tahmid Ahmed <tahmid@tahmid.io>
2026-02-21 03:47:25 -0500

    811: Add SHA as a metric

--- src/main/java/org/patinanetwork/codebloom/utilities/SystemMetricsConfig.java
+++ src/main/java/org/patinanetwork/codebloom/utilities/SystemMetricsConfig.java
@@ -1,17 +1,30 @@
 package org.patinanetwork.codebloom.utilities;
 
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import org.patinanetwork.codebloom.utilities.sha.CommitShaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import oshi.SystemInfo;
 import oshi.hardware.GlobalMemory;
 
 @Configuration
+@EnableConfigurationProperties(CommitShaProperties.class)
 public class SystemMetricsConfig {
 
     @Bean
     public SystemInfo systemInfo() {
         return new SystemInfo();
+    }
+
+    @Bean
+    public MeterBinder applicationInfoMetrics(CommitShaProperties commitShaProperties) {
+        return registry -> {
+            var tags = Tags.of(Tag.of("sha", commitShaProperties.getSha()));
+            registry.gauge("application.info", tags, 1, n -> 1.0);
+        };
     }
 
     @Bean
