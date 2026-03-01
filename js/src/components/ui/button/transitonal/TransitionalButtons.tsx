8d06da73e628b01bc630028b1b7ac1674b765d2f
Angela Yu <angelayu0530@gmail.com>
2026-03-01 10:38:58 -0500

    788: Fixed Fix where buttons r broken

--- js/src/components/ui/button/transitonal/TransitionalButtons.tsx
+++ js/src/components/ui/button/transitonal/TransitionalButtons.tsx
@@ -128,6 +128,7 @@ export default function TransitionalButtons({
       ref={parentRef}
       className={classes.navGroup}
       onMouseLeave={handleParentMouseLeave}
+      wrap="nowrap"
     >
       <Box
         className={classes.hoverIndicator}
