2dbfeb8b976f3dffcda75cb08c5a9c69f9d56847
haokingluo <hao5k6032@gmail.com>
2026-03-01 17:51:36 -0500

    809: add tests

---
+++ js/src/components/ui/footer/Footer.test.tsx
@@ -0,0 +1,40 @@
+import { Footer } from "@/components/ui/footer/Footer";
+import { TestUtils, TestUtilTypes } from "@/lib/test";
+import { screen } from "@testing-library/react";
+
+describe("LeaderboardSkeleton succeeded", () => {
+  let renderProviderFn: TestUtilTypes.RenderWithAllProvidersFn | null = null;
+  beforeEach(() => {
+    renderProviderFn = TestUtils.getRenderWithAllProvidersFn();
+  });
+
+  it("should render mission text", () => {
+    renderProviderFn?.(<Footer />);
+    const elements = screen.getAllByText(
+      "LeetCode motivation site for Patina Network",
+    );
+    expect(elements).toHaveLength(2);
+    for (const element of elements) {
+      expect(element).toBeInTheDocument();
+      expect(element).toBeVisible();
+    }
+  });
+
+  it("should render links section", () => {
+    renderProviderFn?.(<Footer />);
+    const element = screen.getByTestId("footer-links-section");
+    expect(element).toBeInTheDocument();
+    expect(element).toBeVisible();
+  });
+
+  it("should have correct github link", () => {
+    renderProviderFn?.(<Footer />);
+    const element = screen.getByLabelText("CodeBloom GitHub");
+    expect(element).toBeInTheDocument();
+    expect(element).toBeVisible();
+    expect(element).toHaveAttribute(
+      "href",
+      "https://github.com/tahminator/codebloom",
+    );
+  });
+});
