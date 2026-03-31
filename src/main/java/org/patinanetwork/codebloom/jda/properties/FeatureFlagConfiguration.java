cf171ff48beb57de0464e62bed291857da242178
Alfardil Alam <aa11101@nyu.edu>
2026-03-31 16:36:31 -0400

    866: Added javadoc

--- src/main/java/org/patinanetwork/codebloom/jda/properties/FeatureFlagConfiguration.java
+++ src/main/java/org/patinanetwork/codebloom/jda/properties/FeatureFlagConfiguration.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/** Feature flags that we supported. Must be defined through application.yml */
 @Getter
 @Setter
 @Component
