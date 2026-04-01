3dbac03b6468c4eed38f607e52e6fb5da396069d
Arshadul Monir <arshadulmonir@gmail.com>
2026-04-01 13:58:28 -0400

    890: Added allowed Scheduled tasks to be conditional

--- src/main/java/org/patinanetwork/codebloom/CodebloomApplication.java
+++ src/main/java/org/patinanetwork/codebloom/CodebloomApplication.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableAsync
 @Slf4j
 public class CodebloomApplication {
